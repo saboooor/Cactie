b8f05948efe1e7ecb6b562dd8d2cd52c8a2a7b26
saboooor <saboor547@gmail.com>
2021-06-01 22:58:14 -0600

    even smaller index.js

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const Discord = require('discord.js');
-const fs = require('fs');
+const Discord = require('discord.js'); const fs = require('fs');
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'], intents: ['GUILDS', 'GUILD_MESSAGES', 'GUILD_MEMBERS', 'GUILD_BANS', 'GUILD_PRESENCES', 'GUILD_MESSAGE_REACTIONS', 'DIRECT_MESSAGES', 'DIRECT_MESSAGE_REACTIONS'], allowedMentions: { parse: ['users', 'roles', 'everyone'], repliedUser: true } });
-client.handlers = new Discord.Collection();
-const responseFiles = fs.readdirSync('./handlers').filter(file => file.endsWith('.js'));
-for (const file of responseFiles) require(`./handlers/${file}`)(client);
+for (const handler of fs.readdirSync('./handlers').filter(file => file.endsWith('.js'))) require(`./handlers/${handler}`)(client);
