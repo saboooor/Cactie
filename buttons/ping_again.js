eb95a1755e47c2cd49b24bef41d54f80cf222340
saboooor <saboor547@gmail.com>
2021-06-02 02:22:39 -0600

    ping again

---
+++ buttons/ping_again.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+module.exports = {
+	name: 'ping_again',
+	async execute(interaction, client) {
+		const Embed = new Discord.MessageEmbed()
+			.setColor(Math.floor(Math.random() * 16777215))
+			.setTitle('Double Pong!')
+			.setDescription(`${Date.now() - interaction.createdTimestamp}ms`);
+		interaction.update({ embeds: [Embed] });
+	},
+};
--- commands/user/other/ping.js
+++ commands/user/other/ping.js
@@ -5,10 +5,15 @@ module.exports = {
 	aliases: ['pong'],
 	cooldown: 2,
 	execute(message, args, client) {
+		const row = new Discord.MessageActionRow()
+			.addComponents(new Discord.MessageButton()
+				.setCustomID('ping_again')
+				.setLabel('Click here to ping again!')
+				.setStyle('PRIMARY'));
 		const Embed = new Discord.MessageEmbed()
 			.setColor(Math.floor(Math.random() * 16777215))
 			.setTitle('Pong!')
 			.setDescription(`${Date.now() - message.createdTimestamp}ms`);
-		message.channel.send(Embed);
+		message.channel.send({ embed: Embed, components: [row] });
 	},
 };
