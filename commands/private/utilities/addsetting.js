f266e3bcd73058e439b89c88f069835363df02ba
saboooor <saboor547@gmail.com>
2021-06-01 20:33:20 -0600

    add more commands

---
+++ commands/private/utilities/addsetting.js
@@ -0,0 +1,27 @@
+function minTwoDigits(n) {
+	return (n < 10 ? '0' : '') + n;
+}
+module.exports = {
+	name: 'addsetting',
+	description: 'Add a guild setting',
+	async execute(message, args, client, Discord) {
+		if (message.author.id !== '249638347306303499') return message.reply('You can\'t do that!');
+		if (args[1]) {
+			const [prop, ...value] = args;
+			client.guilds.cache.forEach(guild => {
+				client.settings.set(guild.id, value.join(' '), prop);
+				const rn = new Date();
+				const time = `${minTwoDigits(rn.getHours())}:${minTwoDigits(rn.getMinutes())}:${minTwoDigits(rn.getSeconds())}`;
+				console.log(`[${time} INFO]: Added setting: ${prop} = ${value}`);
+			});
+		}
+		const srvconfig = Object.keys(client.settings.get(message.guild.id)).map(prop => {
+			return `**${prop}** \`${client.settings.get(message.guild.id)[prop]}\``;
+		});
+		const Embed = new Discord.MessageEmbed()
+			.setColor(Math.floor(Math.random() * 16777215))
+			.setTitle('Settings')
+			.setDescription(`${srvconfig.join('\n')}`);
+		message.channel.send(Embed);
+	},
+};
---
+++ commands/private/utilities/eval.js
@@ -0,0 +1,25 @@
+function clean(text) {
+	if (typeof (text) === 'string') {return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));}
+	else {return text;}
+}
+module.exports = {
+	name: 'eval',
+	description: 'Execute any code',
+	aliases: ['ec'],
+	args: true,
+	usage: '<Code>',
+	async execute(message, args, client, Discord) {
+		if (message.author.id !== '249638347306303499') return message.reply('You can\'t do that!');
+		try {
+			const code = args.join(' ');
+			let evaled = eval(code);
+
+			if (typeof evaled !== 'string') {evaled = require('util').inspect(evaled);}
+
+			message.channel.send(clean(evaled), { code: 'xl' });
+		}
+		catch (err) {
+			message.channel.send(`\`ERROR\` \`\`\`xl\n${clean(err)}\n\`\`\``);
+		}
+	},
+};
