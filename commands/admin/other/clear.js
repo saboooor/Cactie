dc6944a64a39b8aadfcf01bb42a72fe6fcfb35f8
saboooor <saboor547@gmail.com>
2021-06-01 22:27:17 -0600

    a

--- commands/admin/other/clear.js
+++ commands/admin/other/clear.js
@@ -7,7 +7,6 @@ module.exports = {
 	usage: '<Amount of messages>',
 	permissions: 'MANAGE_MESSAGES',
 	async execute(message, args, client) {
-		await message.delete();
 		if (args[0] > 100) return message.reply('You can only clear 100 messages at once!');
 		await message.channel.messages.fetch({ limit: args[0] }).then(messages => {
 			message.channel.bulkDelete(messages).catch(e => message.channel.send(`\`${`${e}`.split('at')[0]}\``));
