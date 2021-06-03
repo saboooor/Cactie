4c6b5d81a218ba06a14221bb8372e6adcca59cc1
saboooor <saboor547@gmail.com>
2021-06-02 18:32:07 -0600

    add /boner and /instaboner

--- commands/user/fun/boner.js
+++ commands/user/fun/boner.js
@@ -2,33 +2,50 @@ function sleep(ms) { return new Promise(res => setTimeout(res, ms)); }
 const Discord = require('discord.js');
 module.exports = {
 	name: 'boner',
-	description: 'See your pp grow',
+	description: 'See your pp grow!',
 	aliases: ['pp', 'penis', 'erect'],
 	cooldown: 10,
+	options: [{
+		type: 3,
+		name: 'someone',
+		description: 'Pick someone for the bot to calculate the pp size of',
+	}],
 	async execute(message, args, client) {
 		if (client.settings.get(message.guild.id).bonercmd == 'false') return message.reply('This command is disabled!');
+		if (message.commandName) args.forEach(arg => args[args.indexOf(arg)] = arg.value);
 		const srvconfig = client.settings.get(message.guild.id);
 		const random = Math.round(Math.random() * srvconfig.maxppsize);
-		const nick = message.member.displayName;
+		let nick = message.member.displayName;
+		if (args[0]) {
+			nick = args[0];
+			if (nick.startsWith('<@') && nick.endsWith('>')) {
+				let mention = nick.slice(2, -1);
+				if (mention.startsWith('!')) mention = mention.slice(1);
+				nick = client.users.cache.get(mention).username;
+			}
+		}
 		const Embed = new Discord.MessageEmbed()
 			.setColor(Math.round(Math.random() * 16777215))
 			.setTitle(`${nick}'s pp size`)
 			.setDescription('<a:loading:826611946258038805> Calculating...');
-		const pp = await message.channel.send(Embed);
+		const pp = await message.reply(Embed);
 		const shaft = [];
 		for (let step = 0; step < random; step++) {
 			await sleep(1200);
 			Embed.setDescription('8' + shaft.join('') + 'D');
-			await pp.edit (Embed);
+			if (!message.commandName) pp.edit(Embed);
+			else message.editReply(Embed);
 			shaft.push('=');
 		}
 		const sike = Math.round(Math.random() * 10);
 		if (sike == 5) {
 			Embed.setDescription('SIKE').setFooter(`${nick} has no pp`);
-			pp.edit(Embed);
+			if (!message.commandName) pp.edit(Embed);
+			else message.editReply(Embed);
 			return;
 		}
 		Embed.setFooter(`pp size = ${random}"`);
-		pp.edit(Embed);
+		if (!message.commandName) pp.edit(Embed);
+		else message.editReply(Embed);
 	},
 };
--- commands/user/fun/instaboner.js
+++ commands/user/fun/instaboner.js
@@ -1,13 +1,27 @@
 const Discord = require('discord.js');
 module.exports = {
 	name: 'instaboner',
-	description: 'See your pp grow FAST',
+	description: 'See your pp grow FAST!',
 	aliases: ['instapp', 'instapenis', 'instaerect'],
 	cooldown: 1,
+	options: [{
+		type: 3,
+		name: 'someone',
+		description: 'Pick someone for the bot to calculate the pp size of',
+	}],
 	async execute(message, args, client) {
 		if (client.settings.get(message.guild.id).bonercmd == 'false') return message.reply('This command is disabled!');
+		if (message.commandName) args.forEach(arg => args[args.indexOf(arg)] = arg.value);
 		const srvconfig = client.settings.get(message.guild.id);
-		const nick = message.member.displayName;
+		let nick = message.member.displayName;
+		if (args[0]) {
+			nick = args[0];
+			if (nick.startsWith('<@') && nick.endsWith('>')) {
+				let mention = nick.slice(2, -1);
+				if (mention.startsWith('!')) mention = mention.slice(1);
+				nick = client.users.cache.get(mention).username;
+			}
+		}
 		const hard = Math.round(Math.random());
 		let hardtxt = 'soft';
 		if (hard == '1') hardtxt = 'hard';
@@ -16,11 +30,11 @@ module.exports = {
 			.setTitle(`${nick}'s ${hardtxt} pp size`);
 		if (Math.round(Math.random() * 10) == 5) {
 			Embed.setDescription('SIKE').setFooter(`${nick} has no pp`);
-			message.channel.send(Embed);
+			message.reply(Embed);
 			return;
 		}
 		const random = Math.round(Math.random() * srvconfig.maxppsize);
 		Embed.setDescription('8' + '='.repeat(random - 1) + 'D').setFooter(`${hardtxt} pp size = ${random}"`);
-		message.channel.send(Embed);
+		message.reply(Embed);
 	},
 };
