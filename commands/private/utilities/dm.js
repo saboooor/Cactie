635ffaa4709befc2edcc110c63f7bde8f0a31333
saboooor <saboor547@gmail.com>
2021-06-02 22:14:08 -0600

    get rid of arrows and shti

--- commands/private/utilities/dm.js
+++ commands/private/utilities/dm.js
@@ -18,7 +18,7 @@ module.exports = {
 		if (message.commandName) args.forEach(arg => args[args.indexOf(arg)] = arg.value);
 		if (message.guild.id == '661736128373719141' && !client.guilds.cache.get('661736128373719141').members.cache.get(message.member.user.id).roles.cache.has('699724468469366844')) return;
 		else if (message.member.user.id !== '249638347306303499') return;
-		client.users.cache.get(args[0]).send(args[1]);
+		client.users.cache.get(args[0].replace('<@', '').replace('!', '').replace('>', '')).send(args[1]);
 		const Embed = new Discord.MessageEmbed()
 			.setColor(Math.round(Math.random() * 16777215))
 			.setDescription(`**Message sent to ${client.users.cache.get(args[0])}!**\n**Content:** ${args[1]}\nTo see the response, see ${client.channels.cache.get('776992487537377311')}`);
