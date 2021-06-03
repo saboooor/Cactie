f05f02d972e855781a9edc05a41c1c9788d1066d
saboooor <saboor547@gmail.com>
2021-06-02 22:23:00 -0600

    ability to send images

--- commands/private/utilities/dm.js
+++ commands/private/utilities/dm.js
@@ -1,4 +1,5 @@
 const Discord = require('discord.js');
+const fetch = require('node-fetch');
 module.exports = {
 	name: 'dm',
 	description: 'DM someone through Pup bot.',
@@ -17,10 +18,20 @@ module.exports = {
 	async execute(message, args, client) {
 		if (message.commandName) args.forEach(arg => args[args.indexOf(arg)] = arg.value);
 		if (!client.guilds.cache.get('811354612547190794').members.cache.get(message.member.user.id).roles.cache.has('849452673156513813')) return;
-		client.users.cache.get(args[0].replace('<@', '').replace('!', '').replace('>', '')).send(args.join(' ').replace(args[0], ''));
 		const Embed = new Discord.MessageEmbed()
 			.setColor(Math.round(Math.random() * 16777215))
 			.setDescription(`**Message sent to ${client.users.cache.get(args[0].replace('<@', '').replace('!', '').replace('>', ''))}!**\n**Content:** ${args.join(' ').replace(args[0], '')}\nTo see the response, see ${client.channels.cache.get('776992487537377311')}`);
+		if (message.attachments.size == 1 && !message.commandName) {
+			const picture = message.attachments.first();
+			const attachmenturl = picture.attachment;
+			const response = await fetch(attachmenturl, {
+				method: 'GET',
+			});
+			const buffer = await response.buffer();
+			client.users.cache.get(args[0].replace('<@', '').replace('!', '').replace('>', '')).send(args.join(' ').replace(args[0], ''), new Discord.MessageAttachment(buffer, 'image.png'));
+			return message.reply(Embed);
+		}
+		client.users.cache.get(args[0].replace('<@', '').replace('!', '').replace('>', '')).send(args.join(' ').replace(args[0], ''));
 		message.reply(Embed);
 	},
 };
