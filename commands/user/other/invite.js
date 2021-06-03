7715442491fe6be4f8504f054773a6cd3aa32252
saboooor <saboor547@gmail.com>
2021-06-03 02:40:08 -0600

    pup

--- commands/user/other/invite.js
+++ commands/user/other/invite.js
@@ -8,10 +8,10 @@ module.exports = {
 		const srvconfig = client.settings.get(message.guild.id);
 		const Embed = new Discord.MessageEmbed()
 			.setColor(Math.round(Math.random() * 16777215))
-			.addField('**Add the bot:**', '[Invite the bot to your server using this link!](https://discord.com/api/oauth2/authorize?client_id=765287593762881616&permissions=2184703062&scope=bot%20applications.commands)')
-			.addField('**Add the bot (Admin):**', '[Invite the bot to your server using this link!](https://discord.com/api/oauth2/authorize?client_id=765287593762881616&permissions=8&scope=bot%20applications.commands)')
-			.addField('**Add the bot (Dev):**', '[Invite the bot to your server using this link!](https://discord.com/api/oauth2/authorize?client_id=848775888673439745&permissions=8&scope=applications.commands%20bot)')
-			.addField('**Bot Support:**', '[Join the bot\'s discord server!](https://discord.gg/Bsefgbaedz)');
+			.addField('**Add the bot:**', '[Invite Pup to your server using this link!](https://discord.com/api/oauth2/authorize?client_id=765287593762881616&permissions=2184703062&scope=bot%20applications.commands)')
+			.addField('**Add the bot (Admin):**', '[Invite Pup to your server using this link!](https://discord.com/api/oauth2/authorize?client_id=765287593762881616&permissions=8&scope=bot%20applications.commands)')
+			.addField('**Add the bot (Dev):**', '[Invite Pup Dev to your server using this link!](https://discord.com/api/oauth2/authorize?client_id=848775888673439745&permissions=8&scope=applications.commands%20bot)')
+			.addField('**Bot Support:**', '[Join Pup\'s discord server!](https://discord.gg/Bsefgbaedz)');
 		if (srvconfig.adfree == 'false') Embed.addField('**Nether Depths:**', '[Also check out Nether Depths!](https://discord.gg/g7hSukX)');
 		await message.reply(Embed);
 	},
