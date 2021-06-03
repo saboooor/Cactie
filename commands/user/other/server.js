e1f1a4447bfe4f73879b39c1e506d7db10250b0a
saboooor <saboor547@gmail.com>
2021-06-02 18:00:23 -0600

    add /server

--- commands/user/other/server.js
+++ commands/user/other/server.js
@@ -5,14 +5,18 @@ module.exports = {
 	description: 'Discord server info',
 	aliases: ['info', 'srv', 'guild'],
 	cooldown: 10,
+	guildOnly: true,
 	async execute(message, args, client) {
 		const Embed = new Discord.MessageEmbed()
 			.setColor(Math.round(Math.random() * 16777215))
 			.setAuthor(message.guild.name, message.guild.iconURL())
-			.setFooter(`Owner: ${client.users.cache.get(message.guild.ownerID).username}`, client.users.cache.get(message.guild.ownerID).avatarURL())
-			.addField('Members', message.guild.members.cache.size)
-			.addField('Channels', message.guild.channels.cache.size)
-			.addField('Creation Date', moment(message.guild.createdAt));
-		await message.channel.send(Embed);
+			.setFooter(`Owner: ${client.users.cache.get(message.guild.ownerID).username}`, client.users.cache.get(message.guild.ownerID).avatarURL());
+		if (message.guild.description) Embed.addField('Description', message.guild.description);
+		if (message.guild.vanityURLCode) Embed.addField('Vanity URL', `discord.gg/${message.guild.vanityURLCode}`);
+		Embed
+			.addField('Members', `${message.guild.memberCount}`)
+			.addField('Channels', `${message.guild.channels.cache.size}`)
+			.addField('Creation Date', `${moment(message.guild.createdAt)}`);
+		await message.reply(Embed);
 	},
 };
