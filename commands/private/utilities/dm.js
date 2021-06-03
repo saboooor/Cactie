f7719af61b084c573ca80e10c571e77c447051b0
saboooor <saboor547@gmail.com>
2021-06-02 22:19:13 -0600

    args dumb

--- commands/private/utilities/dm.js
+++ commands/private/utilities/dm.js
@@ -17,10 +17,10 @@ module.exports = {
 	async execute(message, args, client) {
 		if (message.commandName) args.forEach(arg => args[args.indexOf(arg)] = arg.value);
 		if (!client.guilds.cache.get('811354612547190794').members.cache.get(message.member.user.id).roles.cache.has('849452673156513813')) return;
-		client.users.cache.get(args[0].replace('<@', '').replace('!', '').replace('>', '')).send(args[1]);
+		client.users.cache.get(args[0].replace('<@', '').replace('!', '').replace('>', '')).send(args.join(' ').replace(args[0], ''));
 		const Embed = new Discord.MessageEmbed()
 			.setColor(Math.round(Math.random() * 16777215))
-			.setDescription(`**Message sent to ${client.users.cache.get(args[0].replace('<@', '').replace('!', '').replace('>', ''))}!**\n**Content:** ${args[1]}\nTo see the response, see ${client.channels.cache.get('776992487537377311')}`);
+			.setDescription(`**Message sent to ${client.users.cache.get(args[0].replace('<@', '').replace('!', '').replace('>', ''))}!**\n**Content:** ${args.join(' ').replace(args[0], '')}\nTo see the response, see ${client.channels.cache.get('776992487537377311')}`);
 		message.reply(Embed);
 	},
 };
