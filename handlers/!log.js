34654b74b6b42d79680a2c024c3a380791957220
saboooor <saboor547@gmail.com>
2021-06-04 02:38:32 -0600

    error handling?

--- handlers/!log.js
+++ handlers/!log.js
@@ -5,13 +5,23 @@ function minTwoDigits(n) { return (n < 10 ? '0' : '') + n; }
 module.exports = client => {
 	client.logger = winston.createLogger({
 		format: winston.format.combine(
+			winston.format.colorize(),
 			winston.format.timestamp(),
-			winston.format.printf(log => `[${log.timestamp.split('T')[1].split('.')[0]} ${log.level.toUpperCase()}]: ${log.message}`),
+			winston.format.printf(log => `[${log.timestamp.split('T')[1].split('.')[0]} ${log.level}]: ${log.message}`),
 		),
 		transports: [
 			new winston.transports.Console(),
 			new winston.transports.File({ filename: `logs/${date}.log` }),
 		],
+		exceptionHandlers: [
+			new winston.transports.Console(),
+			new winston.transports.File({ filename: `logs/${date}.log` }),
+		],
+		rejectionHandlers: [
+			new winston.transports.Console(),
+			new winston.transports.File({ filename: `logs/${date}.log` }),
+		],
+		exitOnError: false,
 	});
 	client.logger.info('Logger started!');
 };
