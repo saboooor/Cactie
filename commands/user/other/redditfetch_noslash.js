16b382ae2d645b8ef8eb7126c83693982413fc34
saboooor <saboor547@gmail.com>
2021-06-07 05:35:52 -0600

    return if not found and add description

--- commands/user/other/redditfetch_noslash.js
+++ commands/user/other/redditfetch_noslash.js
@@ -3,12 +3,14 @@ const Discord = require('discord.js');
 module.exports = async (subreddit, message) => {
 	const json = await fetch(`https://www.reddit.com/r/${subreddit}/random.json`);
 	const pong = await json.json();
+	if (pong.message == 'Not Found') return message.reply('Invalid subreddit!');
 	const Embed = new Discord.MessageEmbed()
 		.setAuthor(`u/${pong[0].data.children[0].data.author}`)
 		.setTitle(`${pong[0].data.children[0].data.title} (${pong[0].data.children[0].data.ups} Upvotes)`)
 		.setURL(`https://reddit.com${pong[0].data.children[0].data.permalink}`)
+		.setDescription(pong[0].data.children[0].data.selftext)
 		.setImage(pong[0].data.children[0].data.url)
-		.setFooter(`Fetched from r/${pong[0].data.children[0].data.subreddit}, Pup is not responsible for any of these images`);
+		.setFooter(`Fetched from r/${pong[0].data.children[0].data.subreddit}, Pup is not responsible for any of these posts`);
 	if (!message.channel.nsfw && pong[0].data.children[0].data.over_18) return message.react('🔞');
 	await message.reply(Embed);
 };
