fc8f18a6c5845677d99d97ae1d5c17ee5e0bf144
saboooor <saboor547@gmail.com>
2021-06-07 04:21:50 -0600

    exempt all commands in nsfw from slash

--- handlers/slash.js
+++ handlers/slash.js
@@ -6,7 +6,7 @@ module.exports = client => {
 	for (const folder of slashcommandFolders) {
 		const slashcommandFolders2 = fs.readdirSync(`./commands/${folder}`);
 		for (const folder2 of slashcommandFolders2) {
-			const slashcommandFiles = fs.readdirSync(`./commands/${folder}/${folder2}`).filter(file => file.endsWith('.js') && !file.endsWith('_noslash.js'));
+			const slashcommandFiles = fs.readdirSync(`./commands/${folder}/${folder2}`).filter(file => file.endsWith('.js') && !file.endsWith('_noslash.js') && folder2 == 'nsfw');
 			for (const file of slashcommandFiles) {
 				const slashcommand = require(`../commands/${folder}/${folder2}/${file}`);
 				client.slashcommands.set(slashcommand.name, slashcommand);
