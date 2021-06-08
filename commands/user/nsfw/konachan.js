6ba7e3e07bd0ce55f87fc2bea89b6978d50ca8ae
saboooor <saboor547@gmail.com>
2021-06-08 01:35:36 -0600

    e

--- commands/user/nsfw/konachan.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-	name: 'konachan',
-	description: 'nsfw',
-	cooldown: 1,
-	async execute(message, args, client) {
-		require('../other/redditfetch_noslash.js')('konachan', message);
-	},
-};
