77a7f9323afaf34f27ac589a7fed039caf2521c5
saboooor <saboor547@gmail.com>
2021-06-14 06:45:24 -0600

    dumb

--- commands/private/eval_noslash.js
+++ commands/private/eval_noslash.js
@@ -1,6 +1,6 @@
 function clean(text) {
 	if (typeof (text) === 'string') {return text.replace(/`/g, '`' + String.fromCharCode(8203)).replace(/@/g, '@' + String.fromCharCode(8203));}
-	else {return text;}
+	else { return text; }
 }
 module.exports = {
 	name: 'eval',
@@ -13,9 +13,7 @@ module.exports = {
 		try {
 			const code = args.join(' ');
 			let evaled = eval(code);
-
-			if (typeof evaled !== 'string') {evaled = require('util').inspect(evaled);}
-
+			if (typeof evaled !== 'string') { evaled = require('util').inspect(evaled); }
 			message.channel.send(clean(evaled), { code: 'xl' });
 		}
 		catch (err) {
