2bd3c250e551d810fa7e536a78fe06c30510d524
saboooor <saboor547@gmail.com>
2021-06-14 06:26:03 -0600

    use ternary operator instead of if/else in instapp

--- commands/fun/instaboner.js
+++ commands/fun/instaboner.js
@@ -16,25 +16,19 @@ module.exports = {
 			args.forEach(arg => args[args.indexOf(arg)] = arg[1].value);
 		}
 		const srvconfig = client.settings.get(message.guild.id);
-		let nick = message.member.displayName;
-		if (args[0]) {
-			nick = args[0];
-			if (nick.startsWith('<@') && nick.endsWith('>')) {
-				let mention = nick.slice(2, -1);
-				if (mention.startsWith('!')) mention = mention.slice(1);
-				nick = client.users.cache.get(mention).username;
-			}
+		let nick = args[0] ? args[0] : message.member.displayName;
+		if (args[0] && nick.startsWith('<@') && nick.endsWith('>')) {
+			const mention = nick.includes('!') ? nick.slice(3, -1) : nick.slice(2, -1);
+			nick = client.users.cache.get(mention).username;
 		}
 		const hard = Math.round(Math.random());
-		let hardtxt = 'soft';
-		if (hard == '1') hardtxt = 'hard';
+		const hardtxt = hard == 1 ? 'soft' : 'hard';
 		const Embed = new Discord.MessageEmbed()
 			.setColor(Math.round(Math.random() * 16777215))
 			.setTitle(`${nick}'s ${hardtxt} pp size`);
 		if (Math.round(Math.random() * 10) == 5) {
 			Embed.setDescription('SIKE').setFooter(`${nick} has no pp`);
-			message.reply(Embed);
-			return;
+			return message.reply(Embed);
 		}
 		const random = Math.round(Math.random() * srvconfig.maxppsize);
 		Embed.setDescription('8' + '='.repeat(random - 1) + 'D').setFooter(`${hardtxt} pp size = ${random}"`);
