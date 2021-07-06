f37d3e8c6247122851dd0590e23f67b0a1e50553
saboooor <saboor547@gmail.com>
2021-07-06 10:46:04 -0600

    update -cl

--- commands/private/changelog_noslash.js
+++ commands/private/changelog_noslash.js
@@ -18,6 +18,9 @@ module.exports = {
 				Embed.setDescription(change);
 				client.channels.cache.get('699175256954241095').send({ embeds: [Embed] });
 				client.channels.cache.get('676570220298633237').send({ content: `bcast &4&lNEW UPDATE &#444444•&c ${change}` });
+				client.channels.cache.get('860896125363552326').send({ content: `bcast &4&lNEW UPDATE &#444444•&c ${change}` });
+				client.channels.cache.get('861102381156794418').send({ content: `bcast &4&lNEW UPDATE &#444444•&c ${change}` });
+				client.channels.cache.get('859918682658439199').send({ content: `bcast &4&lNEW UPDATE &#444444•&c ${change}` });
 			});
 		}
 		else if (args[0] == 'th') {
@@ -27,7 +30,6 @@ module.exports = {
 				Embed.setColor(Math.floor(Math.random() * 16777215));
 				Embed.setDescription(change);
 				client.channels.cache.get('717514613930983445').send({ embeds: [Embed] });
-				client.channels.cache.get('717821332695416843').send({ embeds: [Embed] });
 				client.channels.cache.get('744713171067207711').send({ content: `bcast &6[&4&lNEW UPDATE&6]&c ${change}` });
 			});
 		}
