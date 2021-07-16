906d3e7a7cee5fd276539a8528aeda66a82511f0
saboooor <saboor547@gmail.com>
2021-07-16 07:00:48 -0600

    fix

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,9 +1,10 @@
-module.exports = (client, member) => {
+module.exports = async (client, member) => {
 	const srvconfig = client.settings.get(member.guild.id);
 	if (srvconfig.joinmessage == 'false') return;
 	if (!member.guild.systemChannel) {
-		client.logger.warn(`${member.guild.name} (${member.guild.fetchOwner().tag}) has misconfigured join messages!`);
-		return member.guild.fetchOwner().send({ content: `Join messages are enabled but a system message channel isn't set! Please either go into your server settings (${member.guild.name}) and set the system message channel or turn off join messages with the command \`${srvconfig.prefix}settings joinmessage false\`` });
+		const owner = await member.guild.fetchOwner();
+		client.logger.warn(`${member.guild.name} (${owner.tag}) has misconfigured join messages!`);
+		return owner.send({ content: `Join messages are enabled but a system message channel isn't set! Please either go into your server settings (${member.guild.name}) and set the system message channel or turn off join messages with the command \`${srvconfig.prefix}settings joinmessage false\`` });
 	}
 	member.guild.systemChannel.send({ content: srvconfig.joinmessage.replace(/{USER MENTION}/g, client.users.cache.get(member.id)).replace(/{USER TAG}/g, client.users.cache.get(member.id).tag) });
 };
--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -1,9 +1,10 @@
-module.exports = (client, member) => {
+module.exports = async (client, member) => {
 	const srvconfig = client.settings.get(member.guild.id);
 	if (srvconfig.leavemessage == 'false') return;
 	if (!member.guild.systemChannel) {
-		client.logger.warn(`${member.guild.name} (${member.guild.fetchOwner().tag}) has misconfigured leave messages!`);
-		return member.guild.fetchOwner().send({ content: `Leave messages are enabled but a system message channel isn't set! Please either go into your server settings (${member.guild.name}) and set the system message channel or turn off leave messages with the command \`${srvconfig.prefix}settings leavemessage false\`` });
+		const owner = await member.guild.fetchOwner();
+		client.logger.warn(`${member.guild.name} (${owner.tag}) has misconfigured leave messages!`);
+		return owner.send({ content: `Leave messages are enabled but a system message channel isn't set! Please either go into your server settings (${member.guild.name}) and set the system message channel or turn off leave messages with the command \`${srvconfig.prefix}settings leavemessage false\`` });
 	}
 	member.guild.systemChannel.send({ content: srvconfig.leavemessage.replace(/{USER MENTION}/g, client.users.cache.get(member.id)).replace(/{USER TAG}/g, client.users.cache.get(member.id).tag) });
 };
