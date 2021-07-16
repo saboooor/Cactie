188c00e65f72b271a34d043af9b4b66c8187c2aa
saboooor <saboor547@gmail.com>
2021-07-15 18:50:27 -0600

    add specific build and error handling

--- commands/utilities/purpur.js
+++ commands/utilities/purpur.js
@@ -1,19 +1,20 @@
 const Discord = require('discord.js');
 require('moment-duration-format');
 const moment = require('moment');
-const hastebin = require('hastebin');
-const nodeactyl = require('nodeactyl');
 const fetch = require('node-fetch');
-const servers = require('../../config/pterodactyl.json');
-const protocols = require('../../config/mcprotocol.json');
 module.exports = {
 	name: 'purpur',
-	description: 'Get the latest Purpur build',
-	usage: '[Version]',
+	description: 'Get info on the latest Purpur build',
+	usage: '[Version] [Build]',
 	options: [{
 		type: 3,
 		name: 'version',
 		description: 'Specify a Minecraft version',
+	},
+	{
+		type: 3,
+		name: 'build',
+		description: 'Specify a Purpur build number',
 	}],
 	async execute(message, args, client) {
 		if (message.type && message.type == 'APPLICATION_COMMAND') {
@@ -23,21 +24,25 @@ module.exports = {
 		// fetch the latest mc version
 		const a = await fetch('https://api.pl3x.net/v2/purpur');
 		const b = await a.json();
-		// fetch the latest build for mc version specified or latest
+		// if specified args are valid then replace latest with that number
 		const c = args[0] ? args[0] : b.versions[0];
-		const d = await fetch(`https://api.pl3x.net/v2/purpur/${c}/latest`);
-		const e = await d.json();
+		const d = args[1] ? args[1] : 'latest';
+		// fetch the latest build for mc / build versions specified or latest
+		const e = await fetch(`https://api.pl3x.net/v2/purpur/${c}/${d}`);
+		const f = await e.json();
+		// check if error
+		if (f.error) return message.reply(f.error.message);
 		// initial embed creation
 		const Embed = new Discord.MessageEmbed()
 			.setColor(9790364)
-			.setTitle(`Purpur ${c} build ${e.build} (${e.result})`)
+			.setTitle(`Purpur ${f.version} build ${f.build} (${f.result})`)
 			.setThumbnail('https://cdn.discordapp.com/attachments/742476351012864162/865391752675065896/purpur.png')
-			.setDescription(`${e.commits.length} commit(s)`)
-			.setFooter(`${moment(e.timestamp)}`);
+			.setDescription(`${f.commits.length} commit(s)`)
+			.setFooter(`${moment(f.timestamp)}`);
 		// add fields for commits
-		e.commits.forEach(commit => { Embed.addField(commit.author, `${commit.description}\n*${moment(commit.timestamp)}*`); });
-		// add a field for download link
-		Embed.addField('Download', `[Click Here](https://api.pl3x.net/v2/purpur/${c}/latest/download)`);
+		f.commits.forEach(commit => { Embed.addField(commit.author, `${commit.description}\n*${moment(commit.timestamp)}*`); });
+		// add field for download
+		Embed.addField('Download', `[Click Here](https://api.pl3x.net/v2/purpur/${c}/${d}/download)`);
 		// send embed
 		message.reply({ embeds: [Embed] });
 	},
