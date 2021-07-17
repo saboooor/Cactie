526e1b4c1fc84fe1a2e31be1f4e0380d2781714d
saboooor <saboor547@gmail.com>
2021-07-17 13:48:58 -0600

    update -invite

--- commands/utilities/invite.js
+++ commands/utilities/invite.js
@@ -8,9 +8,8 @@ module.exports = {
 		const srvconfig = client.settings.get(message.guild.id);
 		const Embed = new Discord.MessageEmbed()
 			.setColor(Math.round(Math.random() * 16777215))
-			.addField('**Add the bot:**', '[Invite Pup to your server using this link!](https://discord.com/api/oauth2/authorize?client_id=765287593762881616&permissions=2184703062&scope=bot%20applications.commands)')
+			.addField('**Add the bot:**', '[Invite Pup to your server using this link!](https://discord.com/api/oauth2/authorize?client_id=765287593762881616&permissions=2416307446&scope=applications.commands%20bot)')
 			.addField('**Add the bot (Admin):**', '[Invite Pup to your server using this link!](https://discord.com/api/oauth2/authorize?client_id=765287593762881616&permissions=8&scope=bot%20applications.commands)')
-			.addField('**Add the bot (Dev):**', '[Invite Pup Dev to your server using this link!](https://discord.com/api/oauth2/authorize?client_id=848775888673439745&permissions=8&scope=applications.commands%20bot)')
 			.addField('**Bot Support:**', '[Join Pup\'s discord server!](https://discord.gg/Bsefgbaedz)');
 		if (srvconfig.adfree == 'false') Embed.addField('**Nether Depths:**', '[Also check out Nether Depths!](https://discord.gg/g7hSukX)');
 		await message.reply({ embeds: [Embed] });
