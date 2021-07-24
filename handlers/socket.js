40a49dfa6a0776e006cf698915f09db673d5fe5f
saboooor <saboor547@gmail.com>
2021-07-24 00:39:33 -0400

    I WANNA DIE

---
+++ handlers/socket.js
@@ -0,0 +1,16 @@
+const nodeactyl = require('nodeactyl');
+const srv = require('../config/pterodactyl.json')['nether depths bungee'];
+const WebSocket = require('ws');
+module.exports = async client => {
+	const Client = new nodeactyl.NodeactylClient(srv.url, srv.apikey);
+	const socket = await Client.getConsoleWebSocket(srv.id);
+	const ws = new WebSocket(socket.socket, {
+		origin: 'https://panel.birdflop.com',
+		headers : {
+			Authorization: 'Bearer ' + socket.token,
+		},
+	});
+	ws.on('message', message => {
+		console.log(message);
+	});
+};
