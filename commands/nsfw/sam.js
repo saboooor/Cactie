00300f59c3c1dfa8609718876e1ce74c5768fde8
Saboor <42164502+saboooor@users.noreply.github.com>
2021-07-23 23:55:37 -0600

    kill sam

--- commands/nsfw/sam.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-	name: 'sam',
-	description: 'nsfw',
-	async execute(message, args, client) {
-		require('../private/redditfetch_noslash.js')('SamsungGirlr34', message, client);
-	},
-};
