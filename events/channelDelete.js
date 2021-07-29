23a60c0d6ccb619c1000b22df609037592ce46a8
saboooor <saboor547@gmail.com>
2021-07-28 21:10:06 -0600

    oop

--- events/channelDelete.js
+++ events/channelDelete.js
@@ -1,5 +1,5 @@
 module.exports = (client, channel) => {
-	if (client.tickets.get(channel.id) && client.tickets.get(channel.id).voiceticket) {
+	if (client.tickets.get(channel.id) && client.tickets.get(channel.id).voiceticket !== 'false') {
 		const voiceticket = channel.guild.channels.cache.get(client.tickets.get(channel.id).voiceticket);
 		voiceticket.delete();
 		client.tickets.set(channel.id, 'false', 'voiceticket');
