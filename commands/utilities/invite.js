b75c60afbe9e2fc630dcff7506d978d56913fe80
saboooor <saboor547@gmail.com>
2021-09-04 22:45:09 -0600

    fix discord invites

--- commands/utilities/invite.js
+++ commands/utilities/invite.js
@@ -9,8 +9,8 @@ module.exports = {
 			.setColor(Math.round(Math.random() * 16777215))
 			.addField('**Add the bot:**', '[Invite Pup to your server using this link!](https://pup.smhsmh.club/invite)')
 			.addField('**Add the bot (Admin):**', '[Invite Pup to your server using this link!](https://pup.smhsmh.club/admin)')
-			.addField('**Bot Support:**', '[Join Pup\'s discord server!](https://pup.smhsmh.club/invite)')
-			.addField('**Nether Depths:**', '[Also check out Nether Depths!](https://netherdepths.com/invite)');
+			.addField('**Bot Support:**', '[Join Pup\'s discord server!](https://pup.smhsmh.club/discord)')
+			.addField('**Nether Depths:**', '[Also check out Nether Depths!](https://netherdepths.com/discord)');
 		await message.reply({ embeds: [Embed] });
 	},
 };
