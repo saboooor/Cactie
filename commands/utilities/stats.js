8292caeaa8df3b3cccd6da2c7c5c01db4bcbf12d
saboooor <saboor547@gmail.com>
2021-09-19 19:10:20 -0600

    deferreply

--- commands/utilities/stats.js
+++ commands/utilities/stats.js
@@ -21,7 +21,7 @@ module.exports = {
 			args = args._hoistedOptions;
 			args.forEach(arg => args[args.indexOf(arg)] = arg.value);
 		}
-		const reply = message.type && message.type == 'APPLICATION_COMMAND' ? await message.defer() : await message.reply({ content: '<a:loading:826611946258038805> Pup is thinking...' });
+		const reply = message.type && message.type == 'APPLICATION_COMMAND' ? await message.deferReply() : await message.reply({ content: '<a:loading:826611946258038805> Pup is thinking...' });
 		if (!args[0]) args = ['pup'];
 		const srvs = [];
 		Object.keys(servers).map(i => { srvs.push(servers[i]); });
