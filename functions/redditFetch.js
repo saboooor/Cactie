06118634bcd77bcf639c9683f332996411e3a9d7
saboooor <saboor547@gmail.com>
2021-09-19 19:36:51 -0600

    add accent colors to redditFetch

--- functions/redditFetch.js
+++ functions/redditFetch.js
@@ -1,6 +1,9 @@
 const fetch = require('node-fetch');
 const Discord = require('discord.js');
+const splashy = require('splashy');
+const got = require('got');
 module.exports = async (subreddit, message, client) => {
+	if (message.type && message.type == 'APPLICATION_COMMAND' && !message.deferred) message.deferReply();
 	client.logger.info(`Fetching an image from r/${subreddit}...`);
 	const json = await fetch(`https://www.reddit.com/r/${subreddit}/random.json`);
 	const pong = await json.json().catch(e => {
@@ -12,15 +15,17 @@ module.exports = async (subreddit, message, client) => {
 	if (pong.message == 'Not Found') return message.reply({ content: 'Invalid subreddit!' });
 	if (!pong[0]) return message.reply({ content: 'Couldn\'t get data! Try again later.' });
 	if (pong[0].data.children[0].data.selftext) return require('./redditFetch.js')(subreddit, message, client);
+	client.logger.info(`Image URL: ${pong[0].data.children[0].data.url}`);
+	if (!pong[0].data.children[0].data.url.includes('i.redd.it') && !pong[0].data.children[0].data.url.includes('i.imgur.com')) return require('./redditFetch.js')(subreddit, message, client);
+	if (!message.channel.nsfw && pong[0].data.children[0].data.over_18) return message.react('🔞');
+	const { body } = await got(pong[0].data.children[0].data.url, { encoding: null });
+	const palette = await splashy(body);
 	const Embed = new Discord.MessageEmbed()
-		.setColor(Math.round(Math.random() * 16777215))
+		.setColor(palette[2])
 		.setAuthor(`u/${pong[0].data.children[0].data.author}`)
 		.setTitle(`${pong[0].data.children[0].data.title} (${pong[0].data.children[0].data.ups} Upvotes)`)
 		.setURL(`https://reddit.com${pong[0].data.children[0].data.permalink}`)
 		.setImage(pong[0].data.children[0].data.url)
 		.setFooter(`Fetched from r/${pong[0].data.children[0].data.subreddit}, Pup is not responsible for any of these posts`);
-	client.logger.info(`Image URL: ${pong[0].data.children[0].data.url}`);
-	if (!pong[0].data.children[0].data.url.includes('i.redd.it') && !pong[0].data.children[0].data.url.includes('i.imgur.com')) return require('./redditFetch.js')(subreddit, message, client);
-	if (!message.channel.nsfw && pong[0].data.children[0].data.over_18) return message.react('🔞');
-	await message.reply({ embeds: [Embed] });
+	message.type && message.type == 'APPLICATION_COMMAND' ? message.editReply({ embeds: [Embed] }) : message.reply({ embeds: [Embed] });
 };
