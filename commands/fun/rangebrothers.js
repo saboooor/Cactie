94004568495806a37a36a50b528f2bd098e44030
saboooor <saboor547@gmail.com>
2021-09-22 22:20:16 -0600

    more fixes

--- commands/fun/rangebrothers.js
+++ commands/fun/rangebrothers.js
@@ -9,39 +9,39 @@ module.exports = {
 		description: 'Question the 8ball',
 		required: true,
 	}],
-	async execute(message) {
-		let pp = await message.reply('Hold on');
+	async execute(message, args, client) {
+		let pp = await message.reply('Hold on').catch(e => { return client.logger.error(e); });
 		await sleep(1000);
-		pp = await pp.edit(`${pp.content}\nLet's get this shit`);
+		pp = await pp.edit(`${pp.content}\nLet's get this shit`).catch(e => { return client.logger.error(e); });
 		await sleep(1000);
-		pp = await pp.edit(`${pp.content}\nLet's get this shit`);
+		pp = await pp.edit(`${pp.content}\nLet's get this shit`).catch(e => { return client.logger.error(e); });
 		await sleep(1000);
-		pp = await pp.edit(`${pp.content}\nLet's get this shit`);
+		pp = await pp.edit(`${pp.content}\nLet's get this shit`).catch(e => { return client.logger.error(e); });
 		await sleep(1000);
-		pp = await pp.edit(`${pp.content}\nLet's hmm`);
+		pp = await pp.edit(`${pp.content}\nLet's hmm`).catch(e => { return client.logger.error(e); });
 		await sleep(1000);
-		pp = await pp.edit(`${pp.content}\nTop o da mownin`);
+		pp = await pp.edit(`${pp.content}\nTop o da mownin`).catch(e => { return client.logger.error(e); });
 		await sleep(1000);
-		pp = await pp.edit(`${pp.content}\nTop o da mownin`);
+		pp = await pp.edit(`${pp.content}\nTop o da mownin`).catch(e => { return client.logger.error(e); });
 		await sleep(1000);
-		pp = await pp.edit(`${pp.content}\nTop o da mownin`);
+		pp = await pp.edit(`${pp.content}\nTop o da mownin`).catch(e => { return client.logger.error(e); });
 		await sleep(1000);
-		pp = await pp.edit(`${pp.content}\nTop o da mownin`);
+		pp = await pp.edit(`${pp.content}\nTop o da mownin`).catch(e => { return client.logger.error(e); });
 		await sleep(1000);
-		pp = await pp.edit(`${pp.content}\nTop o da mownin`);
+		pp = await pp.edit(`${pp.content}\nTop o da mownin`).catch(e => { return client.logger.error(e); });
 		await sleep(1000);
-		pp = await pp.edit(`${pp.content}\nTop o da mownin`);
+		pp = await pp.edit(`${pp.content}\nTop o da mownin`).catch(e => { return client.logger.error(e); });
 		await sleep(1000);
-		pp = await pp.edit(`${pp.content}\nTop o da mownin`);
+		pp = await pp.edit(`${pp.content}\nTop o da mownin`).catch(e => { return client.logger.error(e); });
 		await sleep(1000);
-		pp = await pp.edit(`${pp.content}\nHold on`);
+		pp = await pp.edit(`${pp.content}\nHold on`).catch(e => { return client.logger.error(e); });
 		await sleep(1000);
-		pp = await pp.edit(`${pp.content}\nLet's get this shit`);
+		pp = await pp.edit(`${pp.content}\nLet's get this shit`).catch(e => { return client.logger.error(e); });
 		await sleep(1000);
-		pp = await pp.edit(`${pp.content}\nLet's get this shit`);
+		pp = await pp.edit(`${pp.content}\nLet's get this shit`).catch(e => { return client.logger.error(e); });
 		await sleep(1000);
-		pp = await pp.edit(`${pp.content}\nLet's get this shit`);
+		pp = await pp.edit(`${pp.content}\nLet's get this shit`).catch(e => { return client.logger.error(e); });
 		await sleep(1000);
-		pp.edit(`${pp.content}\nLet's hmm`);
+		pp.edit(`${pp.content}\nLet's hmm`).catch(e => { return client.logger.error(e); });
 	},
 };
--- events/music/playerMove.js
+++ events/music/playerMove.js
@@ -1,4 +1,4 @@
 module.exports = async (client, player, oldChannel, newChannel) => {
 	player.voiceChannel = client.channels.cache.get(newChannel);
-	client.logger.info(`Player has been moved in ${player.guild} from ${oldChannel} to ${player.voiceChannel.name}`);
+	client.logger.info(`Player has been moved in ${player.guild} from ${oldChannel} to ${player.voiceChannel ? player.voiceChannel.name : null}`);
 };
