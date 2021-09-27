2459d69aa9d855fa0b28d1a4cf0db9582977e2e3
saboooor <saboor547@gmail.com>
2021-09-26 22:30:14 -0600

    invite

--- commands/utilities/invite.js
+++ commands/utilities/invite.js
@@ -7,9 +7,8 @@ module.exports = {
 	async execute(message) {
 		const Embed = new MessageEmbed()
 			.setColor(Math.round(Math.random() * 16777215))
-			.addField('**Add the bot:**', '[Invite Pup to your server using this link!](https://pup.smhsmh.club/invite)')
-			.addField('**Add the bot (Admin):**', '[Invite Pup to your server using this link!](https://pup.smhsmh.club/admin)')
-			.addField('**Add the experimental Music Bot:**', '[Invite Pup Dev to your server using this link!](https://pup.smhsmh.club/dev)')
+			.addField('**Add Pup Bot to your server:**', '[Invite Pup to your server using this link!](https://pup.smhsmh.club/invite)')
+			.addField('**Add the secondary Pup Dev Bot:**', '[Invite Pup Dev to your server using this link!](https://pup.smhsmh.club/dev)')
 			.addField('**Bot Support:**', '[Join Pup\'s discord server!](https://pup.smhsmh.club/discord)')
 			.addField('**Nether Depths:**', '[Also check out Nether Depths!](https://netherdepths.com/discord)');
 		await message.reply({ embeds: [Embed] });
