173f044b1e4915b62c5a4a083e448371102846c0
saboooor <saboor547@gmail.com>
2021-10-06 19:56:59 -0600

    uhhh

--- commands/utilities/userinfo.js
+++ commands/utilities/userinfo.js
@@ -45,7 +45,7 @@ module.exports = {
 					activitystack.push(`\n\`${convertTime(current)} ${progressbar(total, current, 10, '▬', '🔘')} ${convertTime(total)}\``);
 				}
 				else if (activities[i].timestamps && activities[i].timestamps.start) {
-					activitystack.push(`\n<t:${Date.parse(activities[i].timestamps.start) / 1000}:R>`);
+					activitystack.push(`\n<t:${new Date(activities[i].timestamps.start) / 1000}:R>`);
 				}
 				else if (activities[i].timestamps && activities[i].timestamps.end) {
 					activitystack.push(`\nEnds <t:${Date.parse(activities[i].timestamps.end) / 1000}:R>`);
