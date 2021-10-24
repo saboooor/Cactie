03eb2d5036be7335b0d9acf752484e8933db421e
Saboor <42164502+saboooor@users.noreply.github.com>
2021-10-23 20:23:31 -0600

    more commenting VSCODE ON CHROME ON ANDROID ON WINDOWS LETS FUCKING GOO

--- commands/admin/mute.js
+++ commands/admin/mute.js
@@ -30,10 +30,15 @@ module.exports = {
 
 		// Check if user is muted
 		if (member.roles.cache.has(role.id)) return message.reply({ content: 'This user is already muted! Try unmuting instead.' });
-		const Embed = new MessageEmbed().setColor(Math.round(Math.random() * 16777215));
+
+		// Check if duration is set and if it's more than a year
 		const time = ms(args[1] ? args[1] : 'perm');
 		if (time > 31536000000) return message.reply({ content: 'You cannot mute someone for more than 1 year!' });
+
+		// Create embed and check if duration / reason are set and do stuff
+		const Embed = new MessageEmbed().setColor(Math.round(Math.random() * 16777215));
 		if (!isNaN(time) && args[2]) {
+			// Set embed title and send mute message to user
 			Embed.setTitle(`Muted ${user.tag} for ${args[1]}. Reason: ${args.slice(2).join(' ')}`);
 			await user.send({ content: `**You've been muted on ${message.guild.name} for ${args[1]}. Reason: ${args.slice(2).join(' ')}**` })
 				.catch(e => {
@@ -41,9 +46,12 @@ module.exports = {
 					message.channel.send({ content: 'Could not DM user! You may have to manually let them know that they have been muted.' });
 				});
 			client.logger.info(`Muted user: ${user.tag} on ${message.guild.name} for ${args[1]}. Reason: ${args.slice(2).join(' ')}`);
+
+			// Set member data for unmute time
 			client.memberdata.set(`${user.id}-${message.guild.id}`, Date.now() + time, 'mutedUntil');
 		}
 		else if (!isNaN(time) && args[1]) {
+			// Set embed title and send mute message to user
 			Embed.setTitle(`Muted ${user.tag} for ${args[1]}.`);
 			await user.send({ content: `**You've been muted on ${message.guild.name} for ${args[1]}**` })
 				.catch(e => {
@@ -51,9 +59,12 @@ module.exports = {
 					message.channel.send({ content: 'Could not DM user! You may have to manually let them know that they have been muted.' });
 				});
 			client.logger.info({ content: `Muted user: ${user.tag} on ${message.guild.name} for ${args[1]}` });
+
+			// Set member data for unmute time
 			client.memberdata.set(`${user.id}-${message.guild.id}`, Date.now() + time, 'mutedUntil');
 		}
 		else if (args[1]) {
+			// Set embed title and send mute message to user
 			Embed.setTitle(`Muted ${user.tag} for ${args.slice(1).join(' ')}, forever`);
 			await user.send({ content: `**You've been muted on ${message.guild.name} for ${args.slice(1).join(' ')}**` })
 				.catch(e => {
@@ -63,6 +74,7 @@ module.exports = {
 			client.logger.info(`Muted user: ${user.tag} on ${message.guild.name} for ${args.slice(1).join(' ')} forever`);
 		}
 		else {
+			// Set embed title and send mute message to user
 			Embed.setTitle(`Muted ${user.tag} forever.`);
 			await user.send({ content: `**You've been muted on ${message.guild.name} forever.**` })
 				.catch(e => {
@@ -71,8 +83,12 @@ module.exports = {
 				});
 			client.logger.info(`Muted user: ${user.tag} on ${message.guild.name} forever`);
 		}
+
+		// Actually mute the dude (add role)
 		await member.roles.add(role)
 			.catch(e => message.channel.send({ content: `\`${`${e}`.split('at')[0]}\`` }));
+
+		// Reply to command
 		message.reply({ embeds: [Embed], ephemeral: true });
 	},
 };
