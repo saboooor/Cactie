b5630d559f6b62a0f1237ba0d27a38c7d97f355d
saboooor <saboor547@gmail.com>
2021-11-14 13:57:48 -0700

    add mute handling for when muted user rejoins

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -6,5 +6,7 @@ module.exports = async (client, member) => {
 		client.logger.warn(`${member.guild.name} (${owner.tag}) has misconfigured join messages!`);
 		return owner.send({ content: `Join messages are enabled but a system message channel isn't set! Please either go into your server settings (${member.guild.name}) and set the system message channel or turn off join messages with the command \`${srvconfig.prefix}settings joinmessage false\`` }).catch(e => { client.logger.warn(e); });
 	}
+	const muterole = member.guild.roles.cache.get(srvconfig.muterole);
+	if (client.memberdata.get(member.user.id).mutedUntil) member.roles.add(muterole);
 	member.guild.systemChannel.send({ content: srvconfig.joinmessage.replace(/{USER MENTION}/g, client.users.cache.get(member.id)).replace(/{USER TAG}/g, client.users.cache.get(member.id).tag) });
 };
