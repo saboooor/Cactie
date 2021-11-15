736ba3fb095ad47df98a8e9a36ce80c0f9712fbe
saboooor <saboor547@gmail.com>
2021-11-14 20:50:57 -0700

    commenting

--- commands/fun/color.js
+++ commands/fun/color.js
@@ -8,9 +8,14 @@ module.exports = {
 	usage: '<Image URL>',
 	options: require('../options/imgurl.json'),
 	async execute(message, args) {
+		// Check if command is a slash command and defer accordingly
 		const slash = message.type && message.type == 'APPLICATION_COMMAND';
 		const reply = slash ? await message.deferReply() : await message.reply({ content: '<a:loading:826611946258038805> Pup is thinking...' });
+
+		// Get the color from the img url
 		const color = rgb2hex(await getColor(args[0]));
+
+		// Create embed with color and reply with it
 		const Embed = new MessageEmbed()
 			.setColor(color)
 			.setDescription(color);
--- commands/fun/rate.js
+++ commands/fun/rate.js
@@ -5,7 +5,10 @@ module.exports = {
 	usage: '[Something or someone]',
 	options: require('../options/someone.json'),
 	async execute(message, args) {
+		// If arg isn't set, set it to the author's name/nick
 		if (!args[0]) args[0] = message.guild ? message.member.displayName : message.user.username;
+
+		// Get random rating and reply with that
 		const rating = Math.floor(Math.random() * (ratings.length * 10)) / 10;
 		const i = Math.floor(rating);
 		message.reply(ratings[i]
