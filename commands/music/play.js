89ba27a076fe30fc256551ec26590ceba866131b
saboooor <saboor547@gmail.com>
2021-11-16 22:54:35 -0700

    oops

--- commands/music/play.js
+++ commands/music/play.js
@@ -1,7 +1,7 @@
 const { MessageEmbed } = require('discord.js');
 const { TrackUtils } = require('erela.js');
 const { convertTime } = require('../../functions/convert.js');
-const { addsong, playlist, resume } = require('../../config/emoji.json');
+const { addsong, playlist, resume, warn } = require('../../config/emoji.json');
 const { getColor } = require('colorthief');
 const rgb2hex = require('../../functions/rgbhex');
 module.exports = {
@@ -54,7 +54,7 @@ module.exports = {
 				}
 				else {
 					embed.setColor('RED').setDescription('No results found.');
-					return slash ? message.editReply({ content: `${resume} **Found result for \`${search}\`!**`, embeds: [embed] }) : msg.edit({ content: `${resume} **Found result for \`${search}\`!**`, embeds: [embed] });
+					return slash ? message.editReply({ content: `${warn} **Failed to search**`, embeds: [embed] }) : msg.edit({ content: `${resume} **Found result for \`${search}\`!**`, embeds: [embed] });
 				}
 				track.img = 'https://i.imgur.com/cK7XIkw.png';
 			}
@@ -63,7 +63,7 @@ module.exports = {
 				const track = Searched.tracks[0];
 				if (Searched.loadType === 'NO_MATCHES') {
 					embed.setColor('RED').setDescription('No results found.');
-					return slash ? message.editReply({ content: `${resume} **Found result for \`${search}\`!**`, embeds: [embed] }) : msg.edit({ content: `${resume} **Found result for \`${search}\`!**`, embeds: [embed] });
+					return slash ? message.editReply({ content: `${warn} **Failed to search**`, embeds: [embed] }) : msg.edit({ content: `${resume} **Found result for \`${search}\`!**`, embeds: [embed] });
 				}
 				else if (Searched.loadType == 'PLAYLIST_LOADED') {
 					embed.setDescription(`${playlist} **Added Playlist to queue**\n[${Searched.playlist.name}](${search}) \`[${Searched.tracks.length} songs]\` \`[${convertTime(Searched.playlist.duration)}]\` [${message.member.user}]`);
@@ -86,7 +86,7 @@ module.exports = {
 			});
 			player.queue.add(songs);
 			if (!player.playing) player.play();
-			slash ? message.editReply({ content: `${resume} **Found result for \`${search}\`!**`, embeds: [embed] }) : msg.edit({ content: `${resume} **Found result for \`${search}\`!**`, embeds: [embed] });
+			slash ? message.editReply({ content: `${resume} **Found result for \`${search}\`**`, embeds: [embed] }) : msg.edit({ content: `${resume} **Found result for \`${search}\`!**`, embeds: [embed] });
 		}
 		catch (e) {
 			client.logger.error(e);
