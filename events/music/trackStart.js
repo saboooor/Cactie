2b2fa1368a57988ba5990a7eb983c7039d368509
saboooor <saboor547@gmail.com>
2021-11-16 23:41:02 -0700

    look up track img cuz aa

--- events/music/trackStart.js
+++ events/music/trackStart.js
@@ -1,10 +1,20 @@
 const { MessageEmbed } = require('discord.js');
 const { convertTime } = require('../../functions/convert.js');
 const { play } = require('../../config/emoji.json');
+const { getColor } = require('colorthief');
+const rgb2hex = require('../../functions/rgbhex');
 module.exports = async (client, player, track) => {
 	player.skipAmount = null; player.clearQueueAmount = null;
 	player.loopTrackAmount = null; player.loopQueueAmount = null;
 	player.shuffleAmount = null;
+	if (!track.img) {
+		const Searched = await player.search(track.title);
+		const a = Searched.tracks[0];
+		if (a && a.displayThumbnail) {
+			track.img = a.displayThumbnail('hqdefault');
+			track.color = rgb2hex(await getColor(track.img));
+		}
+	}
 	const thing = new MessageEmbed()
 		.setDescription(`${play} **Started Playing**\n [${track.title}](${track.uri}) - \`[${convertTime(track.duration).replace('07:12:56', 'LIVE')}]\` [${track.requester}]`)
 		.setThumbnail(track.img)
