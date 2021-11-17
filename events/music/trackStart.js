d33a4085bccfd92a216a61762b3c1e6dbca7b93b
saboooor <saboor547@gmail.com>
2021-11-16 23:01:57 -0700

    delete last msg

--- events/music/trackStart.js
+++ events/music/trackStart.js
@@ -2,7 +2,6 @@ const { MessageEmbed } = require('discord.js');
 const { convertTime } = require('../../functions/convert.js');
 const { play } = require('../../config/emoji.json');
 module.exports = async (client, player, track) => {
-	const channel = client.channels.cache.get(player.textChannel);
 	player.skipAmount = null; player.clearQueueAmount = null;
 	player.loopTrackAmount = null; player.loopQueueAmount = null;
 	player.shuffleAmount = null;
@@ -12,5 +11,8 @@ module.exports = async (client, player, track) => {
 		.setColor(track.color)
 		.setTimestamp();
 	client.logger.info(`Started playing ${track.title} [${convertTime(track.duration).replace('07:12:56', 'LIVE')}] in ${client.guilds.cache.get(player.guild).name} (Requested by ${track.requester.tag})`);
-	return channel.send({ embeds: [thing] });
+	const NowPlaying = await client.channels.cache
+		.get(player.textChannel)
+		.send({ embeds: [thing] });
+	player.setNowplayingMessage(NowPlaying);
 };
