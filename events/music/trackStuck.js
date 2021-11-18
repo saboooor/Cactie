d2494ad0e11d71eb0c31864662870c70fbb46a07
saboooor <saboor547@gmail.com>
2021-11-17 20:40:38 -0700

    track title got stuck

--- events/music/trackStuck.js
+++ events/music/trackStuck.js
@@ -1,10 +1,10 @@
 const { MessageEmbed } = require('discord.js');
-module.exports = async (client, player) => {
+module.exports = async (client, player, track) => {
 	const channel = client.channels.cache.get(player.textChannel);
 	const thing = new MessageEmbed()
 		.setColor('RED')
-		.setDescription('❌ Track was stuck, skipping..');
+		.setDescription(`❌ [${track.title}](${track.uri}) got stuck, skipping..`);
 	channel.send({ embeds: [thing] });
-	client.logger.error(`Track got stuck in ${client.guilds.cache.get(player.guild).name}`);
+	client.logger.error(`[${track.title}](${track.uri}) got stuck in ${client.guilds.cache.get(player.guild).name}`);
 	if (!player.voiceChannel) player.destroy();
 };
