62c2740e3cba47800c50b02041c13636a9522726
saboooor <saboor547@gmail.com>
2021-11-21 15:38:37 -0700

    logs

--- functions/database/delSettings.js
+++ functions/database/delSettings.js
@@ -2,6 +2,7 @@ module.exports = client => {
 	client.delSettings = function delSettings(args) {
 		try {
 			client.con.query(`DELETE FROM settings WHERE guildId = ${args}`);
+			client.logger.info(`Settings deleted for ${client.guilds.cache.get(args).name}!`);
 		}
 		catch (error) {
 			client.users.cache.get('249638347306303499').send(`${error}`);
--- functions/database/getSettings.js
+++ functions/database/getSettings.js
@@ -2,6 +2,7 @@ module.exports = client => {
 	client.getSettings = async function get(args) {
 		let settings = await client.query(`SELECT * FROM settings WHERE guildId = ${args}`);
 		if(!settings[0]) {
+			client.logger.info(`Generated settings for ${client.guilds.cache.get(args).name}!`);
 			client.setSettings(args);
 			settings = await client.query(`SELECT * FROM settings WHERE guildId = ${args}`);
 		}
