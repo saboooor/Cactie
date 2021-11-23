d5076e25fe62054ac23f65f8b09d7ad812ee0d0b
saboooor <saboor547@gmail.com>
2021-11-22 21:05:20 -0700

    fix

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,6 +1,6 @@
 const { MessageEmbed } = require('discord.js');
 module.exports = async (client, guild) => {
-	client.settings.delete(guild.id);
+	client.delData('settings', 'guildId', guild.id);
 	client.logger.info(`${client.user.username} has been removed from ${guild.name}`);
 	const owner = await guild.fetchOwner();
 	const timestamp = Math.round(guild.createdTimestamp / 1000);
