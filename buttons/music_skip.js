7363c613dd313e78713c62655b3876a4e0b8c00e
Saboor <42164502+saboooor@users.noreply.github.com>
2021-11-24 14:47:26 -0700

    fix skip button

--- buttons/music_skip.js
+++ buttons/music_skip.js
@@ -4,6 +4,19 @@ module.exports = {
 	name: 'music_skip',
 	async execute(interaction, client) {
 		const player = client.manager.get(interaction.guild.id);
+		const error = new MessageEmbed().setColor('RED');
+		if ((!player || !player.queue.current)) {
+			error.setDescription('There is no music playing.');
+			return message.reply({ embeds: [error] });
+		}
+		if (!message.member.voice.channel) {
+			error.setDescription('You must be in a voice channel!');
+			return message.reply({ embeds: [error] });
+		}
+		if (message.member.voice.channel !== message.guild.me.voice.channel) {
+			error.setDescription(`You must be in the same channel as ${client.user}!`);
+			return message.reply({ embeds: [error] });
+		}
 		if (interaction.guild.me.voice.serverMute) return interaction.reply({ content: 'I\'m server muted!', ephemeral: true });
 		if (!player) return interaction.reply({ content: 'The bot is not playing anything!', ephemeral: true });
 		const srvconfig = await client.getData('settings', 'guildId', interaction.guild.id);
