6920edf2ecbc6e2635d160445a3abb1a183ea3ed
saboooor <saboor547@gmail.com>
2021-11-25 03:31:18 -0700

    fix music skip button again

--- buttons/music_skip.js
+++ buttons/music_skip.js
@@ -7,15 +7,15 @@ module.exports = {
 		const error = new MessageEmbed().setColor('RED');
 		if ((!player || !player.queue.current)) {
 			error.setDescription('There is no music playing.');
-			return message.reply({ embeds: [error] });
+			return interaction.reply({ embeds: [error] });
 		}
-		if (!message.member.voice.channel) {
+		if (!interaction.member.voice.channel) {
 			error.setDescription('You must be in a voice channel!');
-			return message.reply({ embeds: [error] });
+			return interaction.reply({ embeds: [error] });
 		}
-		if (message.member.voice.channel !== message.guild.me.voice.channel) {
+		if (interaction.member.voice.channel !== interaction.guild.me.voice.channel) {
 			error.setDescription(`You must be in the same channel as ${client.user}!`);
-			return message.reply({ embeds: [error] });
+			return interaction.reply({ embeds: [error] });
 		}
 		if (interaction.guild.me.voice.serverMute) return interaction.reply({ content: 'I\'m server muted!', ephemeral: true });
 		if (!player) return interaction.reply({ content: 'The bot is not playing anything!', ephemeral: true });
