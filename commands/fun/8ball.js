0de1be3ef15614a654476f3f0da551177c0ca980
saboooor <saboor547@gmail.com>
2021-11-25 05:22:45 -0700

    update 8ball

--- commands/fun/8ball.js
+++ commands/fun/8ball.js
@@ -1,13 +1,17 @@
 const ball = require('../../config/8ball.json');
+const { MessageEmbed } = require('discord.js');
 module.exports = {
 	name: '8ball',
 	description: 'Let the 8ball decide your fate!',
 	args: true,
 	usage: '[Question]',
 	options: require('../options/question.json'),
-	async execute(message) {
+	async execute(message, args) {
 		// Get random index and reply with the string in the array of the index
 		const i = Math.floor(Math.random() * ball.length - 1);
-		message.reply(ball[i]);
+		const embed = new MessageEmbed()
+			.setTitle(`🎱 ${args.join()}?`)
+			.setDescription(ball[i]);
+		message.reply({ embeds: [embed] });
 	},
 };
