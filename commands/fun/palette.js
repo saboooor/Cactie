5b6d61253d4500017edb0fe45fd19af7d70f5618
Saboor <42164502+saboooor@users.noreply.github.com>
2021-11-25 12:31:00 -0700

    catch statement in -palette

--- commands/fun/palette.js
+++ commands/fun/palette.js
@@ -7,17 +7,23 @@ module.exports = {
 	args: true,
 	usage: '<Image URL>',
 	options: require('../options/imgurl.json'),
-	async execute(message, args) {
+	async execute(message, args, client) {
 		// Get the color from the img url
-		const colors = rgb2hex(await getPalette(args[0]));
+		const colors = rgb2hex(await getPalette(args[0])).catch(e => {
+			client.logger.error(e);
+			message.reply(e);
+		});
 		const embeds = [];
 		colors.forEach(color => {
-			// Create embed with color and reply with it
+			// Create embed with color and push it into the list of embeds
 			const Embed = new MessageEmbed()
 				.setColor(color)
 				.setDescription(`${color}`);
 			embeds.push(Embed);
+			client.logger.info(color);
 		});
+
+		// Reply with colors
 		message.reply({ embeds: embeds });
 	},
 };
