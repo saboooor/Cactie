41cbba71c5c2975c6083e783239a7db7d51e406e
saboooor <saboor547@gmail.com>
2021-11-28 00:02:05 -0700

    add refresh button to -stats command

--- buttons/stats_refresh.js
+++ buttons/stats_refresh.js
@@ -1,22 +1,18 @@
-const { MessageEmbed, MessageAttachment, MessageActionRow, MessageButton } = require('discord.js');
 const { createPaste } = require('hastebin');
 const { NodeactylClient } = require('nodeactyl');
 const fetch = require('node-fetch');
-const servers = require('../../config/pterodactyl.json');
-const protocols = require('../../config/mcprotocol.json');
+const servers = require('../config/pterodactyl.json');
+const protocols = require('../config/mcprotocol.json');
 module.exports = {
-	name: 'stats',
-	description: 'Get the status of Pup or a Server',
-	aliases: ['status', 'mcstatus', 'mcstats'],
-	usage: '[Server]',
-	options: require('../options/stats.json'),
-	async execute(message, args, client) {
-		if (!args[0]) args = ['pup'];
+	name: 'stats_refresh',
+	async execute(interaction, client) {
 		const srvs = [];
 		Object.keys(servers).map(i => { srvs.push(servers[i]); });
-		let server = servers[args.join(' ').toLowerCase()];
-		if (!server) server = srvs.find(srv => args[0].toLowerCase() == srv.short);
-		const Embed = new MessageEmbed().setColor(Math.floor(Math.random() * 16777215));
+		const Embed = interaction.message.embeds[0];
+		Embed.fields = [];
+		const arg = Embed.url.replace('https://', '').split('.pup')[0];
+		let server = servers[arg.toLowerCase()];
+		if (!server) server = srvs.find(srv => arg.toLowerCase() == srv.short);
 		if (server && server.id) {
 			const Client = new NodeactylClient(server.url, server.apikey);
 			const info = await Client.getServerDetails(server.id);
@@ -35,18 +31,17 @@ module.exports = {
 			if (usages.resources.memory_bytes) Embed.addField('**RAM Usage:**', `${Math.round(usages.resources.memory_bytes / 1048576)} MB / ${info.limits.memory} MB`, true);
 			if (usages.resources.network_tx_bytes) Embed.addField('**Network Sent:**', `${Math.round(usages.resources.network_tx_bytes / 1048576)} MB`, true);
 			if (usages.resources.network_rx_bytes) Embed.addField('**Network Recieved:**', `${Math.round(usages.resources.network_rx_bytes / 1048576)} MB`, true);
-			info.name ? Embed.setTitle(`${info.name} (${usages.current_state.replace(/\b(\w)/g, s => s.toUpperCase())})`) : Embed.setTitle(args.join(' '));
+			info.name ? Embed.setTitle(`${info.name} (${usages.current_state.replace(/\b(\w)/g, s => s.toUpperCase())})`) : Embed.setTitle(arg);
 			if (server.client) Embed.setThumbnail(client.user.avatarURL({ dynamic : true }));
 		}
 		else {
-			server = { ip: args[0] };
+			server = { ip: arg };
 		}
-		const iconpng = [];
 		if (server.ip) {
 			const json = await fetch(`https://api.mcsrvstat.us/2/${server.ip}`);
 			const pong = await json.json();
 			const serverlist = Object.keys(servers).map(i => { return `\n${servers[i].name} (${servers[i].short})`; });
-			if (!pong.online) return message.reply(`**Invalid Server**\nYou can use any valid Minecraft server IP\nor use an option from the list below:\`\`\`yml${serverlist.join('')}\`\`\``);
+			if (!pong.online) return interaction.reply(`**Invalid Server**\nYou can use any valid Minecraft server IP\nor use an option from the list below:\`\`\`yml${serverlist.join('')}\`\`\``);
 			if (!Embed.title && pong.hostname) Embed.setTitle(pong.hostname);
 			else if (!Embed.title && pong.port == 25565) Embed.setTitle(pong.ip);
 			else if (!Embed.title) Embed.setTitle(`${pong.ip}:${pong.port}`);
@@ -64,27 +59,13 @@ module.exports = {
 				Embed.addField('**Players:**', pong.players.list.join('\n').replace(/_/g, '\\_'));
 			}
 			if (pong.motd) Embed.addField('**MOTD:**', pong.motd.clean.join('\n').replace(/&lt;/g, '<').replace(/&gt;/g, '>').replace(/&le;/g, '≤').replace(/&ge;/g, '≥'));
-			if (pong.icon) {
-				const base64string = Buffer.from(pong.icon.replace(/^data:image\/png;base64,/, ''), 'base64');
-				iconpng.push(new MessageAttachment(base64string, 'icon.png'));
-				Embed.setThumbnail('attachment://icon.png');
-			}
-			else {
-				Embed.setThumbnail('https://cdn.mos.cms.futurecdn.net/6QQEiDSc3p6yXjhohY3tiF.jpg');
-			}
 			if (pong.plugins && pong.plugins.raw[0]) {
 				const link = await createPaste(pong.plugins.raw.join('\n'), { server: 'https://bin.birdflop.com' });
 				Embed.addField('**Plugins:**', `[Click Here](${link})`, true);
 			}
 			if (!pong.debug.query) Embed.setFooter('Query disabled! If you want more info, contact the owner to enable query.');
 		}
-		Embed.setURL(`https://${args[0]}.pup`).setTimestamp();
-		const row = new MessageActionRow().addComponents(
-			new MessageButton()
-				.setCustomId('stats_refresh')
-				.setLabel('Refresh')
-				.setStyle('SUCCESS'),
-		);
-		message.reply({ embeds: [Embed], files: iconpng, components: [row] });
+		Embed.setTimestamp();
+		interaction.update({ embeds: [Embed], components: interaction.message.components });
 	},
 };
