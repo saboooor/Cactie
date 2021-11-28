c727fe1d4ba4e8e0b2b5e7b9a21334d0ff46469c
saboooor <saboor547@gmail.com>
2021-11-27 18:39:59 -0700

    fix mutecmd setting

--- commands/admin/settings.js
+++ commands/admin/settings.js
@@ -29,8 +29,8 @@ module.exports = {
 
 			// Tickets setting can only be either buttons, reactions, or false
 			if (prop == 'tickets' && value != 'buttons' && value != 'reactions' && value != 'false') return message.reply({ content: 'This setting must be either `buttons`, `reactions`, or `false`!' });
-			// Reactions / Bonercmd / Mutecmd // Suggestthreads settings can only either be true or false
-			if ((prop == 'reactions' || prop == 'bonercmd' || prop == 'mutecmd' || prop == 'suggestthreads') && value != 'true' && value != 'false') return message.reply({ content: 'This setting must be either `true` or `false`!' });
+			// Reactions / Bonercmd / Suggestthreads settings can only either be true or false
+			if ((prop == 'reactions' || prop == 'bonercmd' || prop == 'suggestthreads') && value != 'true' && value != 'false') return message.reply({ content: 'This setting must be either `true` or `false`!' });
 			// Leavemessage / Joinmessage can only be enabled if the systemChannel is set (may change later to a separate setting)
 			if ((prop == 'leavemessage' || prop == 'joinmessage') && !message.guild.systemChannel && value != 'false') return message.reply({ content: 'Please set a system channel in your server settings first!' });
 			// Maxppsize can only be less than 76
