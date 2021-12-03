03b6c3b95146332b38467be8d5508b4b8ede3bf6
saboooor <saboor547@gmail.com>
2021-12-02 21:29:46 -0700

    completely get rid of enmap

--- handlers/database.js
+++
@@ -1,9 +0,0 @@
-const Enmap = require('enmap');
-module.exports = client => {
-	client.tickets = new Enmap({
-		name: 'tickets',
-		autoFetch: true,
-		fetchAll: false,
-	});
-	client.logger.info('Ticket database loaded');
-};
