25a5f231a5ff413ce0dadf4a969341276c40aab5
saboooor <saboor547@gmail.com>
2021-12-05 17:19:40 -0700

    ex: 01:39 => 1:39 01:01:39 => 1:01:39

--- functions/convert.js
+++ functions/convert.js
@@ -2,14 +2,13 @@ module.exports = {
 	convertTime: function(duration) {
 		let seconds = parseInt((duration / 1000) % 60);
 		let minutes = parseInt((duration / (1000 * 60)) % 60);
-		let hours = parseInt((duration / (1000 * 60 * 60)) % 24);
-		hours = (hours < 10) ? '0' + hours : hours;
-		minutes = (minutes < 10) ? '0' + minutes : minutes;
+		const hours = parseInt((duration / (1000 * 60 * 60)) % 24);
 		seconds = (seconds < 10) ? '0' + seconds : seconds;
 		if (duration < 3600000) {
 			return minutes + ':' + seconds ;
 		}
 		else {
+			minutes = (minutes < 10) ? '0' + minutes : minutes;
 			return hours + ':' + minutes + ':' + seconds ;
 		}
 	},
