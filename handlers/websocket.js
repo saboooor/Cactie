024f159f61a8c400381e298fb4e5f80f2fdf83be
saboooor <saboor547@gmail.com>
2021-12-08 20:59:59 -0700

    RangeError: Start can not be bigger than end.

--- handlers/websocket.js
+++ handlers/websocket.js
@@ -75,7 +75,7 @@ module.exports = client => {
 					const guild = client.guilds.cache.get(player.guild);
 					const member = guild.members.cache.get(userid);
 					if (member && member.voice && member.voice.channel.id === player.options.voiceChannel) {
-						player.queue.remove(0, index);
+						player.queue.remove(0, index).catch(e => client.logger.error(e));
 						player.stop();
 					}
 				});
