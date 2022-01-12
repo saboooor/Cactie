1f9970c8c5e43b74be4c80fb5e4b202488e0b97a
Saboor <saboor547@gmail.com>
2022-01-12 05:16:02 -0700

    add timings command

---
+++ commands/utilities/timings.js
@@ -0,0 +1,211 @@
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
+const fetch = require('node-fetch');
+const YAML = require('yaml');
+const fs = require('fs');
+function create_field(option) {
+	const field = { name: option.name, value: option.value };
+	if (option.prefix) field.name = option.prefix + ' ' + field.name;
+	if (option.suffix) field.name = field.name + option.suffix;
+	if (option.inline) field.inline = option.inline;
+	return field;
+}
+module.exports = {
+	name: 'timings',
+	description: 'Analyze Paper timings to optimize the Paper server.',
+	cooldown: 10,
+	args: true,
+	usage: '<Timings Link>',
+	options: require('../options/url.json'),
+	async execute(message, args, client) {
+		const Embed = new MessageEmbed()
+			.setTitle('Timings Analysis')
+			.setDescription('These are not magic values. Many of these settings have real consequences on your server\'s mechanics. See [this guide](https://eternity.community/index.php/paper-optimization/) for detailed information on the functionality of each setting.')
+			.setFooter({ text: `Requested by ${message.member.user.tag}`, iconURL: message.member.user.avatarURL({ dynamic: true }) });
+
+		const url = args[0].replace('/d=', '/?id=').split('#')[0];
+
+		if (url.startsWith('https://www.spigotmc.org/go/timings?url=') || args[0].startsWith('https://spigotmc.org/go/timings?url=')) {
+			Embed.addField('❌ Spigot', 'Spigot timings have limited information. Switch to [Purpur](https://purpur.pl3x.net/downloads) for better timings analysis. All your plugins will be compatible, and if you don\'t like it, you can easily switch back.')
+				.setURL(url);
+			return message.reply({ embeds: [Embed] });
+		}
+
+		if (!url.startsWith('https://timin')) return message.reply({ content: 'Invalid Timings URL.' });
+		if (!url.includes('?id=')) return message.reply({ content: 'Invalid Timings URL.' });
+
+		Embed.setURL(url);
+
+		client.logger.info(`Timings analyzed from ${message.member.user.tag} (${message.member.user.id}): ${url}`);
+
+		const timings_host = url.split('?id=')[0];
+		const timings_id = url.split('?id=')[1];
+
+		const timings_json = timings_host + 'data.php?id=' + timings_id;
+		const url_raw = url + '&raw=1';
+
+		const response_raw = await fetch(url_raw);
+		const request_raw = await response_raw.json();
+		const response_json = await fetch(timings_json);
+		const request = await response_json.json();
+
+		if (!request_raw || !request) {
+			Embed.addFields('❌ Invalid report', 'Create a new timings report.', true);
+			return message.reply({ embeds: [Embed] });
+		}
+
+		let version = request.timingsMaster.version;
+		client.logger.info(version);
+
+		if (version.endsWith('(MC: 1.17)')) version = version.replace('(MC: 1.17)', '(MC: 1.17.0)');
+
+		const TIMINGS_CHECK = await YAML.parse(fs.readFileSync('./config/timings_check.yml', 'utf8'));
+
+		if (TIMINGS_CHECK.version && version) {
+			// ghetto version check
+			if (version.split('(MC: ')[1].split(')')[0] != TIMINGS_CHECK.version) {
+				version = version.replace('git-', '').replace('MC: ', '');
+				Embed.addField('❌ Outdated', `You are using \`${version}\`. Update to \`${TIMINGS_CHECK.version}\`.`, true);
+			}
+		}
+
+		if (TIMINGS_CHECK.servers) {
+			TIMINGS_CHECK.servers.forEach(server => {
+				if (version.includes(server.name)) Embed.addFields(create_field(server));
+			});
+		}
+
+		const timing_cost = parseInt(request.timingsMaster.system.timingcost);
+		if (timing_cost > 300) Embed.addField('❌ Timingcost', `Your timingcost is ${timing_cost}. Your cpu is overloaded and/or slow. Find a [better host](https://www.birdflop.com).`, true);
+
+		const flags = request.timingsMaster.system.flags;
+		const jvm_version = request.timingsMaster.system.jvmversion;
+		if (flags.includes('-XX:+UseZGC') && flags.includes('-Xmx')) {
+			const flaglist = flags.split(' ');
+			flaglist.forEach(flag => {
+				if (flag.startsWith('-Xmx')) {
+					let max_mem = flag.split('-Xmx')[1];
+					max_mem = max_mem.replace('G', '000');
+					max_mem = max_mem.replace('M', '');
+					max_mem = max_mem.replace('g', '000');
+					max_mem = max_mem.replace('m', '');
+					if (parseInt(max_mem) < 10000) Embed.addField('❌ Low Memory', 'ZGC is only good with a lot of memory.', true);
+				}
+			});
+		}
+		else if (flags.includes('-Daikars.new.flags=true')) {
+			if (!flags.includes('-XX:+PerfDisableSharedMem')) Embed.addField('❌ Outdated Flags', 'Add `-XX:+PerfDisableSharedMem` to flags.', true);
+			if (!flags.includes('-XX:G1MixedGCCountTarget=4')) Embed.addField('❌ Outdated Flags', 'Add `XX:G1MixedGCCountTarget=4` to flags.', true);
+			if (!flags.includes('-XX:+UseG1GC') && jvm_version.startswith('1.8.')) Embed.addField('❌ Aikar\'s Flags', 'You must use G1GC when using Aikar\'s flags.', true);
+			if (flags.includes('-Xmx')) {
+				let max_mem = 0;
+				const flaglist = flags.split(' ');
+				flaglist.forEach(flag => {
+					if (flag.startsWith('-Xmx')) {
+						max_mem = flag.split('-Xmx')[1];
+						max_mem = max_mem.replace('G', '000');
+						max_mem = max_mem.replace('M', '');
+						max_mem = max_mem.replace('g', '000');
+						max_mem = max_mem.replace('m', '');
+					}
+				});
+				if (parseInt(max_mem) < 5400) Embed.addField('❌ Low Memory', 'Allocate at least 6-10GB of ram to your server if you can afford it.', true);
+				let index = 0;
+				let max_online_players = 0;
+				while (index < request.timingsMaster.data.length) {
+					const timed_ticks = request.timingsMaster.data[index].minuteReports[0].ticks.timedTicks;
+					const player_ticks = request.timingsMaster.data[index].minuteReports[0].ticks.playerTicks;
+					const players = (player_ticks / timed_ticks);
+					max_online_players = Math.max(players, max_online_players);
+					index = index + 1;
+				}
+				if (1000 * max_online_players / parseInt(max_mem) > 6 && parseInt(max_mem) < 10000) Embed.addField('❌ Low Memory', 'You should be using more RAM with this many players.', true);
+				if (flags.includes('-Xms')) {
+					let min_mem = 0;
+					flaglist.forEach(flag => {
+						if (flag.startsWith('-Xmx')) {
+							min_mem = flag.split('-Xmx')[1];
+							min_mem = min_mem.replace('G', '000');
+							min_mem = min_mem.replace('M', '');
+							min_mem = min_mem.replace('g', '000');
+							min_mem = min_mem.replace('m', '');
+						}
+					});
+					if (min_mem != max_mem) Embed.addField('❌ Aikar\'s Flags', 'Your Xmx and Xms values should be equal when using Aikar\'s flags.', true);
+				}
+			}
+		}
+		else if (flags.includes('-Dusing.aikars.flags=mcflags.emc.gs')) {
+			Embed.addField('❌ Outdated Flags', 'Update [Aikar\'s flags](https://aikar.co/2018/07/02/tuning-the-jvm-g1gc-garbage-collector-flags-for-minecraft/).', true);
+		}
+		else {
+			Embed.addField('❌ Aikar\'s Flags', 'Use [Aikar\'s flags](https://aikar.co/2018/07/02/tuning-the-jvm-g1gc-garbage-collector-flags-for-minecraft/).', true);
+		}
+
+		const cpu = parseInt(request.timingsMaster.system.cpu);
+		if (cpu <= 2) Embed.addField('❌ Threads', `You only have ${cpu} thread(s). Find a [better host](https://www.birdflop.com).`, true);
+
+		const handlers = Object.keys(request_raw.idmap.handlers).map(i => { return request_raw.idmap.handlers[i]; });
+		handlers.forEach(handler => {
+			let handler_name = handler[1];
+			if (handler_name.startsWith('Command Function - ') && handler_name.endsWith(':tick')) {
+				handler_name = handler_name.split('Command Function - ')[1].split(':tick')[0];
+				Embed.addField(`❌ ${handler_name}`, 'This datapack uses command functions which are laggy.', true);
+			}
+		});
+
+		const plugins = Object.keys(request.timingsMaster.plugins).map(i => { return request.timingsMaster.plugins[i]; });
+		const server_properties = request.timingsMaster.config['server.properties'];
+		const bukkit = request.timingsMaster.config ? request.timingsMaster.config.bukkit : null;
+		const spigot = request.timingsMaster.config ? request.timingsMaster.config.spigot : null;
+		const paper = request.timingsMaster.config ? request.timingsMaster.config.paper : null;
+		const purpur = request.timingsMaster.config ? request.timingsMaster.config.purpur : null;
+		// LINE 216 PY
+
+		plugins.forEach(plugin => {
+			if (plugin.authors && plugin.authors.toLowerCase().includes('songoda')) {
+				if (plugin.name == 'EpicHeads') Embed.addField('❌ EpicHeads', 'This plugin was made by Songoda. Songoda is sketchy. You should find an alternative such as [HeadsPlus](https://spigotmc.org/resources/headsplus-»-1-8-1-16-4.40265/) or [HeadDatabase](https://www.spigotmc.org/resources/head-database.14280/).', true);
+				else if (plugin.name == 'UltimateStacker') Embed.addField('❌ UltimateStacker', 'Stacking plugins actually causes more lag.\nRemove UltimateStacker.', true);
+				else Embed.addField(`❌ ${plugin.name}`, 'This plugin was made by Songoda. Songoda is sketchy. You should find an alternative.', true);
+			}
+		});
+
+		const worlds = Object.keys(request_raw.worlds).map(i => { return request_raw.worlds[i]; });
+		let high_mec = false;
+		worlds.forEach(world => {
+			const max_entity_cramming = parseInt(world.gamerules.maxEntityCramming);
+			if (max_entity_cramming >= 24) high_mec = true;
+		});
+		if (high_mec) Embed.addField('❌ maxEntityCramming', 'Decrease this by running the /gamerule command in each world. Recommended: 8.', true);
+
+		// LINE 273 PY
+
+		const issue_count = Embed.fields.length;
+		if (issue_count == 0) {
+			Embed.addField('✅ All good', 'Analyzed with no recommendations.');
+			return message.reply({ embeds: [Embed] });
+		}
+		const components = [];
+		if (issue_count >= 13) {
+			Embed.fields.splice(12, issue_count);
+			Embed.addField(`Plus ${issue_count - 12} more recommendations`, 'Click the buttons below to see more');
+			components.push(
+				new MessageActionRow()
+					.addComponents(
+						new MessageButton()
+							.setCustomId('timings_prev')
+							.setLabel('◄')
+							.setStyle('PRIMARY'),
+						new MessageButton()
+							.setCustomId('timings_next')
+							.setLabel('►')
+							.setStyle('PRIMARY'),
+						new MessageButton()
+							.setURL('https://github.com/pemigrade/botflop')
+							.setLabel('Botflop')
+							.setStyle('LINK'),
+					),
+			);
+		}
+		message.reply({ embeds: [Embed], components: components });
+	},
+};
