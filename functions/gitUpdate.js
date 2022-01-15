2a86d5d1e8fb452bc4c26d8296f53b480cde79c0
Saboor <saboor547@gmail.com>
2022-01-14 19:42:48 -0700

    used the wrong thing

--- functions/gitUpdate.js
+++ functions/gitUpdate.js
@@ -4,7 +4,7 @@ function sleep(ms) { return new Promise(res => setTimeout(res, ms)); }
 module.exports = async function gitUpdate(client, message) {
 	// get embed and check if it's an embed in the git channel
 	const embed = message.embeds[0];
-	if (!message.webhookId && !message.channel.id == '812082273393704960' && !embed) return;
+	if (!message.webhookId || !message.channel.id == '812082273393704960' || !embed) return;
 
 	// get the server name from pterodactyl.json and branch name from embed title and check if it's this bot
 	let server = null;
