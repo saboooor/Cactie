4206446c25a936404aba2415285f211b6435303d
Saboor <saboor547@gmail.com>
2022-01-15 02:42:52 -0700

    remove vote dm

--- functions/addVote.js
+++ functions/addVote.js
@@ -1,7 +1,6 @@
 module.exports = async function addVote(body, client) {
 	const user = client.users.cache.get(body.id) || client.users.cache.get(body.user);
-	if (!user) return client.logger.info(`Got vote from ${body.id || body.user} (Invalid user, not giving vote perks)`);
-	client.logger.info(`Got vote from ${user.tag}!`);
-	user.send('Thanks for voting for Pup!');
-	await client.setData('lastvoted', 'userId', `${user.id}`, 'timestamp', Date.now());
+	if (!user) client.logger.info(`Got vote from ${body.id || body.user}!`);
+	else client.logger.info(`Got vote from ${user.tag}!`);
+	await client.setData('lastvoted', 'userId', `${body.id || body.user}`, 'timestamp', Date.now());
 };
