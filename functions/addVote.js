ff4e2a372fb9e9f4a27f1d1923a839fac35781b2
Saboor <saboor547@gmail.com>
2022-01-15 21:34:22 -0700

    use id instead of the actual user

--- functions/addVote.js
+++ functions/addVote.js
@@ -6,7 +6,7 @@ module.exports = async function addVote(body, client) {
 	const Embed = new MessageEmbed()
 		.setColor(Math.floor(Math.random() * 16777215))
 		.setTitle('Vote Received!')
-		.setDescription(`Thank you for voting, ${user || body.id || body.user}!`);
+		.setDescription(`Thank you for voting, <@${user.id || body.id || body.user}>!`);
 	client.guilds.cache.get('811354612547190794').channels.cache.get('931848198773948427').send({ embeds: [Embed] });
 	await client.setData('lastvoted', 'userId', `${body.id || body.user}`, 'timestamp', Date.now());
 };
