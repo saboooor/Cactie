9f2e487378ff59b38f88b9348bee6fc4914e545f
Saboor <saboor547@gmail.com>
2022-01-19 01:45:34 -0700

    leave after 5 mins of no music

--- events/music/queueEnd.js
+++ events/music/queueEnd.js
@@ -1,3 +1,4 @@
+function sleep(ms) { return new Promise(res => setTimeout(res, ms)); }
 const { MessageEmbed } = require('discord.js');
 const { warn } = require('../../config/emoji.json');
 module.exports = async (client, player) => {
@@ -8,4 +9,17 @@ module.exports = async (client, player) => {
 		.setFooter({ text: client.user.username, iconURL: client.user.avatarURL({ dynamic : true }) });
 	const NowPlaying = await channel.send({ embeds: [thing] });
 	player.setNowplayingMessage(NowPlaying);
+	if (!player.twentyFourSeven) {
+		await sleep(300000);
+		if (player.queue.current) return;
+		const Embed = new MessageEmbed()
+			.setColor(Math.round(Math.random() * 16777215))
+			.setDescription(`${warn} **Left because of 5 minutes of inactivity!**`)
+			.addField('Tired of me leaving?', 'Enable the **24/7** mode with the /247 command!')
+			.setFooter({ text: client.user.username, iconURL: client.user.avatarURL({ dynamic : true }) });
+		const LeaveMsg = await channel.send({ embeds: [Embed] });
+		player.setNowplayingMessage(LeaveMsg);
+		client.logger.info(`Destroyed player in ${client.guilds.cache.get(player.guild).name} because of queue end`);
+		player.destroy();
+	}
 };
