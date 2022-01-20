9c381bafd541f35c3290f11c74f221a1bd277d55
Saboor <saboor547@gmail.com>
2022-01-20 01:41:07 -0700

    add buttons to greeting message and embed

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -1,4 +1,4 @@
-const { MessageEmbed } = require('discord.js');
+const { MessageEmbed, MessageActionRow, MessageButton } = require('discord.js');
 module.exports = async (client, guild) => {
 	client.logger.info(`${client.user.username} has been added to ${guild.name}`);
 	const owner = await guild.fetchOwner();
@@ -8,10 +8,31 @@ module.exports = async (client, guild) => {
 		.setTitle(`${client.user.username} has been added to ${guild.name}`)
 		.setThumbnail(guild.iconURL({ dynamic : true }))
 		.setFooter({ text: `Owner: ${owner.user.username}`, iconURL: owner.user.avatarURL({ dynamic : true }) })
-		.setDescription(`This guild has ${guild.memberCount} members`)
+		.setDescription(`This guild has ${guild.memberCount} members\nPup is now in ${client.guilds.cache.size} servers`)
 		.addField('Created At', `<t:${timestamp}>\n<t:${timestamp}:R>`);
 	client.channels.cache.get('865682839616618506').send({ embeds: [Embed] });
 	const srvconfig = await client.getData('settings', 'guildId', guild.id);
-	if (!guild.systemChannel) owner.send(`**Thanks for adding me to your server!**\nMy prefix is \`${srvconfig.prefix}\`, you can change this with ${srvconfig.prefix}settings\nType ${srvconfig.prefix}help for help, and join the support discord at https://pup.smhsmh.club/discord \nDo ${srvconfig.prefix}invite to invite me to other servers!\nThere is also a dashboard at https://pup.smhsmh.club!`).catch(e => { client.logger.warn(e); });
-	else guild.systemChannel.send(`**Thanks for adding me to your server!**\nMy prefix is \`${srvconfig.prefix}\`, you can change this with ${srvconfig.prefix}settings\nType ${srvconfig.prefix}help for help, and join the support discord at https://pup.smhsmh.club/discord \nDo ${srvconfig.prefix}invite to invite me to other servers!\nThere is also a dashboard at https://pup.smhsmh.club!`).catch(e => { client.logger.warn(e); });
+	const row = new MessageActionRow()
+		.addComponents(
+			new MessageButton()
+				.setURL('https://pup.smhsmh.club')
+				.setLabel('Dashboard')
+				.setStyle('LINK'),
+			new MessageButton()
+				.setURL('https://pup.smhsmh.club/discord')
+				.setLabel('Support Server')
+				.setStyle('LINK'),
+			new MessageButton()
+				.setURL('https://pup.smhsmh.club/discord')
+				.setLabel('Vote on top.gg')
+				.setStyle('LINK'),
+		);
+	const greetingEmbed = new MessageEmbed()
+		.setColor(Embed.color)
+		.setTitle(`Thanks for adding me to ${guild.name}!`)
+		.setDescription(`My prefix is \`${srvconfig.prefix}\`, you can change this with ${srvconfig.prefix}settings\nType ${srvconfig.prefix}help for help, and ${srvconfig.prefix}invite to invite me to other servers!`)
+		.setThumbnail('https://pup.smhsmh.club/assets/images/pup.png');
+	const message = { embeds: [greetingEmbed], components: [row] };
+	if (!guild.systemChannel) owner.send(message).catch(e => { client.logger.warn(e); });
+	else guild.systemChannel.send(message).catch(e => { client.logger.warn(e); });
 };
