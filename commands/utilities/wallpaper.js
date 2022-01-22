98ea509d0e58fb24dc952d4a396b22f2e39b7d6a
Saboor <saboor547@gmail.com>
2022-01-21 18:10:20 -0700

    no

--- commands/utilities/wallpaper.js
+++ commands/utilities/wallpaper.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'wallpaper',
 	description: 'Get a fresh new wallpaper!',
 	async execute(message, args, client) {
-		require('../../functions/redditFetch.js')(['wallpaper', 'wallpapers', 'phonewallpapers', 'wallpaperdump'], message, client);
+		require('../../functions/redditFetch.js')(['wallpaper', 'wallpapers', 'wallpaperdump'], message, client);
 	},
 };
