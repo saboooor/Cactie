94bdcaa52ced28df77dd153eb92b82e23b077086
Saboor <saboor547@gmail.com>
2022-01-22 22:22:40 -0700

    fix unknown message issues

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,7 +1,7 @@
 function sleep(ms) { return new Promise(res => setTimeout(res, ms)); }
 module.exports = async (client, reaction, user) => {
 	if (user.bot) return;
-	const message = await reaction.message.fetch();
+	const message = await reaction.message.fetch().catch(err => client.logger.error(err));
 	if (message.channel.type == 'DM') return;
 	let emojiId = reaction.emoji.id;
 	if (!emojiId) emojiId = reaction.emoji.name;
