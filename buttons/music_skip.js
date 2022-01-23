da7361a5d7b7d71b5463ac4253b291a5cfce4772
Saboor <saboor547@gmail.com>
2022-01-22 19:17:20 -0700

    only add skipamount if djrole isn't false

--- buttons/music_skip.js
+++ buttons/music_skip.js
@@ -11,14 +11,17 @@ module.exports = {
 	async execute(interaction, client) {
 		// Get the player
 		const player = client.manager.get(interaction.guild.id);
-		const requiredAmount = Math.floor((interaction.guild.me.voice.channel.members.size - 1) / 2);
-		if (!player.skipAmount) player.skipAmount = [];
-		let alr = false;
-		player.skipAmount.forEach(i => { if (i == interaction.member.id) alr = true; });
-		if (alr) return interaction.reply({ content: 'You\'ve already voted to skip this song!' });
-		player.skipAmount.push(interaction.member.id);
-		if (player.skipAmount.length < requiredAmount) return interaction.reply(`**Skipping?** \`${player.skipAmount.length} / ${requiredAmount}\` Use \`/skip\` to skip or \`/forceskip\` to force skip`);
-		player.skipAmount = null;
+		const srvconfig = await client.getData('settings', 'guildId', interaction.guild.id);
+		if (srvconfig.djrole != 'false') {
+			const requiredAmount = Math.floor((interaction.guild.me.voice.channel.members.size - 1) / 2);
+			if (!player.skipAmount) player.skipAmount = [];
+			let alr = false;
+			player.skipAmount.forEach(i => { if (i == interaction.member.id) alr = true; });
+			if (alr) return interaction.reply({ content: 'You\'ve already voted to skip this song!' });
+			player.skipAmount.push(interaction.member.id);
+			if (player.skipAmount.length < requiredAmount) return interaction.reply(`**Skipping?** \`${player.skipAmount.length} / ${requiredAmount}\` Use \`/skip\` to skip or \`/forceskip\` to force skip`);
+			player.skipAmount = null;
+		}
 		const song = player.queue.current;
 		player.stop();
 		const thing = new MessageEmbed()
--- commands/music/skip.js
+++ commands/music/skip.js
@@ -41,14 +41,16 @@ module.exports = {
 				return message.commandName ? message.deleteReply() : msg.edit({ content: '**Skipped**', embeds: [] });
 			}
 		}
-		const requiredAmount = Math.floor((message.guild.me.voice.channel.members.size - 1) / 2);
-		if (!player.skipAmount) player.skipAmount = [];
-		let alr = false;
-		player.skipAmount.forEach(i => { if (i == message.member.id) alr = true; });
-		if (alr) return message.reply('You\'ve already voted to skip this song!');
-		player.skipAmount.push(message.member.id);
-		if (player.skipAmount.length < requiredAmount) return message.reply(`**Skipping?** \`${player.skipAmount.length} / ${requiredAmount}\` Use \`${srvconfig.prefix}forceskip\` to force skip`);
-		player.skipAmount = null;
+		if (srvconfig.djrole != 'false') {
+			const requiredAmount = Math.floor((message.guild.me.voice.channel.members.size - 1) / 2);
+			if (!player.skipAmount) player.skipAmount = [];
+			let alr = false;
+			player.skipAmount.forEach(i => { if (i == message.member.id) alr = true; });
+			if (alr) return message.reply('You\'ve already voted to skip this song!');
+			player.skipAmount.push(message.member.id);
+			if (player.skipAmount.length < requiredAmount) return message.reply(`**Skipping?** \`${player.skipAmount.length} / ${requiredAmount}\` Use \`/forceskip\` to force skip`);
+			player.skipAmount = null;
+		}
 		const song = player.queue.current;
 		player.stop();
 		const thing = new MessageEmbed()
