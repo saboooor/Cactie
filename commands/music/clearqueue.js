30d23a9e8c154c0b61687c6b7222c59491402956
Saboor <saboor547@gmail.com>
2022-01-23 03:17:55 -0700

    add comments to clearqueue

--- commands/music/clearqueue.js
+++ commands/music/clearqueue.js
@@ -8,7 +8,10 @@ module.exports = {
 	inVoiceChannel: true,
 	sameVoiceChannel: true,
 	async execute(message, args, client) {
+		// Get player
 		const player = client.manager.get(message.guild.id);
+
+		// Check if djrole is set, if so, check if user has djrole, if not, vote for queue clear instead of clearing queue
 		const srvconfig = await client.getData('settings', 'guildId', message.guild.id);
 		if (srvconfig.djrole != 'false' && message.guild.roles.cache.get(srvconfig.djrole) && !message.member.roles.cache.has(srvconfig.djrole)) {
 			const requiredAmount = Math.floor((message.guild.me.voice.channel.members.size - 1) / 2);
@@ -20,6 +23,8 @@ module.exports = {
 			if (player.clearQueueAmount.length < requiredAmount) return message.reply(`**Clear Queue?** \`${player.clearQueueAmount.length} / ${requiredAmount}\``);
 			player.clearQueueAmount = null;
 		}
+
+		// Clear the queue and send message
 		player.queue.clear();
 		const thing = new MessageEmbed()
 			.setColor(Math.round(Math.random() * 16777215))
