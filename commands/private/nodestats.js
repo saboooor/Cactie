cd5ee3c24f4afb9d0c640438fc2e74c36b339fb9
Saboor <saboor547@gmail.com>
2022-01-23 22:27:36 -0700

    different ns format

--- commands/private/nodestats.js
+++ commands/private/nodestats.js
@@ -2,25 +2,21 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
 	name: 'nodestats',
 	description: 'Check music backend stats',
-	ephemeral: true,
-	aliases: ['ns'],
 	async execute(message, args, client) {
 		const all = client.manager.nodes.map(node =>
-			`Players: ${node.stats.players}` +
-            `\nPlaying Players: ${node.stats.playingPlayers}` +
-            `\nUptime: ${new Date(node.stats.uptime).toISOString().slice(11, 19)}` +
-            '\n\nMemory' +
-            `\nReservable Memory: ${Math.round(node.stats.memory.reservable / 1024 / 1024)}mb` +
-            `\nUsed Memory: ${Math.round(node.stats.memory.used / 1024 / 1024)}mb` +
-            `\nFree Memory: ${Math.round(node.stats.memory.free / 1024 / 1024)}mb` +
-            `\nAllocated Memory: ${Math.round(node.stats.memory.allocated / 1024 / 1024)}mb` +
-            '\n\nCPU' +
-            `\nCores: ${node.stats.cpu.cores}` +
-            `\nSystem Load: ${(Math.round(node.stats.cpu.systemLoad * 100) / 100).toFixed(2)}%` +
-            `\nLavalink Load: ${(Math.round(node.stats.cpu.lavalinkLoad * 100) / 100).toFixed(2)}%`,
+			`**Players:** ${node.stats.players}` +
+            `\n**Playing Players:** ${node.stats.playingPlayers}` +
+            `\n**Uptime:** ${new Date(node.stats.uptime).toISOString().slice(11, 19)}` +
+            `\n**Reservable Memory:** ${Math.round(node.stats.memory.reservable / 1024 / 1024)}mb` +
+            `\n**Used Memory:** ${Math.round(node.stats.memory.used / 1024 / 1024)}mb` +
+            `\n**Free Memory:** ${Math.round(node.stats.memory.free / 1024 / 1024)}mb` +
+            `\n**Allocated Memory:** ${Math.round(node.stats.memory.allocated / 1024 / 1024)}mb` +
+            `\n**Cores:** ${node.stats.cpu.cores}` +
+            `\n**System Load:** ${(Math.round(node.stats.cpu.systemLoad * 100) / 100).toFixed(2)}%` +
+            `\n**Lavalink Load:** ${(Math.round(node.stats.cpu.lavalinkLoad * 100) / 100).toFixed(2)}%`,
 		).join('\n\n----------------------------\n');
 		const embed = new MessageEmbed()
-			.setDescription(`\`\`\`${all}\`\`\``)
+			.setDescription(all)
 			.setColor(Math.round(Math.random() * 16777215));
 		await message.reply({ embeds: [embed] });
 	},
