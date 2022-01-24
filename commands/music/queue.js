3e961615aa6c163898afaf74b41fe791fa6d2997
Saboor <42164502+saboooor@users.noreply.github.com>
2022-01-24 10:27:06 -0700

    add comments to queue command

--- commands/music/queue.js
+++ commands/music/queue.js
@@ -4,27 +4,34 @@ const { createPaste } = require('hastebin');
 module.exports = {
 	name: 'queue',
 	description: 'Show the music queue and now playing.',
+	usage: '[Page Number]',
 	aliases: ['q'],
 	cooldown: 4,
 	player: true,
 	async execute(message, args, client) {
+		// Get the player, queue, and embed
 		const player = client.manager.get(message.guild.id);
 		const queue = player.queue;
 		const song = queue.current;
 		const embed = new MessageEmbed()
 			.setColor(song.color)
 			.setThumbnail(song.img);
-		const multiple = 10;
+
+		// Get first page or page specified
 		const page = args.length && Number(args[0]) ? Number(args[0]) : 1;
-		const end = page * multiple;
-		const start = end - multiple;
+		const end = page * 10;
+		const start = end - 10;
 		const tracks = queue.slice(start, end);
+
+		// Add current song as a field and queue list
 		if (song) embed.addField('Now Playing', `[${song.title}](${song.uri}) \`[${convertTime(song.duration).replace('7:12:56', 'LIVE')}]\` [${song.requester}]`);
 		let mapped = tracks.map((track, i) => `**${start + (++i)}** • ${track.title} \`[${convertTime(track.duration).replace('7:12:56', 'LIVE')}]\` [${track.requester}]`).join('\n');
 		if (mapped.length > 1024) mapped = `List too long, shortened to a link\n${await createPaste(mapped)}`;
 		if (!tracks.length) embed.addField('No tracks up next', `in ${page > 1 ? `page ${page}` : 'the queue'}.`);
 		else embed.addField('🎶 Queue List', mapped);
-		const maxPages = Math.ceil(queue.length / multiple);
+
+		// Get max pages and add it to footer and reply with buttons
+		const maxPages = Math.ceil(queue.length / 10);
 		embed.setFooter({ text: `Page ${page > maxPages ? maxPages : page} of ${maxPages}` });
 		const row = new MessageActionRow()
 			.addComponents(
@@ -37,6 +44,6 @@ module.exports = {
 					.setLabel('►')
 					.setStyle('SECONDARY'),
 			);
-		return message.reply({ embeds: [embed], components: [row] });
+		message.reply({ embeds: [embed], components: [row] });
 	},
 };
