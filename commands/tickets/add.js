00f6d20eb21011eeac276d85c226e19f885e1fac
Saboor <42164502+saboooor@users.noreply.github.com>
2022-01-25 09:59:13 -0700

    add comments to add and close commands

--- commands/tickets/add.js
+++ commands/tickets/add.js
@@ -7,6 +7,7 @@ module.exports = {
 	usage: '<User Mention or Id>',
 	options: require('../options/user.json'),
 	async execute(message, args, client) {
+		// Check if channel is a subticket, if so tell the user to use the command in the ticket itself instead
 		if (message.channel.name.startsWith(`Subticket${client.user.username.replace('Pup', '') + ' '}`) && message.channel.parent.name.startsWith(`ticket${client.user.username.replace('Pup', '').replace(' ', '').toLowerCase()}-`)) return message.reply(`This is a subticket!\nYou must use this command in ${message.channel.parent}`);
 
 		// Check if ticket is an actual ticket
@@ -14,18 +15,29 @@ module.exports = {
 		if (!ticketData) return;
 		if (ticketData.users) ticketData.users = ticketData.users.split(',');
 
+		// Get settings and check if tickets are disabled
 		const srvconfig = await client.getData('settings', 'guildId', message.guild.id);
 		if (srvconfig.tickets == 'false') return message.reply({ content: 'Tickets are disabled!' });
+
+		// Check if ticket is closed
 		if (message.channel.name.startsWith(`closed${client.user.username.replace('Pup', '').replace(' ', '').toLowerCase()}-`)) return message.reply({ content: 'This ticket is closed!' });
+
+		// Check if user is valid
 		const user = client.users.cache.find(u => u.id === args[0].replace('<@', '').replace('!', '').replace('>', ''));
 		if (!user) return message.reply('Invalid User!');
+
+		// Check if user is already in the ticket, if not, add them to the ticket data
 		if (ticketData.users.includes(user.id)) return message.reply({ content: 'This user has already been added!' });
 		ticketData.users.push(user.id);
 		client.setData('ticketdata', 'channelId', message.channel.id, 'users', ticketData.users.join(','));
+
+		// If the ticket has a voiceticket, give permissions to the user there
 		if (ticketData.voiceticket && ticketData.voiceticket !== 'false') {
 			const voiceticket = message.guild.channels.cache.get(ticketData.voiceticket);
 			voiceticket.permissionOverwrites.edit(user, { VIEW_CHANNEL: true });
 		}
+
+		// Give permissions to the user and reply
 		message.channel.permissionOverwrites.edit(user, { VIEW_CHANNEL: true });
 		const Embed = new MessageEmbed()
 			.setColor(15105570)
--- commands/tickets/close.js
+++ commands/tickets/close.js
@@ -6,16 +6,22 @@ module.exports = {
 	description: 'Close a ticket',
 	botperm: 'MANAGE_CHANNELS',
 	async execute(message, user, client, reaction) {
+		// Set author to reaction author if the command is a reaction
 		let author = message.member.user;
 		if (reaction) {
 			if (message.author.id != client.user.id) return;
 			author = user;
 		}
+
 		// Check if ticket is an actual ticket
 		const ticketData = (await client.query(`SELECT * FROM ticketdata WHERE channelId = '${message.channel.id}'`))[0];
 		if (!ticketData) return;
+
+		// Split the list of users into an array cuz mysql dumb
 		if (ticketData.users) ticketData.users = ticketData.users.split(',');
 		const srvconfig = await client.getData('settings', 'guildId', message.guild.id);
+
+		// If the channel is a subticket, delete the subticket instead
 		if (message.channel.name.startsWith(`Subticket${client.user.username.replace('Pup', '') + ' '}`) &&
 		message.channel.parent.name.startsWith(`ticket${client.user.username.replace('Pup', '').replace(' ', '').toLowerCase()}-`)) {
 			const messages = await message.channel.messages.fetch({ limit: 100 });
@@ -31,23 +37,41 @@ module.exports = {
 			client.logger.info(`Closed subticket #${message.channel.name}`);
 			return message.channel.delete();
 		}
+
+		// Check if tickets are disabled
 		if (srvconfig.tickets == 'false') return message.reply({ content: 'Tickets are disabled!' });
+		
+		// Check if ticket is already closed
 		if (message.channel.name.startsWith(`closed${client.user.username.replace('Pup', '').replace(' ', '').toLowerCase()}-`)) return message.reply({ content: 'This ticket is already closed!' });
+		
+		// Check if user is a user that has been added with -add
 		if (ticketData.users.includes(author.id) && author.id != ticketData.opener) return message.reply({ content: 'You can\'t close this ticket!' });
+		
+		// Set the name to closed and check if bot has been rate limited
 		message.channel.setName(message.channel.name.replace('ticket', 'closed'));
 		await sleep(1000);
 		if (message.channel.name.startsWith(`ticket${client.user.username.replace('Pup', '').replace(' ', '').toLowerCase()}-`)) return message.reply({ content: 'Failed to close ticket, please try again in 10 minutes' });
+		
+		// If voiceticket is set, delete the voiceticket
 		if (ticketData.voiceticket !== 'false') {
 			const voiceticket = message.guild.channels.cache.get(ticketData.voiceticket);
 			voiceticket.delete();
 			await client.setData('ticketdata', 'channelId', message.channel.id, 'voiceticket', 'false');
 		}
+
+		// Unresolve ticket
 		await client.setData('ticketdata', 'channelId', message.channel.id, 'resolved', 'false');
+		
+		// Get rid of the permissions of the users in the tickets from the ticket itself
 		ticketData.users.forEach(userid => {
 			message.channel.permissionOverwrites.edit(client.users.cache.get(userid), { VIEW_CHANNEL: false });
 		});
+
+		// Create a transcript of the ticket
 		const messages = await message.channel.messages.fetch({ limit: 100 });
 		const link = await getTranscript(messages);
+
+		// Get all the users and send the embed to their DMs
 		const users = [];
 		await ticketData.users.forEach(userid => users.push(client.users.cache.get(userid)));
 		const EmbedDM = new MessageEmbed()
@@ -61,9 +85,13 @@ module.exports = {
 			usr.send({ embeds: [EmbedDM] })
 				.catch(error => { client.logger.warn(error); });
 		});
+
+		// Create embed
 		const Embed = new MessageEmbed()
 			.setColor(15105570)
 			.setDescription(`Ticket Closed by ${author}`);
+
+		// If the ticket mode is set to buttons, add the buttons, if not, don't
 		if (srvconfig.tickets == 'buttons') {
 			const row = new MessageActionRow()
 				.addComponents([
@@ -84,6 +112,8 @@ module.exports = {
 		else {
 			message.reply({ embeds: [Embed] });
 		}
+
+		// Add reaction panel if ticket mode is set to reactions
 		if (srvconfig.tickets == 'reactions') {
 			Embed.setColor(3447003);
 			Embed.setDescription('🔓 Reopen Ticket `/open`\n⛔ Delete Ticket `/delete`');
@@ -91,6 +121,8 @@ module.exports = {
 			embed.react('🔓');
 			embed.react('⛔');
 		}
+
+		// Log
 		client.logger.info(`Closed ticket #${message.channel.name}`);
 	},
 };
