5748bc29cab134f8d569d8c8178ce95898c37745
Saboor <saboor547@gmail.com>
2022-01-28 15:24:06 -0700

    add getRawMessage

---
+++ commands/message/getRawMessage.js
@@ -0,0 +1,15 @@
+const { createPaste } = require('hastebin');
+module.exports = {
+	name: 'Get Raw Message',
+	ephemeral: true,
+	type: 3,
+	async execute(interaction) {
+		// Get embed and check if it exists
+		const content = interaction.message.content;
+		if (!content) return interaction.reply({ content: 'There is no text in this message!', ephemeral: true });
+
+		// If the json string is too long, put it in hastebin, otherwise just send it
+		if (`\`\`\`json\n${content}\n\`\`\``.length > 2000) interaction.reply({ content: await createPaste(content, { server: 'https://bin.birdflop.com' }), ephemeral: true });
+		else interaction.reply({ content: `\`\`\`json\n${content}\n\`\`\``, ephemeral: true });
+	},
+};
