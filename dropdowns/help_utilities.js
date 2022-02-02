25cc04361c1857afcb28bd5d363bbdf51fd7865e
Saboor <42164502+saboooor@users.noreply.github.com>
2022-02-02 11:59:01 -0700

    add try-catch to every dropdown and fix nsfw dm

--- dropdowns/help_utilities.js
+++ dropdowns/help_utilities.js
@@ -2,14 +2,19 @@ const { MessageEmbed } = require('discord.js');
 module.exports = {
 	name: 'help_utilities',
 	async execute(interaction, client) {
-		const srvconfig = await client.getData('settings', 'guildId', interaction.guild.id);
-		const prefix = srvconfig.prefix.replace(/([^\\]|^|\*|_|`|~)(\*|_|`|~)/g, '$1\\$2');
-		const Embed = new MessageEmbed()
-			.setColor(Math.floor(Math.random() * 16777215))
-			.setTitle('**HELP**');
-		require('../help/utilities.js')(prefix, Embed);
-		interaction.message.components[0].components[0].options.forEach(option => option.default = false);
-		interaction.message.components[0].components[0].options[5].default = true;
-		interaction.reply({ embeds: [Embed], components: interaction.message.components });
-	},
+		try {
+			const srvconfig = await client.getData('settings', 'guildId', interaction.guild.id);
+			const prefix = srvconfig.prefix.replace(/([^\\]|^|\*|_|`|~)(\*|_|`|~)/g, '$1\\$2');
+			const Embed = new MessageEmbed()
+				.setColor(Math.floor(Math.random() * 16777215))
+				.setTitle('**HELP**');
+			require('../help/utilities.js')(prefix, Embed);
+			interaction.message.components[0].components[0].options.forEach(option => option.default = false);
+			interaction.message.components[0].components[0].options[5].default = true;
+			interaction.reply({ embeds: [Embed], components: interaction.message.components });
+		}
+		catch (err) {
+			client.error(err, interaction);
+		}
+},
 };
