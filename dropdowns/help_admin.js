ce26802b9cd2aef28e4e7ae16dcc0f318e212ba7
Saboor <42164502+saboooor@users.noreply.github.com>
2022-02-02 12:01:27 -0700

    bruh

--- dropdowns/help_admin.js
+++ dropdowns/help_admin.js
@@ -8,7 +8,7 @@ module.exports = {
 			const Embed = new MessageEmbed()
 				.setColor(Math.floor(Math.random() * 16777215))
 				.setTitle('**HELP**');
-			require('../help/admin.js')(prefix, Embed, await client.getData('settings', 'guildId', interaction.guild.id));
+			require('../help/admin.js')(prefix, Embed, srvconfig);
 			interaction.message.components[0].components[0].options.forEach(option => option.default = false);
 			interaction.message.components[0].components[0].options[0].default = true;
 			interaction.reply({ embeds: [Embed], components: interaction.message.components });
