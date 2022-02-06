dcff84b2509222dee0a495fe7832a03422695ee9
Saboor <saboor547@gmail.com>
2022-02-06 10:10:07 -0700

    fix 2

--- events/music/trackError.js
+++ events/music/trackError.js
@@ -5,10 +5,10 @@ module.exports = async (client, player, track, payload) => {
 	const channel = guild.channels.cache.get(player.textChannel);
 	const thing = new MessageEmbed()
 		.setColor('RED')
-		.setDescription(`❌ Failed to load track`);
+		.setDescription('❌ Failed to load track');
 	const msg = await channel.send({ embeds: [thing] });
 	client.logger.error(payload.error);
-	client.logger.error(`Failed to load ${track.title} in ${guild.name}`);
+	client.logger.error(`Failed to load track in ${guild.name}`);
 	if (!player.queue.current) player.destroy();
 	await sleep(30000);
 	msg.delete();
