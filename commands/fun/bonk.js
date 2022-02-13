56e99e337358cd22fdac36bf09d1a93a13832a39
Saboor <saboor547@gmail.com>
2022-02-13 16:23:59 -0700

    fix bonk cmd

--- commands/fun/bonk.js
+++ commands/fun/bonk.js
@@ -7,19 +7,20 @@ module.exports = {
 	async execute(message, args, client) {
 		try {
 			// Check if arg is a user and set it
+			let user = null;
 			if (args[0]) {
-				const user = client.users.cache.get(args[0].replace(/\D/g, ''));
-				if (user) args[0] = user;
+				user = client.users.cache.get(args[0].replace(/\D/g, ''));
+				if (user) args[0] = user.username;
 			}
 
 			// Create embed with bonk gif and author / footer
 			const BonkEmbed = new Embed()
-				.setAuthor({ name: `${message.member.displayName} bonks ${args[0] ? args[0].username ? args[0].username : args.join(' ') : 'themselves'}`, iconURL: message.member.user.avatarURL() })
+				.setAuthor({ name: `${message.member.displayName} bonks ${args[0] ? args.join(' ') : 'themselves'}`, iconURL: message.member.user.avatarURL() })
 				.setImage('https://c.tenor.com/TbLpG9NCzjkAAAAC/bonk.gif')
 				.setFooter({ text: 'get bonked' });
 
 			// Reply with bonk message, if user is set then mention the user
-			message.reply({ content: args[0].username ? args[0] : null, embeds: [BonkEmbed] });
+			message.reply({ content: user ? `${user}` : null, embeds: [BonkEmbed] });
 		}
 		catch (err) {
 			client.error(err, message);
