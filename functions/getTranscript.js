5a3db255409f445a9afa6db4da3726ee9214f8d7
saboooor <saboor547@gmail.com>
2022-02-25 10:36:29 -0700

    fix getTranscript

--- functions/getTranscript.js
+++ functions/getTranscript.js
@@ -5,10 +5,12 @@ module.exports = async function getTranscript(messages) {
 		const time = new Date(msg.createdTimestamp).toLocaleString('default', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
 		msg.embeds.forEach(MsgEmbed => {
 			if (MsgEmbed.footer) logs.push(`${MsgEmbed.footer.text}`);
-			MsgEmbed.fields.forEach(field => {
-				logs.push(`${field.value}`);
-				logs.push(`${field.name}`);
-			});
+			if (MsgEmbed.fields) {
+				MsgEmbed.fields.forEach(field => {
+					logs.push(`${field.value}`);
+					logs.push(`${field.name}`);
+				});
+			}
 			if (MsgEmbed.description) logs.push(`${MsgEmbed.description}`);
 			if (MsgEmbed.title) logs.push(`${MsgEmbed.title}`);
 			if (MsgEmbed.author) logs.push(`${MsgEmbed.author.name}`);
