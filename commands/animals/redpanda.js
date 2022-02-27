6f85b6106dad1fab456962f3ff2c13f11d139e06
Saboor <saboor547@gmail.com>
2022-02-27 12:30:40 -0700

    add v.redd.it support and panda doesn't work just red panda now

--- commands/animals/redpanda.js
+++ commands/animals/redpanda.js
@@ -1,12 +1,12 @@
 const redditFetch = require('../../functions/redditFetch.js');
 module.exports = {
-	name: 'panda',
-	description: 'cute panda ya woo',
-	aliases: ['pandas'],
+	name: 'redpanda',
+	description: 'cute red pandas ya woo',
+	aliases: ['redpandas'],
 	async execute(message, args, client) {
 		try {
-			// Get from r/panda with the redditFetch function
-			redditFetch(['panda', 'redpandas'], message, client);
+			// Get from r/redpandas with the redditFetch function
+			redditFetch(['redpandas'], message, client);
 		}
 		catch (err) {
 			client.error(err, message);
