9053372393479b0618e31426c5e0c247d1c129ee
Saboor <saboor547@gmail.com>
2022-02-27 11:52:03 -0700

    add panda and meerkat

---
+++ commands/animals/meerkat.js
@@ -0,0 +1,15 @@
+const redditFetch = require('../../functions/redditFetch.js');
+module.exports = {
+	name: 'meerkat',
+	description: 'yes meerkat',
+	aliases: ['meerkats'],
+	async execute(message, args, client) {
+		try {
+			// Get from r/meerkats with the redditFetch function
+			redditFetch(['meerkats'], message, client);
+		}
+		catch (err) {
+			client.error(err, message);
+		}
+	},
+};
