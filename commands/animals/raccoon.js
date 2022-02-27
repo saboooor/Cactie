f06ef6a59098b5730f418b4b871adc2e96a9a90f
Saboor <saboor547@gmail.com>
2022-02-27 12:49:36 -0700

    add raccoon command

---
+++ commands/animals/raccoon.js
@@ -0,0 +1,15 @@
+const redditFetch = require('../../functions/redditFetch.js');
+module.exports = {
+	name: 'raccoon',
+	description: 'yes meerkat',
+	aliases: ['raccoons'],
+	async execute(message, args, client) {
+		try {
+			// Get from r/Raccoons with the redditFetch function
+			redditFetch(['Raccoons', 'raccoonfanclub', 'trashpandas'], message, client);
+		}
+		catch (err) {
+			client.error(err, message);
+		}
+	},
+};
