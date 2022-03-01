c3f1f57471c5f859baeea44acc5d5441b5f78e7d
Saboor <saboor547@gmail.com>
2022-02-28 22:04:38 -0700

    👁️🫦👁️

--- handlers/mysql.js
+++ handlers/mysql.js
@@ -20,7 +20,7 @@ module.exports = async client => {
 
 	// Query function
 	client.query = function query(args) {
-		client.logger.info('Query: ' + args);
+		if (!args.startsWith('SELECT *')) client.logger.info('Query: ' + args);
 		return new Promise((resolve, reject) => {
 			client.con.query(args, (err, rows, fields) =>{
 				if(err) return err;
