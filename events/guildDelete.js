da4a0762946c5754998b3b94cab472a855cb877d
Saboor <saboor547@gmail.com>
2022-02-28 19:58:37 -0700

    delete reaction role and ticket data on kick

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -2,6 +2,8 @@ const { Embed } = require('discord.js');
 module.exports = async (client, guild) => {
 	if (!guild.available) return;
 	client.delData('settings', 'guildId', guild.id);
+	client.delData('reactionroles', 'guildId', guild.id);
+	client.delData('ticketdata', 'guildId', guild.id);
 	client.logger.info(`${client.user.username} has been removed from ${guild.name}`);
 	const owner = await guild.fetchOwner();
 	const timestamp = Math.round(guild.createdTimestamp / 1000);
