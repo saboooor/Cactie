7fb5482af176b821aa6058a334636d5c78cec1db
Saboor <saboor547@gmail.com>
2022-02-28 21:12:05 -0700

    animal commands don't need any description fuck that

--- help/animals.js
+++ help/animals.js
@@ -1,11 +1,14 @@
 const fs = require('fs');
 module.exports = (prefix, HelpEmbed) => {
 	const funCommands = fs.readdirSync('./commands/animals').filter(file => file.endsWith('.js'));
+	const commands = [];
 	for (const file of funCommands) {
 		const command = require(`../commands/animals/${file}`);
-		HelpEmbed.addField({ name: `${prefix}${command.name}`, value: `${command.aliases ? `\n(Aliases: ${command.aliases.join(', ')})` : ''}\n${command.description}` });
+		commands.push(`${prefix}${command.name}`);
 	}
 	HelpEmbed.setDescription(`
 **ANIMALS:**
+
+**${commands.join(', ')}**
 `);
 };
