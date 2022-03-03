d0bf74a92c5d7836b2c017388de4d2c76ef36750
Saboor <saboor547@gmail.com>
2022-03-02 17:28:19 -0700

    better now playing format

--- events/music/trackStart.js
+++ events/music/trackStart.js
@@ -10,7 +10,8 @@ module.exports = async (client, player, track) => {
 	if (!player.lyrics) player.lyrics = 'Lyrics not found.';
 	if (!player.voiceChannel) return;
 	const thing = new MessageEmbed()
-		.setDescription(`<:play:${play}> **Started Playing**\n[${track.title}](${track.uri})\n\`[${convertTime(track.duration).replace('7:12:56', 'LIVE')}]\` [${track.requester}]`)
+		.setDescription(`<:play:${play}> **Started Playing** \`[${convertTime(track.duration).replace('7:12:56', 'LIVE')}]\`\n[${track.title}](${track.uri})`)
+		.setFooter({ text: track.requester.tag, iconURL: track.requester.displayAvatarURL() })
 		.setThumbnail(track.img)
 		.setColor(track.color)
 		.setTimestamp();
