3c88b40d33413a92b15ef358b4fdfe5d72f747da
Saboor <saboor547@gmail.com>
2022-03-04 12:19:29 -0700

    minimize code for button rows and columns

--- commands/private/buttons.js
+++ commands/private/buttons.js
@@ -4,114 +4,19 @@ module.exports = {
 	name: 'buttons',
 	description: 'buttons (for testing)',
 	async execute(message) {
-		const btns = {
-			ticone: new ButtonComponent()
-				.setCustomId('ticone')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			tictwo: new ButtonComponent()
-				.setCustomId('tictwo')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			ticthree: new ButtonComponent()
-				.setCustomId('ticthree')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			ticfour: new ButtonComponent()
-				.setCustomId('ticfour')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			ticfive: new ButtonComponent()
-				.setCustomId('ticfive')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			tacone: new ButtonComponent()
-				.setCustomId('tacone')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			tactwo: new ButtonComponent()
-				.setCustomId('tactwo')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			tacthree: new ButtonComponent()
-				.setCustomId('tacthree')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			tacfour: new ButtonComponent()
-				.setCustomId('tacfour')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			tacfive: new ButtonComponent()
-				.setCustomId('tacfive')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			toeone: new ButtonComponent()
-				.setCustomId('toeone')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			toetwo: new ButtonComponent()
-				.setCustomId('toetwo')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			toethree: new ButtonComponent()
-				.setCustomId('toethree')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			toefour: new ButtonComponent()
-				.setCustomId('toefour')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			toefive: new ButtonComponent()
-				.setCustomId('toefive')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			fourone: new ButtonComponent()
-				.setCustomId('fourone')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			fourtwo: new ButtonComponent()
-				.setCustomId('fourtwo')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			fourthree: new ButtonComponent()
-				.setCustomId('fourthree')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			fourfour: new ButtonComponent()
-				.setCustomId('fourfour')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			fourfive: new ButtonComponent()
-				.setCustomId('fourfive')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			fiveone: new ButtonComponent()
-				.setCustomId('fiveone')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			fivetwo: new ButtonComponent()
-				.setCustomId('fivetwo')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			fivethree: new ButtonComponent()
-				.setCustomId('fivethree')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			fivefour: new ButtonComponent()
-				.setCustomId('fivefour')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			fivefive: new ButtonComponent()
-				.setCustomId('fivefive')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-		};
-		const tic = new ActionRow().addComponents(btns.ticone, btns.tictwo, btns.ticthree, btns.ticfour, btns.ticfive);
-		const tac = new ActionRow().addComponents(btns.tacone, btns.tactwo, btns.tacthree, btns.tacfour, btns.tacfive);
-		const toe = new ActionRow().addComponents(btns.toeone, btns.toetwo, btns.toethree, btns.toefour, btns.toefive);
-		const four = new ActionRow().addComponents(btns.fourone, btns.fourtwo, btns.fourthree, btns.fourfour, btns.fourfive);
-		const five = new ActionRow().addComponents(btns.fiveone, btns.fivetwo, btns.fivethree, btns.fivefour, btns.fivefive);
-		const msg = await message.reply({ content: '\u200b', components: [tic, tac, toe, four, five] });
+		const btns = {};
+		const rows = [];
+		for (let row = 0; row < 5; row++) {
+			rows.push(new ActionRow());
+			for (let column = 0; column < 5; column++) {
+				btns[`${column}${row}`] = new ButtonComponent()
+					.setCustomId(`${column}${row}`)
+					.setEmoji({ id: empty })
+					.setStyle(ButtonStyle.Secondary);
+				rows[row].addComponents(btns[`${column}${row}`]);
+			}
+		}
+		const msg = await message.reply({ content: '\u200b', components: rows });
 
 		const collector = msg.createMessageComponentCollector({ time: 36000000 });
 
@@ -126,7 +31,7 @@ module.exports = {
 				btn.setStyle(ButtonStyle.Secondary)
 					.setEmoji({ id: o });
 			}
-			msg.edit({ components: [tic, tac, toe, four, five] });
+			msg.edit({ components: rows });
 		});
 
 		collector.on('end', () => msg.edit({ content: 'A game of tic tac toe should not last longer than an hour are you high', components: [] }));
--- commands/private/xo.js
+++ commands/private/xo.js
@@ -4,48 +4,19 @@ module.exports = {
 	name: 'xo',
 	description: 'tic tac toe (for testing)',
 	async execute(message) {
-		const btns = {
-			ticone: new ButtonComponent()
-				.setCustomId('ticone')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			tictwo: new ButtonComponent()
-				.setCustomId('tictwo')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			ticthree: new ButtonComponent()
-				.setCustomId('ticthree')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			tacone: new ButtonComponent()
-				.setCustomId('tacone')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			tactwo: new ButtonComponent()
-				.setCustomId('tactwo')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			tacthree: new ButtonComponent()
-				.setCustomId('tacthree')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			toeone: new ButtonComponent()
-				.setCustomId('toeone')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			toetwo: new ButtonComponent()
-				.setCustomId('toetwo')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-			toethree: new ButtonComponent()
-				.setCustomId('toethree')
-				.setEmoji({ id: empty })
-				.setStyle(ButtonStyle.Secondary),
-		};
-		const tic = new ActionRow().addComponents(btns.ticone, btns.tictwo, btns.ticthree);
-		const tac = new ActionRow().addComponents(btns.tacone, btns.tactwo, btns.tacthree);
-		const toe = new ActionRow().addComponents(btns.toeone, btns.toetwo, btns.toethree);
-		const msg = await message.reply({ content: '\u200b', components: [tic, tac, toe] });
+		const btns = {};
+		const rows = [];
+		for (let row = 0; row < 3; row++) {
+			rows.push(new ActionRow());
+			for (let column = 0; column < 3; column++) {
+				btns[`${column}${row}`] = new ButtonComponent()
+					.setCustomId(`${column}${row}`)
+					.setEmoji({ id: empty })
+					.setStyle(ButtonStyle.Secondary);
+				rows[row].addComponents(btns[`${column}${row}`]);
+			}
+		}
+		const msg = await message.reply({ content: '\u200b', components: rows });
 
 		const collector = msg.createMessageComponentCollector({ time: 36000000 });
 
@@ -56,7 +27,7 @@ module.exports = {
 				btn.setStyle(ButtonStyle.Danger)
 					.setEmoji({ id: x });
 			}
-			msg.edit({ components: [tic, tac, toe] });
+			msg.edit({ components: rows });
 		});
 
 		collector.on('end', () => msg.edit({ content: 'A game of tic tac toe should not last longer than an hour are you high', components: [] }));
