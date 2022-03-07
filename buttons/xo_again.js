a699e05b368e045922b7fea1018a05114ae7a83b
Saboor <saboor547@gmail.com>
2022-03-07 00:45:32 -0700

    i have no idea what i'm doing but i hope this oddly fixes xo

--- buttons/xo_again.js
+++ buttons/xo_again.js
@@ -9,20 +9,19 @@ function EndXO(btns, a, b, c, TicTacToe, msg, xuser, ouser, rows) {
 	TicTacToe.setColor(xwin ? 0xff0000 : 0x0000ff)
 		.setFields({ name: 'Result:', value: `${xwin ? xuser : ouser} wins!` })
 		.setThumbnail(xwin ? xuser.avatarURL() : ouser.avatarURL());
-	rows.push(again);
 	msg.edit({ content: `${xwin ? xuser : ouser}`, embeds: [TicTacToe], components: rows, allowedMentions: { repliedUser: xwin } });
 }
-const again = new ActionRow()
-	.addComponents(new ButtonComponent()
-		.setCustomId('xo_again')
-		.setEmoji({ id: refresh })
-		.setLabel('Play Again')
-		.setStyle(ButtonStyle.Secondary),
-	);
 module.exports = {
 	name: 'xo_again',
 	ephemeral: true,
 	async execute(interaction) {
+		const again = new ActionRow()
+			.addComponents(new ButtonComponent()
+				.setCustomId('xo_again')
+				.setEmoji({ id: refresh })
+				.setLabel('Play Again')
+				.setStyle(ButtonStyle.Secondary),
+			);
 		const TicTacToe = interaction.message.embeds[0];
 		const lines = TicTacToe.description.split('\n');
 		const xuserId = lines[0].split('**X:** ')[1].replace(/\D/g, '');
@@ -69,16 +68,16 @@ module.exports = {
 			const reslist = Object.keys(btns).map(i => { return `${btns[i].data.style}`; });
 
 			// horizontal
-			if (reslist[0] == reslist[1] && reslist[1] == reslist[2] && reslist[0] != ButtonStyle.Secondary) return EndXO(btns, 11, 12, 13, TicTacToe, msg, xuser.user, ouser.user, rows) && collector.stop();
-			if (reslist[3] == reslist[4] && reslist[4] == reslist[5] && reslist[3] != ButtonStyle.Secondary) return EndXO(btns, 21, 22, 23, TicTacToe, msg, xuser.user, ouser.user, rows) && collector.stop();
-			if (reslist[6] == reslist[7] && reslist[7] == reslist[8] && reslist[6] != ButtonStyle.Secondary) return EndXO(btns, 31, 32, 33, TicTacToe, msg, xuser.user, ouser.user, rows) && collector.stop();
+			if (reslist[0] == reslist[1] && reslist[1] == reslist[2] && reslist[0] != ButtonStyle.Secondary) return EndXO(btns, 11, 12, 13, TicTacToe, msg, xuser.user, ouser.user, rows) && rows.push(again) && collector.stop();
+			if (reslist[3] == reslist[4] && reslist[4] == reslist[5] && reslist[3] != ButtonStyle.Secondary) return EndXO(btns, 21, 22, 23, TicTacToe, msg, xuser.user, ouser.user, rows) && rows.push(again) && collector.stop();
+			if (reslist[6] == reslist[7] && reslist[7] == reslist[8] && reslist[6] != ButtonStyle.Secondary) return EndXO(btns, 31, 32, 33, TicTacToe, msg, xuser.user, ouser.user, rows) && rows.push(again) && collector.stop();
 			// diagonal
-			if (reslist[0] == reslist[4] && reslist[4] == reslist[8] && reslist[0] != ButtonStyle.Secondary) return EndXO(btns, 11, 22, 33, TicTacToe, msg, xuser.user, ouser.user, rows) && collector.stop();
-			if (reslist[6] == reslist[4] && reslist[4] == reslist[2] && reslist[6] != ButtonStyle.Secondary) return EndXO(btns, 13, 22, 31, TicTacToe, msg, xuser.user, ouser.user, rows) && collector.stop();
+			if (reslist[0] == reslist[4] && reslist[4] == reslist[8] && reslist[0] != ButtonStyle.Secondary) return EndXO(btns, 11, 22, 33, TicTacToe, msg, xuser.user, ouser.user, rows) && rows.push(again) && collector.stop();
+			if (reslist[6] == reslist[4] && reslist[4] == reslist[2] && reslist[6] != ButtonStyle.Secondary) return EndXO(btns, 13, 22, 31, TicTacToe, msg, xuser.user, ouser.user, rows) && rows.push(again) && collector.stop();
 			// vertical
-			if (reslist[0] == reslist[3] && reslist[3] == reslist[6] && reslist[0] != ButtonStyle.Secondary) return EndXO(btns, 11, 21, 31, TicTacToe, msg, xuser.user, ouser.user, rows) && collector.stop();
-			if (reslist[1] == reslist[4] && reslist[4] == reslist[7] && reslist[1] != ButtonStyle.Secondary) return EndXO(btns, 12, 22, 32, TicTacToe, msg, xuser.user, ouser.user, rows) && collector.stop();
-			if (reslist[2] == reslist[5] && reslist[5] == reslist[8] && reslist[2] != ButtonStyle.Secondary) return EndXO(btns, 13, 23, 33, TicTacToe, msg, xuser.user, ouser.user, rows) && collector.stop();
+			if (reslist[0] == reslist[3] && reslist[3] == reslist[6] && reslist[0] != ButtonStyle.Secondary) return EndXO(btns, 11, 21, 31, TicTacToe, msg, xuser.user, ouser.user, rows) && rows.push(again) && collector.stop();
+			if (reslist[1] == reslist[4] && reslist[4] == reslist[7] && reslist[1] != ButtonStyle.Secondary) return EndXO(btns, 12, 22, 32, TicTacToe, msg, xuser.user, ouser.user, rows) && rows.push(again) && collector.stop();
+			if (reslist[2] == reslist[5] && reslist[5] == reslist[8] && reslist[2] != ButtonStyle.Secondary) return EndXO(btns, 13, 23, 33, TicTacToe, msg, xuser.user, ouser.user, rows) && rows.push(again) && collector.stop();
 
 			// check for draw
 			let draw = true;
