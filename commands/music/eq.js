8c9d2d8d514db199d7a4c8fbb55bb25f6677d555
saboooor <saboor547@gmail.com>
2022-03-07 11:00:05 -0700

    move all eq buttons to collector

--- commands/music/eq.js
+++ commands/music/eq.js
@@ -1,5 +1,7 @@
+function sleep(ms) { return new Promise(res => setTimeout(res, ms)); }
 const { Embed, ButtonComponent, ButtonStyle, ActionRow } = require('discord.js');
 const msg = require('../../lang/en/msg.json');
+const presets = require('../../lang/int/eqpresets.json');
 module.exports = {
 	name: 'eq',
 	description: 'Set Equalizer',
@@ -42,18 +44,31 @@ module.exports = {
 				// Get the player and EQ preset
 				const player = client.manager.get(interaction.guild.id);
 				const preset = interaction.customId.split('_')[1];
-				if (preset == 'clear') {
-					// Get the player and clear the EQ
-					await player.clearEQ();
 
+				// Clear the EQ before setting the new one
+				await player.clearEQ();
+
+				// Check if the preset is clear or not
+				if (preset == 'clear') {
 					// Update the message with the new EQ
 					EQEmbed.setDescription(msg.music.eq.btn.replace('-m', msg.off));
-					await EQMsg.edit({ embeds: [EQEmbed] });
 				}
+				else {
+					// Wait 30ms after clear cuz idk i have to do it
+					await sleep(30);
+
+					// Get bands from preset
+					const bands = presets[preset];
+					await player.setEQ(...bands);
+
+					// Update the message with the new EQ
+					EQEmbed.setDescription(msg.music.eq.btn.replace('-m', msg.music.eq[preset]));
+				}
+				await EQMsg.edit({ embeds: [EQEmbed] });
 			});
 
 			// When the collector stops, remove the undo button from it
-			collector.on('end', () => msg.edit({ components: [] }));
+			collector.on('end', () => EQMsg.edit({ components: [] }));
 		}
 		catch (err) {
 			client.error(err, message);
