ba972a298253ef942d0d0d339983a7ec98388536
Saboor <saboor547@gmail.com>
2022-03-08 22:21:15 -0700

    this better yes

--- events/interactionCreate/Button.js
+++ events/interactionCreate/Button.js
@@ -5,59 +5,60 @@ function clean(text) {
 }
 const msg = require('../../lang/en/msg.json');
 module.exports = async (client, interaction) => {
-	if (interaction.isButton()) {
-		// Get the button from the available buttons in the bot, if there isn't one, just return because discord will throw an error itself
-		const button = client.buttons.get(interaction.customId);
-		if (!button) return;
+	// Check if interaction is button
+	if (!interaction.isButton()) return;
 
-		// Check if bot has the permissions necessary to run the button
-		if (button.botperm && (!interaction.guild.me.permissions.has(PermissionsBitField.Flags[button.botperm]) || !interaction.guild.me.permissionsIn(interaction.channel).has(PermissionsBitField.Flags[button.botperm]))) {
-			client.logger.error(`Bot is missing ${button.botperm} permission from ${interaction.customId} in #${interaction.channel.name} at ${interaction.guild.name}`);
-			return interaction.reply({ content: `I don't have the ${button.botperm} permission!`, ephemeral: true }).catch(e => { client.logger.warn(e); });
+	// Get the button from the available buttons in the bot, if there isn't one, just return because discord will throw an error itself
+	const button = client.buttons.get(interaction.customId);
+	if (!button) return;
+
+	// Check if bot has the permissions necessary to run the button
+	if (button.botperm && (!interaction.guild.me.permissions.has(PermissionsBitField.Flags[button.botperm]) || !interaction.guild.me.permissionsIn(interaction.channel).has(PermissionsBitField.Flags[button.botperm]))) {
+		client.logger.error(`Bot is missing ${button.botperm} permission from ${interaction.customId} in #${interaction.channel.name} at ${interaction.guild.name}`);
+		return interaction.reply({ content: `I don't have the ${button.botperm} permission!`, ephemeral: true }).catch(e => { client.logger.warn(e); });
+	}
+
+	// Check if user has the permissions necessary to use the button
+	if (button.permission && interaction.user.id !== '249638347306303499' && (!interaction.member.permissions || !interaction.member.permissions.has(PermissionsBitField.Flags[button.permission]) || !interaction.member.permissionsIn(interaction.channel).has(PermissionsBitField.Flags[button.permission]))) {
+		client.logger.error(`User is missing ${button.permission} permission from ${interaction.customId} in #${interaction.channel.name} at ${interaction.guild.name}`);
+		return interaction.reply({ content: msg.permreq.replace('-p', button.permission), ephemeral: true }).catch(e => { client.logger.warn(e); });
+	}
+
+	// Get player
+	const player = client.manager.get(interaction.guild.id);
+
+	// Check if player exists and button needs it
+	if (button.player && (!player || !player.queue.current)) return client.error('There is no music playing.', interaction, true);
+
+	// Check if bot is server muted and button needs unmute
+	if (button.serverUnmute && interaction.guild.me.voice.serverMute) return client.error('I\'m server muted!', interaction, true);
+
+	// Check if user is in vc and button needs user to be in vc
+	if (button.inVoiceChannel && !interaction.member.voice.channel) return client.error('You must be in a voice channel!', interaction, true);
+
+	// Check if user is in the same vc as bot and button needs it
+	if (button.sameVoiceChannel && interaction.member.voice.channel !== interaction.guild.me.voice.channel) return client.error(`You must be in the same channel as ${client.user}!`, interaction, true);
+
+	// Defer and execute the button
+	try {
+		client.logger.info(`${interaction.user.tag} clicked button: ${button.name}, in ${interaction.guild.name}`);
+		if (!button.noDefer) {
+			await interaction[button.deferReply ? 'deferReply' : 'deferUpdate']({ ephemeral: button.ephemeral });
+			interaction.reply = interaction.editReply;
 		}
-
-		// Check if user has the permissions necessary to use the button
-		if (button.permission && interaction.user.id !== '249638347306303499' && (!interaction.member.permissions || !interaction.member.permissions.has(PermissionsBitField.Flags[button.permission]) || !interaction.member.permissionsIn(interaction.channel).has(PermissionsBitField.Flags[button.permission]))) {
-			client.logger.error(`User is missing ${button.permission} permission from ${interaction.customId} in #${interaction.channel.name} at ${interaction.guild.name}`);
-			return interaction.reply({ content: msg.permreq.replace('-p', button.permission), ephemeral: true }).catch(e => { client.logger.warn(e); });
-		}
-
-		// Get player
-		const player = client.manager.get(interaction.guild.id);
-
-		// Check if player exists and button needs it
-		if (button.player && (!player || !player.queue.current)) return client.error('There is no music playing.', interaction, true);
-
-		// Check if bot is server muted and button needs unmute
-		if (button.serverUnmute && interaction.guild.me.voice.serverMute) return client.error('I\'m server muted!', interaction, true);
-
-		// Check if user is in vc and button needs user to be in vc
-		if (button.inVoiceChannel && !interaction.member.voice.channel) return client.error('You must be in a voice channel!', interaction, true);
-
-		// Check if user is in the same vc as bot and button needs it
-		if (button.sameVoiceChannel && interaction.member.voice.channel !== interaction.guild.me.voice.channel) return client.error(`You must be in the same channel as ${client.user}!`, interaction, true);
-
-		// Defer and execute the button
-		try {
-			client.logger.info(`${interaction.user.tag} clicked button: ${button.name}, in ${interaction.guild.name}`);
-			if (!button.noDefer) {
-				await interaction[button.deferReply ? 'deferReply' : 'deferUpdate']({ ephemeral: button.ephemeral });
-				interaction.reply = interaction.editReply;
-			}
-			button.execute(interaction, client);
-		}
-		catch (err) {
-			const interactionFailed = new Embed()
-				.setColor(Math.floor(Math.random() * 16777215))
-				.setTitle('INTERACTION FAILED')
-				.setAuthor({ name: interaction.user.tag, iconURL: interaction.user.avatarURL() })
-				.addFields({ name: '**Type:**', value: 'Button' })
-				.addFields({ name: '**Interaction:**', value: button.name })
-				.addFields({ name: '**Error:**', value: clean(err) });
-			if (interaction.guild) interactionFailed.addFields({ name: '**Guild:**', value: interaction.guild.name }).addFields({ name: '**Channel:**', value: interaction.channel.name });
-			client.users.cache.get('249638347306303499').send({ embeds: [interactionFailed] });
-			interaction.user.send({ embeds: [interactionFailed] }).catch(e => { client.logger.warn(e); });
-			client.logger.error(err);
-		}
+		button.execute(interaction, client);
+	}
+	catch (err) {
+		const interactionFailed = new Embed()
+			.setColor(Math.floor(Math.random() * 16777215))
+			.setTitle('INTERACTION FAILED')
+			.setAuthor({ name: interaction.user.tag, iconURL: interaction.user.avatarURL() })
+			.addFields({ name: '**Type:**', value: 'Button' })
+			.addFields({ name: '**Interaction:**', value: button.name })
+			.addFields({ name: '**Error:**', value: clean(err) });
+		if (interaction.guild) interactionFailed.addFields({ name: '**Guild:**', value: interaction.guild.name }).addFields({ name: '**Channel:**', value: interaction.channel.name });
+		client.users.cache.get('249638347306303499').send({ embeds: [interactionFailed] });
+		interaction.user.send({ embeds: [interactionFailed] }).catch(e => { client.logger.warn(e); });
+		client.logger.error(err);
 	}
 };
--- events/interactionCreate/Command.js
+++ events/interactionCreate/Command.js
@@ -5,162 +5,163 @@ function clean(text) {
 }
 const msg = require('../../lang/en/msg.json');
 module.exports = async (client, interaction) => {
-	if (interaction.isChatInputCommand() || interaction.isContextMenuCommand()) {
-		// Get the command from the available slash cmds in the bot, if there isn't one, just return because discord will throw an error itself
-		const command = client.slashcommands.get(interaction.commandName);
-		if (!command) return;
+	// Check if interaction is command
+	if (!interaction.isChatInputCommand() || !interaction.isContextMenuCommand()) return;
 
-		// Make args variable from interaction options for compatibility with dash command code
-		// If command is context menu, set it to client instead since it's (interaction, client)
-		const args = interaction.isContextMenuCommand() ? client : interaction.options._hoistedOptions;
-		if (interaction.isContextMenuCommand()) {
-			// Set message variable to the message of the context menu
-			const msgs = await interaction.channel.messages.fetch({ around: interaction.targetId, limit: 1 });
-			interaction.message = msgs.first();
+	// Get the command from the available slash cmds in the bot, if there isn't one, just return because discord will throw an error itself
+	const command = client.slashcommands.get(interaction.commandName);
+	if (!command) return;
+
+	// Make args variable from interaction options for compatibility with dash command code
+	// If command is context menu, set it to client instead since it's (interaction, client)
+	const args = interaction.isContextMenuCommand() ? client : interaction.options._hoistedOptions;
+	if (interaction.isContextMenuCommand()) {
+		// Set message variable to the message of the context menu
+		const msgs = await interaction.channel.messages.fetch({ around: interaction.targetId, limit: 1 });
+		interaction.message = msgs.first();
+	}
+	else {
+		// Set args to value of options
+		args.forEach(arg => args[args.indexOf(arg)] = arg.value);
+
+		// If subcommand exists, set the subcommand to args[0]
+		if (interaction.options._subcommand) args.unshift(interaction.options._subcommand);
+	}
+
+	// Get cooldowns and check if cooldown exists, if not, create it
+	const { cooldowns } = client;
+	if (!cooldowns.has(command.name)) cooldowns.set(command.name, new Collection());
+
+	// Get current timestamp and the command's last used timestamps
+	const now = Date.now();
+	const timestamps = cooldowns.get(command.name);
+
+	// Calculate the cooldown in milliseconds (default is 3600 miliseconds, idk why)
+	const cooldownAmount = (command.cooldown || 3) * 1200;
+
+	// Check if user is in the last used timestamp
+	if (timestamps.has(interaction.user.id)) {
+		// Get a random cooldown message
+		const messages = require('../lang/en/cooldown.json');
+		const random = Math.floor(Math.random() * messages.length);
+
+		// Get cooldown expiration timestamp
+		const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+
+		// If cooldown expiration hasn't passed, send cooldown message
+		if (now < expirationTime) {
+			const timeLeft = (expirationTime - now) / 1000;
+			const cooldownEmbed = new Embed()
+				.setColor(Math.floor(Math.random() * 16777215))
+				.setTitle(messages[random])
+				.setDescription(`wait ${timeLeft.toFixed(1)} more seconds before reusing the ${command.name} command.`);
+			return interaction.reply({ embeds: [cooldownEmbed], ephemeral: true });
+		}
+	}
+
+	// Set last used timestamp to now for user and delete the timestamp after cooldown passes
+	timestamps.set(interaction.user.id, now);
+	setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+
+	// Check if slash command is being sent in a DM, if so, send error message because commands in DMs are stupid
+	if (interaction.channel.isDM()) return client.error('You can\'t execute commands in DMs!', interaction, true);
+
+	// Get current settings for the guild
+	const srvconfig = await client.getData('settings', 'guildId', interaction.guild.id);
+
+	// Check if command can be ran only if the user voted since the past 24 hours
+	if (command.voteOnly && client.user.id == '765287593762881616') {
+		// Get vote data for user
+		const vote = await client.getData('lastvoted', 'userId', interaction.member.user.id);
+
+		// If user has not voted since the past 24 hours, send error message with vote buttons
+		if (Date.now() > vote.timestamp + 86400000) {
+			const errEmbed = new Embed().setTitle(`You need to vote to use ${command.name}! Vote below!`)
+				.setDescription('Voting helps us get Pup in more servers!\nIt\'ll only take a few seconds!');
+			const row = new ActionRow()
+				.addComponents(
+					new ButtonComponent()
+						.setURL('https://top.gg/bot/765287593762881616/vote')
+						.setLabel('top.gg')
+						.setStyle(ButtonStyle.Link),
+				)
+				.addComponents(
+					new ButtonComponent()
+						.setURL('https://discordbotlist.com/bots/pup/upvote')
+						.setLabel('dbl.com')
+						.setStyle(ButtonStyle.Link),
+				);
+			return interaction.reply({ embeds: [errEmbed], components: [row] });
+		}
+	}
+
+	// Check if user has the permissions necessary to use the command
+	if (command.permission) {
+		client.logger.info(JSON.stringify(interaction.member.permissions));
+		client.logger.info(command.permission);
+	}
+	if (command.permission && interaction.user.id !== '249638347306303499' && (!interaction.member.permissions || (!interaction.member.permissions.has(PermissionsBitField.Flags[command.permission]) && !interaction.member.permissionsIn(interaction.channel).has(PermissionsBitField.Flags[command.permission]) && !interaction.member.roles.cache.has(srvconfig.adminrole)))) {
+		if (command.permission == 'Administrator' && srvconfig.adminrole != 'permission') {
+			client.logger.error(`User is missing ${command.permission} permission (${srvconfig.adminrole}) from /${command.name} in #${interaction.channel.name} at ${interaction.guild.name}`);
+			return client.error(msg.rolereq.replace('-r', interaction.guild.roles.cache.get(srvconfig.adminrole).name), interaction, true);
 		}
 		else {
-			// Set args to value of options
-			args.forEach(arg => args[args.indexOf(arg)] = arg.value);
-
-			// If subcommand exists, set the subcommand to args[0]
-			if (interaction.options._subcommand) args.unshift(interaction.options._subcommand);
+			client.logger.error(`User is missing ${command.permission} permission from /${command.name} in #${interaction.channel.name} at ${interaction.guild.name}`);
+			return client.error(msg.permreq.replace('-p', command.permission), interaction, true);
 		}
+	}
 
-		// Get cooldowns and check if cooldown exists, if not, create it
-		const { cooldowns } = client;
-		if (!cooldowns.has(command.name)) cooldowns.set(command.name, new Collection());
+	// Check if bot has the permissions necessary to run the command
+	if (command.botperm && (!interaction.guild.me.permissions || (!interaction.guild.me.permissions.has(PermissionsBitField.Flags[command.botperm]) && !interaction.guild.me.permissionsIn(interaction.channel).has(PermissionsBitField.Flags[command.botperm])))) {
+		client.logger.error(`Bot is missing ${command.botperm} permission from /${command.name} in #${interaction.channel.name} at ${interaction.guild.name}`);
+		return client.error(`I don't have the ${command.botperm} permission!`, interaction, true);
+	}
 
-		// Get current timestamp and the command's last used timestamps
-		const now = Date.now();
-		const timestamps = cooldowns.get(command.name);
+	// Get player for music checks
+	const player = client.manager.get(interaction.guild.id);
 
-		// Calculate the cooldown in milliseconds (default is 3600 miliseconds, idk why)
-		const cooldownAmount = (command.cooldown || 3) * 1200;
+	// Check if player exists and command needs it
+	if (command.player && (!player || !player.queue.current)) return client.error('There is no music playing.', interaction, true);
 
-		// Check if user is in the last used timestamp
-		if (timestamps.has(interaction.user.id)) {
-			// Get a random cooldown message
-			const messages = require('../lang/en/cooldown.json');
-			const random = Math.floor(Math.random() * messages.length);
+	// Check if bot is server muted and command needs unmute
+	if (command.serverUnmute && interaction.guild.me.voice.serverMute) return client.error('I\'m server muted!', interaction, true);
 
-			// Get cooldown expiration timestamp
-			const expirationTime = timestamps.get(interaction.user.id) + cooldownAmount;
+	// Check if user is in vc and command needs user to be in vc
+	if (command.inVoiceChannel && !interaction.member.voice.channel) return client.error('You must be in a voice channel!', interaction, true);
 
-			// If cooldown expiration hasn't passed, send cooldown message
-			if (now < expirationTime) {
-				const timeLeft = (expirationTime - now) / 1000;
-				const cooldownEmbed = new Embed()
-					.setColor(Math.floor(Math.random() * 16777215))
-					.setTitle(messages[random])
-					.setDescription(`wait ${timeLeft.toFixed(1)} more seconds before reusing the ${command.name} command.`);
-				return interaction.reply({ embeds: [cooldownEmbed], ephemeral: true });
-			}
-		}
+	// Check if user is in the same vc as bot and command needs it
+	if (command.sameVoiceChannel && interaction.member.voice.channel !== interaction.guild.me.voice.channel) return client.error(`You must be in the same channel as ${client.user}!`, interaction, true);
 
-		// Set last used timestamp to now for user and delete the timestamp after cooldown passes
-		timestamps.set(interaction.user.id, now);
-		setTimeout(() => timestamps.delete(interaction.user.id), cooldownAmount);
+	// Check if user has dj role and command needs user to have it
+	if (command.djRole && srvconfig.djrole != 'false') {
+		// Get dj role, if it doesn't exist, send error message because invalid setting value
+		const role = interaction.guild.roles.cache.get(srvconfig.djrole);
+		if (!role) return interaction.reply({ content: msg.dj.notfound, ephemeral: true });
 
-		// Check if slash command is being sent in a DM, if so, send error message because commands in DMs are stupid
-		if (interaction.channel.isDM()) return client.error('You can\'t execute commands in DMs!', interaction, true);
+		// Check if user has role, if not, send error message
+		if (!interaction.member.roles.cache.has(srvconfig.djrole)) return client.error(msg.rolereq.replace('-r', role.name), interaction, true);
+	}
 
-		// Get current settings for the guild
-		const srvconfig = await client.getData('settings', 'guildId', interaction.guild.id);
-
-		// Check if command can be ran only if the user voted since the past 24 hours
-		if (command.voteOnly && client.user.id == '765287593762881616') {
-			// Get vote data for user
-			const vote = await client.getData('lastvoted', 'userId', interaction.member.user.id);
-
-			// If user has not voted since the past 24 hours, send error message with vote buttons
-			if (Date.now() > vote.timestamp + 86400000) {
-				const errEmbed = new Embed().setTitle(`You need to vote to use ${command.name}! Vote below!`)
-					.setDescription('Voting helps us get Pup in more servers!\nIt\'ll only take a few seconds!');
-				const row = new ActionRow()
-					.addComponents(
-						new ButtonComponent()
-							.setURL('https://top.gg/bot/765287593762881616/vote')
-							.setLabel('top.gg')
-							.setStyle(ButtonStyle.Link),
-					)
-					.addComponents(
-						new ButtonComponent()
-							.setURL('https://discordbotlist.com/bots/pup/upvote')
-							.setLabel('dbl.com')
-							.setStyle(ButtonStyle.Link),
-					);
-				return interaction.reply({ embeds: [errEmbed], components: [row] });
-			}
-		}
-
-		// Check if user has the permissions necessary to use the command
-		if (command.permission) {
-			client.logger.info(JSON.stringify(interaction.member.permissions));
-			client.logger.info(command.permission);
-		}
-		if (command.permission && interaction.user.id !== '249638347306303499' && (!interaction.member.permissions || (!interaction.member.permissions.has(PermissionsBitField.Flags[command.permission]) && !interaction.member.permissionsIn(interaction.channel).has(PermissionsBitField.Flags[command.permission]) && !interaction.member.roles.cache.has(srvconfig.adminrole)))) {
-			if (command.permission == 'Administrator' && srvconfig.adminrole != 'permission') {
-				client.logger.error(`User is missing ${command.permission} permission (${srvconfig.adminrole}) from /${command.name} in #${interaction.channel.name} at ${interaction.guild.name}`);
-				return client.error(msg.rolereq.replace('-r', interaction.guild.roles.cache.get(srvconfig.adminrole).name), interaction, true);
-			}
-			else {
-				client.logger.error(`User is missing ${command.permission} permission from /${command.name} in #${interaction.channel.name} at ${interaction.guild.name}`);
-				return client.error(msg.permreq.replace('-p', command.permission), interaction, true);
-			}
-		}
-
-		// Check if bot has the permissions necessary to run the command
-		if (command.botperm && (!interaction.guild.me.permissions || (!interaction.guild.me.permissions.has(PermissionsBitField.Flags[command.botperm]) && !interaction.guild.me.permissionsIn(interaction.channel).has(PermissionsBitField.Flags[command.botperm])))) {
-			client.logger.error(`Bot is missing ${command.botperm} permission from /${command.name} in #${interaction.channel.name} at ${interaction.guild.name}`);
-			return client.error(`I don't have the ${command.botperm} permission!`, interaction, true);
-		}
-
-		// Get player for music checks
-		const player = client.manager.get(interaction.guild.id);
-
-		// Check if player exists and command needs it
-		if (command.player && (!player || !player.queue.current)) return client.error('There is no music playing.', interaction, true);
-
-		// Check if bot is server muted and command needs unmute
-		if (command.serverUnmute && interaction.guild.me.voice.serverMute) return client.error('I\'m server muted!', interaction, true);
-
-		// Check if user is in vc and command needs user to be in vc
-		if (command.inVoiceChannel && !interaction.member.voice.channel) return client.error('You must be in a voice channel!', interaction, true);
-
-		// Check if user is in the same vc as bot and command needs it
-		if (command.sameVoiceChannel && interaction.member.voice.channel !== interaction.guild.me.voice.channel) return client.error(`You must be in the same channel as ${client.user}!`, interaction, true);
-
-		// Check if user has dj role and command needs user to have it
-		if (command.djRole && srvconfig.djrole != 'false') {
-			// Get dj role, if it doesn't exist, send error message because invalid setting value
-			const role = interaction.guild.roles.cache.get(srvconfig.djrole);
-			if (!role) return interaction.reply({ content: msg.dj.notfound, ephemeral: true });
-
-			// Check if user has role, if not, send error message
-			if (!interaction.member.roles.cache.has(srvconfig.djrole)) return client.error(msg.rolereq.replace('-r', role.name), interaction, true);
-		}
-
-		// Defer and execute the command
-		try {
-			const cmdlog = args.join ? `${command.name} ${args.join(' ')}` : command.name;
-			client.logger.info(`${interaction.user.tag} issued slash command: /${cmdlog}, in ${interaction.guild.name}`.replace(' ,', ','));
-			await interaction.deferReply({ ephemeral: command.ephemeral });
-			interaction.reply = interaction.editReply;
-			command.execute(interaction, args, client);
-		}
-		catch (err) {
-			const interactionFailed = new Embed()
-				.setColor(Math.floor(Math.random() * 16777215))
-				.setTitle('INTERACTION FAILED')
-				.setAuthor({ name: interaction.user.tag, iconURL: interaction.user.avatarURL() })
-				.addFields({ name: '**Type:**', value: 'Slash' })
-				.addFields({ name: '**Interaction:**', value: command.name })
-				.addFields({ name: '**Error:**', value: `${clean(err)}` })
-				.addFields({ name: '**Guild:**', value: interaction.guild.name })
-				.addFields({ name: '**Channel:**', value: interaction.channel.name });
-			client.users.cache.get('249638347306303499').send({ embeds: [interactionFailed] });
-			interaction.user.send({ embeds: [interactionFailed] }).catch(e => { client.logger.warn(e); });
-			client.logger.error(err);
-		}
+	// Defer and execute the command
+	try {
+		const cmdlog = args.join ? `${command.name} ${args.join(' ')}` : command.name;
+		client.logger.info(`${interaction.user.tag} issued slash command: /${cmdlog}, in ${interaction.guild.name}`.replace(' ,', ','));
+		await interaction.deferReply({ ephemeral: command.ephemeral });
+		interaction.reply = interaction.editReply;
+		command.execute(interaction, args, client);
+	}
+	catch (err) {
+		const interactionFailed = new Embed()
+			.setColor(Math.floor(Math.random() * 16777215))
+			.setTitle('INTERACTION FAILED')
+			.setAuthor({ name: interaction.user.tag, iconURL: interaction.user.avatarURL() })
+			.addFields({ name: '**Type:**', value: 'Slash' })
+			.addFields({ name: '**Interaction:**', value: command.name })
+			.addFields({ name: '**Error:**', value: `${clean(err)}` })
+			.addFields({ name: '**Guild:**', value: interaction.guild.name })
+			.addFields({ name: '**Channel:**', value: interaction.channel.name });
+		client.users.cache.get('249638347306303499').send({ embeds: [interactionFailed] });
+		interaction.user.send({ embeds: [interactionFailed] }).catch(e => { client.logger.warn(e); });
+		client.logger.error(err);
 	}
 };
