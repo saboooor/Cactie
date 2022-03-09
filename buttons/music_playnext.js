ab069bb527e20021018defa37f359644e1d1a48a
Saboor <saboor547@gmail.com>
2022-03-08 22:26:49 -0700

    client.logger.info

--- buttons/music_playnext.js
+++ buttons/music_playnext.js
@@ -6,7 +6,7 @@ module.exports = {
 			// Get the song link from the embed description
 			const a = interaction.message.embeds[0].description.split('](');
 			const b = a[a.length - 1].split(')')[0];
-			client.logger.log(b);
+			client.logger.info(b);
 
 			// Queue up the song in the embed, True is to playtop it
 			playSongs(interaction.member, interaction.message, [b], client, true);
