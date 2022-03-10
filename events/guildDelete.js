4fa11b5a59a34e9cb4cf930c7abfe481dd2406bb
Saboor <saboor547@gmail.com>
2022-03-09 19:14:13 -0700

    yes

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -1,6 +1,8 @@
 const { Embed } = require('discord.js');
 module.exports = async (client, guild) => {
 	if (!guild.available) return;
+	const player = client.manager.get(guild.id);
+	if (player) player.destroy();
 	client.delData('settings', 'guildId', guild.id);
 	client.delData('reactionroles', 'guildId', guild.id);
 	client.delData('ticketdata', 'guildId', guild.id);
