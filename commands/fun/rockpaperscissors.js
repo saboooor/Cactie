281868a9c9900c8010829fd0ef708ee3769e2cec
Saboor <saboor547@gmail.com>
2022-03-17 19:24:28 -0600

    better version yes

--- commands/fun/rockpaperscissors.js
+++ commands/fun/rockpaperscissors.js
@@ -12,30 +12,28 @@ module.exports = {
 		const user = message.guild.members.cache.get(args[0].replace(/\D/g, ''));
 		if (!user) return client.error(msg.invalidmember, message, true);
 		if (user.id == message.member.id) return client.error('You played yourself, oh wait, you can\'t.', message, true);
-		const row = new ActionRowBuilder().addComponents(
-			new ButtonBuilder()
-				.setCustomId('rock')
-				.setEmoji({ name: '🪨' })
-				.setLabel('Rock')
-				.setStyle(ButtonStyle.Secondary),
-			new ButtonBuilder()
-				.setCustomId('paper')
-				.setEmoji({ name: '📄' })
-				.setLabel('Paper')
-				.setStyle(ButtonStyle.Secondary),
-			new ButtonBuilder()
-				.setCustomId('scissors')
-				.setEmoji({ name: '✂' })
-				.setLabel('Scissors')
-				.setStyle(ButtonStyle.Secondary),
-		);
+		const emoji = {
+			rock: ['🪨', 'Rock', '🪨 Rock'],
+			paper: ['📄', 'Paper', '📄 Paper'],
+			scissors: ['✂️', 'Scissors', '✂️ Scissors'],
+		};
+		const row = new ActionRowBuilder();
+		Object.keys(emoji).map(i => {
+			row.addComponents(
+				new ButtonBuilder()
+					.setCustomId(i)
+					.setEmoji({ name: emoji[i][0] })
+					.setLabel(emoji[i][1])
+					.setStyle(ButtonStyle.Secondary),
+			);
+		});
 		const TicTacToe = new EmbedBuilder()
 			.setColor(Math.floor(Math.random() * 16777215))
 			.setTitle('Rock Paper Scissors')
 			.setDescription('Select an option!')
 			.setFields({ name: '**Waiting for:**', value: `${message.member}\n${user}` });
 
-		const rpsmsg = await message.reply({ content: `${message.member}\n${user}`, embeds: [TicTacToe], components: [row] });
+		const rpsmsg = await message.reply({ content: `${message.member} ${user}`, embeds: [TicTacToe], components: [row] });
 
 		const collector = rpsmsg.createMessageComponentCollector({ time: 3600000 });
 
@@ -44,30 +42,27 @@ module.exports = {
 			if (interaction.customId != 'rock' && interaction.customId != 'paper' && interaction.customId != 'scissors') return;
 			await interaction.deferReply({ ephemeral: true }).catch(err => client.logger.error(err));
 			if (interaction.user.id != message.member.id && interaction.user.id != user.id) return interaction.editReply({ content: 'You\'re not in this game!' });
-			if (choices[interaction.user.id]) return interaction.editReply({ content: `You've already selected ${choices[interaction.user.id]}!` });
+			if (choices[interaction.user.id]) return interaction.editReply({ content: `You've already selected ${emoji[choices[interaction.user.id]][2]}!` });
 			choices[interaction.user.id] = interaction.customId;
-			await interaction.editReply({ content: `Selected ${interaction.customId}!` });
+			await interaction.editReply({ content: `**Selected ${emoji[interaction.customId][2]}!**` });
 
 			if (interaction.user.id == message.member.id) TicTacToe.setFields({ name: '**Waiting for:**', value: `${user}` });
 			else if (interaction.user.id == user.id) TicTacToe.setFields({ name: '**Waiting for:**', value: `${message.member}` });
 
 			if (choices[message.member.id] && choices[user.id]) {
 				TicTacToe.setFields();
+				let win = true;
+				if (choices[user.id] == 'rock' && choices[message.member.id] == 'scissors') win = false;
+				else if (choices[user.id] == 'paper' && choices[message.member.id] == 'rock') win = false;
+				else if (choices[user.id] == 'scissors' && choices[message.member.id] == 'paper') win = false;
 				if (choices[message.member.id] == choices[user.id]) {
-					TicTacToe.setDescription(`**It's a tie!**\nBoth users picked ${choices[user.id]}!`);
+					TicTacToe.setDescription(`**It's a tie!**\nBoth users picked ${emoji[choices[user.id]][2]}!`);
+					return rpsmsg.edit({ embeds: [TicTacToe], components: [] });
 				}
-				else if (choices[message.member.id] == 'rock' && choices[user.id] == 'scissors') {
-					TicTacToe.setDescription(`**${message.member} wins!**\n${message.member} picked rock and ${user} picked scissors!`);
-				}
-				else if (choices[message.member.id] == 'paper' && choices[user.id] == 'rock') {
-					TicTacToe.setDescription(`**${message.member} wins!**\n${message.member} picked paper and ${user} picked rock!`);
-				}
-				else if (choices[message.member.id] == 'scissors' && choices[user.id] == 'paper') {
-					TicTacToe.setDescription(`**${message.member} wins!**\n${message.member} picked scissors and ${user} picked paper!`);
-				}
-				else {
-					TicTacToe.setDescription(`**${user} wins!**\n${message.member} picked ${choices[message.member.id]} and ${user} picked ${choices[user.id]}!`);
-				}
+				const winner = win ? message.member : user;
+				const loser = win ? user : message.member;
+				TicTacToe.setDescription(`**${winner} wins!**\n\n${emoji[choices[winner.id]][2]} wins over ${emoji[choices[loser.id]][2]}!`)
+					.setThumbnail(winner.user.avatarURL());
 				return rpsmsg.edit({ embeds: [TicTacToe], components: [] });
 			}
 
