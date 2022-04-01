f54afa80e2570e1236eeab5b02904309744b8ead
saboooor <saboor547@gmail.com>
2022-04-01 14:21:53 -0600

    don't skip if player moved idk

--- events/music/socketClosed.js
+++ events/music/socketClosed.js
@@ -1,6 +1,4 @@
 module.exports = async (client, player, payload) => {
 	const guild = client.guilds.cache.get(player.guild);
-	if (!payload.reason && !guild.me.voice.serverMute) player.stop();
-	if (!guild) return;
-	client.logger.error(`Socket has been closed because ${payload.reason} in ${guild.name}`);
+	client.logger.error(`Socket has been closed because ${payload.reason} in ${guild ? guild.name : player.guild}`);
 };
