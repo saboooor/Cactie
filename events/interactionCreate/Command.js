581af8ad89099a1243ffc6feabf38aae6f60afa5
saboooor <saboor547@gmail.com>
2022-04-03 17:15:32 -0600

    no mroe apleril foul

--- events/interactionCreate/Command.js
+++ events/interactionCreate/Command.js
@@ -65,7 +65,6 @@ module.exports = async (client, interaction) => {
 	const srvconfig = await client.getData('settings', 'guildId', interaction.guild.id);
 
 	// Get the language for the user if specified or guild language
-	srvconfig.language = 'Uwu';
 	const data = await client.query(`SELECT * FROM memberdata WHERE memberId = '${interaction.user.id}'`);
 	if (data[0]) interaction.lang = require(`../../lang/${data[0].language}/msg.json`);
 	else interaction.lang = require(`../../lang/${srvconfig.language}/msg.json`);
