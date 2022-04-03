8bc1a862bc0df4aa6771ba742ff0a3b8cabf84a9
saboooor <saboor547@gmail.com>
2022-04-03 17:40:02 -0600

    add join command move and force move command

--- commands/music/move.js
+++ commands/music/move.js
@@ -1,32 +1,24 @@
 const { EmbedBuilder } = require('discord.js');
 const { join } = require('../../lang/int/emoji.json');
 module.exports = {
-	name: 'join',
-	description: 'Join voice channel',
-	aliases: ['j'],
+	name: 'move',
+	description: 'Move Cactie from one voice channel to another',
+	aliases: ['mv'],
 	cooldown: 2,
+	player: true,
 	invc: true,
+	djRole: true,
 	async execute(message, args, client) {
 		try {
+			const srvconfig = await client.getData('settings', 'guildId', message.guild.id);
+			if (srvconfig.djrole == 'false') return client.error(message.lang.music.dj.notfound, message, true);
+
 			// Get the voice channel the user is in
 			const { channel } = message.member.voice;
 
-			let player = client.manager.get(message.guild.id);
-			if (player) {
-				if (!player.paused) return client.error('I am already currently playing music!\nTo force move me, use the move command', message, true);
-				player.voiceChannel = channel.id;
-				player.textChannel = message.channel.id;
-			}
-			else {
-				// Create player in that voice channel and connect to voice
-				player = client.manager.create({
-					guild: message.guild.id,
-					voiceChannel: channel.id,
-					textChannel: message.channel.id,
-					volume: 50,
-					selfDeafen: true,
-				});
-			}
+			const player = client.manager.get(message.guild.id);
+			player.voiceChannel = channel.id;
+			player.textChannel = message.channel.id;
 			player.connect();
 
 			// Send message to channel
