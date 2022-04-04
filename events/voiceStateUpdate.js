d36aa4deb1c14d595e9be2c7534d659ec0c51d44
saboooor <saboor547@gmail.com>
2022-04-03 18:09:53 -0600

    add notice for pause/play stuff idk

--- events/voiceStateUpdate.js
+++ events/voiceStateUpdate.js
@@ -1,3 +1,5 @@
+const { EmbedBuilder } = require('discord.js');
+const { play, pause } = require('../lang/int/emoji.json');
 module.exports = async (client, oldState, newState) => {
 	// get guild and player
 	const guildId = newState.guild.id;
@@ -12,14 +14,29 @@ module.exports = async (client, oldState, newState) => {
 	if (oldState.channel !== null && newState.channel === null) stateChange.type = 'LEAVE';
 	if (oldState.channel !== null && newState.channel !== null) stateChange.type = 'MOVE';
 	if (oldState.channel === null && newState.channel === null) return;
+	const song = player.queue.current;
+	const PauseEmbed = new EmbedBuilder()
+		.setDescription(`<:pause:${pause}> **Paused**\n[${song.title}](${song.uri})`)
+		.setColor(song.color)
+		.setThumbnail(song.img);
+	const ResEmbed = new EmbedBuilder()
+		.setDescription(`<:play:${play}> **Resumed**\n[${song.title}](${song.uri})`)
+		.setColor(song.color)
+		.setThumbnail(song.img);
 	if (newState.serverMute == true && oldState.serverMute == false && oldState.id == client.user.id) {
 		player.pause(true);
 		client.logger.info(`Paused player in ${newState.guild.name} because of server mute`);
+		PauseEmbed.setFooter({ text: 'Reason: The bot has been server muted' });
+		if (player.nowPlayingMessage) player.nowPlayingMessage.edit({ embeds: [PauseEmbed] });
+		else newState.guild.channels.cache.get(player.textChannel).send({ embeds: [PauseEmbed] });
 		return player.timeout = Date.now() + 300000;
 	}
 	if (newState.serverMute == false && oldState.serverMute == true && oldState.id == client.user.id) {
 		player.pause(false);
 		client.logger.info(`Resumed player in ${newState.guild.name} because of server unmute`);
+		ResEmbed.setFooter({ text: 'Reason: The bot has been server unmuted' });
+		if (player.nowPlayingMessage) player.nowPlayingMessage.edit({ embeds: [ResEmbed] });
+		else newState.guild.channels.cache.get(player.textChannel).send({ embeds: [ResEmbed] });
 		return player.timeout = null;
 	}
 
@@ -41,12 +58,18 @@ module.exports = async (client, oldState, newState) => {
 	if (stateChange.type == 'JOIN' && stateChange.members.size == 1) {
 		player.pause(false);
 		client.logger.info(`Resumed player in ${newState.guild.name} because of user join`);
+		ResEmbed.setFooter({ text: 'Reason: Someone joined the voice channel' });
+		if (player.nowPlayingMessage) player.nowPlayingMessage.edit({ embeds: [ResEmbed] });
+		else newState.guild.channels.cache.get(player.textChannel).send({ embeds: [ResEmbed] });
 		return player.timeout = null;
 	}
 
 	if (stateChange.type == 'LEAVE' && stateChange.members.size == 0) {
 		player.pause(true);
 		client.logger.info(`Paused player in ${newState.guild.name} because of empty channel`);
+		PauseEmbed.setFooter({ text: 'Reason: The bot has been left alone' });
+		if (player.nowPlayingMessage) player.nowPlayingMessage.edit({ embeds: [PauseEmbed] });
+		else newState.guild.channels.cache.get(player.textChannel).send({ embeds: [PauseEmbed] });
 		return player.timeout = Date.now() + 300000;
 	}
 
@@ -56,11 +79,17 @@ module.exports = async (client, oldState, newState) => {
 	if (deafened) {
 		player.pause(true);
 		client.logger.info(`Paused player in ${newState.guild.name} because of user deafen`);
+		PauseEmbed.setFooter({ text: 'Reason: Everyone\'s deafened' });
+		if (player.nowPlayingMessage) player.nowPlayingMessage.edit({ embeds: [PauseEmbed] });
+		else newState.guild.channels.cache.get(player.textChannel).send({ embeds: [PauseEmbed] });
 		return player.timeout = Date.now() + 300000;
 	}
 	else if (player.paused) {
 		player.pause(false);
 		client.logger.info(`Resumed player in ${newState.guild.name} because of user undeafen`);
+		ResEmbed.setFooter({ text: 'Reason: Someone undeafened themselves' });
+		if (player.nowPlayingMessage) player.nowPlayingMessage.edit({ embeds: [ResEmbed] });
+		else newState.guild.channels.cache.get(player.textChannel).send({ embeds: [ResEmbed] });
 		return player.timeout = null;
 	}
 };
