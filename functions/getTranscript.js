80e566b36516a873cf222fe08a0149a6cf544bd1
saboooor <saboor547@gmail.com>
2022-04-15 00:06:24 -0600

    new transcript system

--- functions/getTranscript.js
+++ functions/getTranscript.js
@@ -1,23 +1,36 @@
-const { createPaste } = require('hastebin');
+const fetch = (...args) => import('node-fetch').then(({ default: e }) => e(...args));
 module.exports = async function getTranscript(messages) {
-	const logs = [];
+	const logs = {
+		channel: messages.first().channel.name,
+		time: new Date().toLocaleString('default', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true }),
+		logs: [],
+	};
 	messages.forEach(async msg => {
-		const time = new Date(msg.createdTimestamp).toLocaleString('default', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+		const json = {
+			time: new Date(msg.createdTimestamp).toLocaleString('default', { weekday: 'long', month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true }),
+			author: {
+				color: msg.member ? (msg.member.roles.highest ? msg.member.roles.highest.color.toString(16) : 'ffffff') : 'ffffff',
+				name: msg.author.tag,
+				avatar: msg.author.displayAvatarURL(),
+			},
+		};
+		if (msg.embeds[0]) json.embeds = [];
+		if (msg.content) json.content = msg.content;
 		msg.embeds.forEach(MsgEmbed => {
-			if (MsgEmbed.footer) logs.push(`${MsgEmbed.footer.text}`);
-			if (MsgEmbed.fields) {
-				MsgEmbed.fields.forEach(field => {
-					logs.push(`${field.value}`);
-					logs.push(`${field.name}`);
-				});
-			}
-			if (MsgEmbed.description) logs.push(`${MsgEmbed.description}`);
-			if (MsgEmbed.title) logs.push(`${MsgEmbed.title}`);
-			if (MsgEmbed.author) logs.push(`${MsgEmbed.author.name}`);
+			const embedjson = {};
+			if (MsgEmbed.color) embedjson.color = MsgEmbed.color.toString(16);
+			if (MsgEmbed.author) embedjson.author = MsgEmbed.author.name;
+			if (MsgEmbed.title) embedjson.title = MsgEmbed.title;
+			if (MsgEmbed.description) embedjson.description = MsgEmbed.description;
+			if (MsgEmbed.fields) embedjson.fields = MsgEmbed.fields;
+			if (MsgEmbed.thumbnail) embedjson.thumb = MsgEmbed.thumbnail.url;
+			if (MsgEmbed.image) embedjson.image = MsgEmbed.image.url;
+			if (MsgEmbed.footer) embedjson.footer = MsgEmbed.footer.text;
+			json.embeds.push(embedjson);
 		});
-		if (msg.content) logs.push(`${msg.content}`);
-		logs.push(`\n[${time}] ${msg.author.tag}`);
+		logs.logs.push(json);
 	});
-	logs.reverse();
-	return await createPaste(logs.join('\n'), { server: 'https://bin.birdflop.com' });
+	logs.logs.reverse();
+	const balls = await fetch('https://files.smhsmh.club/transcript', { method: 'POST', body: JSON.stringify(logs), headers: { 'Content-Type': 'application/json' } });
+	return balls.statusText;
 };
