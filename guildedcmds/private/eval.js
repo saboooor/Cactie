eea36d28ab3f1b24d9be24c370483a07e79579c9
saboooor <saboor547@gmail.com>
2022-04-23 01:56:30 -0600

    add eval command

---
+++ guildedcmds/private/eval.js
@@ -0,0 +1,22 @@
+module.exports = {
+	name: 'eval',
+	description: 'Runs code specified in args',
+	aliases: ['ec'],
+	args: true,
+	usage: '<Code>',
+	cooldown: 0.1,
+	// eslint-disable-next-line no-unused-vars
+	async execute(message, args, client) {
+		// Check if user is sab lolololol
+		if (message.author.id !== 'AYzRpEe4') return client.error('You can\'t do that!', message, true);
+		try {
+			const code = args.join(' ');
+			let evaled = eval(code);
+			if (typeof evaled !== 'string') { evaled = require('util').inspect(evaled); }
+			message.reply({ content: evaled });
+		}
+		catch (err) {
+			message.reply({ content: `\`ERROR\` \`\`\`\n${err}\n\`\`\`` });
+		}
+	},
+};
