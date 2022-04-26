f8f6b995b197858394476529cd25b2bb423e633b
saboooor <saboor547@gmail.com>
2022-04-26 11:18:43 -0600

    fix

--- guildedcmds/private/embed.js
+++ guildedcmds/private/embed.js
@@ -1,10 +1,7 @@
 const { Embed } = require('guilded.js');
 module.exports = {
-	name: 'eval',
-	description: 'Runs code specified in args',
-	aliases: ['ec'],
-	args: true,
-	usage: '<Code>',
+	name: 'embed',
+	description: 'Embed',
 	cooldown: 0.1,
 	// eslint-disable-next-line no-unused-vars
 	async execute(message, args, client) {
