0caf1c61c5cf441f5996e999e543fbee68e0a171
Saboor <42164502+saboooor@users.noreply.github.com>
2022-04-27 12:22:13 -0600

    shorter code

--- handlers/commands.js
+++ handlers/commands.js
@@ -4,23 +4,17 @@ module.exports = client => {
 	let amount = 0;
 	client.cooldowns = new Collection();
 	client.commands = new Collection();
-	const commandFolders = readdirSync(`./commands/${client.type.name}`);
-	for (const folder of commandFolders) {
-		const commandFiles = readdirSync(`./commands/${client.type.name}/${folder}`).filter(file => file.endsWith('.js') && folder != 'options' && folder != 'message');
-		for (const file of commandFiles) {
-			const command = require(`../commands/${client.type.name}/${folder}/${file}`);
-			client.commands.set(command.name, command);
-			amount++;
-		}
-	}
-	const universalcmdFolders = readdirSync('./commands/universal');
-	for (const folder of universalcmdFolders) {
-		const universalcmdFiles = readdirSync(`./commands/universal/${folder}`).filter(file => file.endsWith('.js'));
-		for (const file of universalcmdFiles) {
-			const command = require(`../commands/universal/${folder}/${file}`);
-			client.commands.set(command.name, command);
-			amount++;
-		}
-	}
+	const types = [client.type.name, 'universal']
+	types.forEach(type => {
+		const commandFolders = readdirSync(`./commands/${type}`);
+		for (const folder of commandFolders) {
+			const commandFiles = readdirSync(`./commands/${type}/${folder}`).filter(file => file.endsWith('.js') && folder != 'options' && folder != 'message');
+			for (const file of commandFiles) {
+				const command = require(`../commands/${type}/${folder}/${file}`);
+				client.commands.set(command.name, command);
+				amount++;
+			}
+		}	
+	})
 	client.logger.info(`${amount} message commands loaded`);
 };
--- handlers/discord/slash.js
+++ handlers/discord/slash.js
@@ -3,23 +3,17 @@ const { Collection } = require('discord.js');
 module.exports = client => {
 	let amount = 0;
 	client.slashcommands = new Collection();
-	const slashcommandFolders = readdirSync('./commands/discord');
-	for (const folder of slashcommandFolders) {
-		const slashcommandFiles = readdirSync(`./commands/discord/${folder}`).filter(file => file.endsWith('.js') && folder != 'nsfw' && folder != 'options' && folder != 'private');
-		for (const file of slashcommandFiles) {
-			const slashcommand = require(`../../commands/discord/${folder}/${file}`);
-			client.slashcommands.set(slashcommand.name, slashcommand);
-			amount = amount + 1;
-		}
-	}
-	const unislashcommandFolders = readdirSync('./commands/universal');
-	for (const folder of unislashcommandFolders) {
-		const slashcommandFiles = readdirSync(`./commands/universal/${folder}`).filter(file => file.endsWith('.js') && folder != 'nsfw');
-		for (const file of slashcommandFiles) {
-			const slashcommand = require(`../../commands/universal/${folder}/${file}`);
-			client.slashcommands.set(slashcommand.name, slashcommand);
-			amount = amount + 1;
-		}
-	}
+	const types = ['discord', 'universal']
+	types.forEach(type => {
+		const slashcommandFolders = readdirSync(`./commands/${type}`);
+		for (const folder of slashcommandFolders) {
+			const slashcommandFiles = readdirSync(`./commands/${type}/${folder}`).filter(file => file.endsWith('.js') && folder != 'nsfw' && folder != 'options' && folder != 'private');
+			for (const file of slashcommandFiles) {
+				const slashcommand = require(`../../commands/${type}/${folder}/${file}`);
+				client.slashcommands.set(slashcommand.name, slashcommand);
+				amount++;
+			}
+		}	
+	})
 	client.logger.info(`${amount} slash commands loaded`);
 };
