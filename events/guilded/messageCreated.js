7d7d6f6eee810227ee91804f8521df3292d341dc
saboooor <saboor547@gmail.com>
2022-04-29 22:57:17 -0600

    fix for system messages

--- events/guilded/messageCreated.js
+++ events/guilded/messageCreated.js
@@ -2,7 +2,7 @@ const { Collection } = require('discord.js');
 const { Embed, UserType } = require('guilded.js');
 module.exports = async (client, message) => {
 	// Check if message is from a bot and if so return, and fetch member if not already fetched
-	if (message.createdByBotId || message.createdByWebhookId) return;
+	if (message.createdByBotId || message.createdByWebhookId || (!message.content && !message.embeds)) return;
 	message.member = client.members.cache.get(`${message.serverId}:${message.createdById}`);
 	if (!message.member) message.member = await client.members.fetch(message.serverId, message.createdById);
 	if (message.member.user.type == UserType.Bot) return;
