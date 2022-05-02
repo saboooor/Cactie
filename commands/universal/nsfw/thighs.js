0cd92b9ac6fb767ebb23b46b1c4b243483fee0ac
saboooor <saboor547@gmail.com>
2022-05-02 01:30:58 -0600

    banned

--- commands/universal/nsfw/thighs.js
+++ commands/universal/nsfw/thighs.js
@@ -2,10 +2,10 @@ const redditFetch = require('../../../functions/redditFetch.js');
 module.exports = {
 	name: 'thighs',
 	aliases: ['thigh'],
-	description: 'r/Thighs, r/ThickThighs, r/thick, r/thighdeology, r/Thigh, r/TheThiccness, r/curvy',
+	description: 'r/ThickThighs, r/thick, r/thighdeology, r/Thigh, r/TheThiccness, r/curvy',
 	async execute(message, args, client) {
 		try {
-			redditFetch(['Thighs', 'ThickThighs', 'thick', 'thighdeology', 'Thigh', 'TheThiccness', 'curvy'], message, client);
+			redditFetch(['ThickThighs', 'thick', 'thighdeology', 'Thigh', 'TheThiccness', 'curvy'], message, client);
 		}
 		catch (err) { client.error(err, message); }
 	},
