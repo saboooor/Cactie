0d04b2784ef3475843a62c415c28a2f919f04f34
saboooor <saboor547@gmail.com>
2022-05-02 00:04:46 -0600

    more progress stuff

--- functions/ffmpegSync.js
+++ functions/ffmpegSync.js
@@ -1,16 +1,26 @@
 const ffmpeg = require('fluent-ffmpeg');
-const { refresh } = require('../lang/int/emoji.json');
+const { refresh, warn } = require('../lang/int/emoji.json');
+let count = 0;
 module.exports = function ffmpegSync(url, path, msg) {
-	return new Promise((resolve, reject)=>{
-		ffmpeg(url.replace('.gifv', '.mp4'))
+	return new Promise(resolve =>{
+		const cmd = ffmpeg(url.replace('.gifv', '.mp4'))
 			.outputOption('-vf', 'scale=-1:360:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse,fps=24')
 			.save(path)
 			.on('end', () => resolve())
 			.on('start', () => msg.edit({ content: `<:refresh:${refresh}> **Downloading GIF...**` }))
+			.on('codecData', function(data) { cmd.duration = data.duration.replace(/:00/g, ''); })
 			.on('progress', (progress) => {
-				msg.edit({ content: `<:refresh:${refresh}> **Processing GIF... (${progress.frames} frames, ${progress.timemark})**` });
-				msg.client.logger.info(`Processing GIF... (${JSON.stringify(progress)})`);
+				if (count == 0) {
+					msg.edit({ content: `<:refresh:${refresh}> **Processing GIF... (${progress.frames} frames, ${progress.timemark.replace(/:00/g, '')} / ${cmd.duration})**` });
+					msg.client.logger.info(`Processing GIF... (${JSON.stringify(progress)})`);
+					count++;
+				}
+				else { count = 0; }
+				if (progress.targetSize > 50000) {
+					msg.edit({ content: `<:alert:${warn}> **GIF too big**` });
+					cmd.kill();
+				}
 			})
-			.on('error', (err) => { return reject(new Error(err)); });
+			.on('error', (err) => { return msg.client.logger.error(err); });
 	});
 };
