7527d0f6e5272e6e2da4526874173f8c5cb66a01
saboooor <saboor547@gmail.com>
2022-05-05 11:30:43 -0600

    update

--- events/discord/ready/guildowners.js
+++ events/discord/ready/guildowners.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
 	const pupguild = client.guilds.cache.get('811354612547190794');
-	const role = pupguild.roles.cache.get('811355441464475669');
+	const role = pupguild.roles.cache.find(r => r.name == `${client.user.name} User`);
 	const owners = [];
 	await client.guilds.cache.forEach(async guild => {
 		if (!owners.includes(guild.ownerId)) owners.push(guild.ownerId);
@@ -8,11 +8,11 @@ module.exports = async (client) => {
 		if (!member) return;
 		if (member.roles.cache.has(role.id)) return;
 		member.roles.add(role.id);
-		client.logger.info(`Added client user role to ${member.user.tag}`);
+		client.logger.info(`Added cactie user role to ${member.user.tag}`);
 	});
 	await role.members.forEach(async member => {
 		if (owners.includes(member.id)) return;
 		member.roles.remove(role.id);
-		client.logger.info(`Removed client user role from ${member.user.tag}`);
+		client.logger.info(`Removed cactie user role from ${member.user.tag}`);
 	});
 };
