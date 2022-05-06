5aa8e56f711042fdfb86271f25201171a9f7e563
Saboor <42164502+saboooor@users.noreply.github.com>
2022-05-06 12:49:17 -0600

    burh

--- commands/discord/utilities/invite.js
+++ commands/discord/utilities/invite.js
@@ -1,4 +1,26 @@
 const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
+const InvEmbed = new EmbedBuilder()
+	.setColor(Math.floor(Math.random() * 16777215))
+	.setTitle('Want to invite this bot to your server?')
+	.setDescription('Use the buttons below!');
+const row1 = new ActionRowBuilder()
+	.addComponents([
+		new ButtonBuilder()
+			.setURL('https://cactie.smhsmh.club/invite')
+			.setLabel('Invite Cactie to your server!')
+			.setStyle(ButtonStyle.Link),
+		new ButtonBuilder()
+			.setURL('https://cactie.smhsmh.club/discord')
+			.setLabel('Join the support server!')
+			.setStyle(ButtonStyle.Link),
+	]);
+const row2 = new ActionRowBuilder()
+	.addComponents([
+		new ButtonBuilder()
+			.setURL('https://netherdepths.com')
+			.setLabel('Also check out Nether Depths!')
+			.setStyle(ButtonStyle.Link),
+	]);
 module.exports = {
 	name: 'invite',
 	description: 'Get Cactie\'s invite links',
@@ -6,28 +28,6 @@ module.exports = {
 	cooldown: 10,
 	async execute(message, args, client) {
 		try {
-			const InvEmbed = new EmbedBuilder()
-				.setColor(Math.floor(Math.random() * 16777215))
-				.setTitle('Want to invite this bot to your server?')
-				.setDescription('Use the buttons below!');
-			const row1 = new ActionRowBuilder()
-				.addComponents([
-					new ButtonBuilder()
-						.setURL('https://cactie.smhsmh.club/invite')
-						.setLabel('Invite Cactie to your server!')
-						.setStyle(ButtonStyle.Link),
-					new ButtonBuilder()
-						.setURL('https://cactie.smhsmh.club/discord')
-						.setLabel('Join the support server!')
-						.setStyle(ButtonStyle.Link),
-				]);
-			const row2 = new ActionRowBuilder
-				.addComponents([
-					new ButtonBuilder()
-						.setURL('https://netherdepths.com')
-						.setLabel('Also check out Nether Depths!')
-						.setStyle(ButtonStyle.Link),
-				]);
 			await message.reply({ embeds: [InvEmbed], components: [row1, row2] });
 		}
 		catch (err) { client.error(err, message); }
--- commands/guilded/utilities/invite.js
+++ commands/guilded/utilities/invite.js
@@ -1,4 +1,11 @@
 const { EmbedBuilder } = require('discord.js');
+const InvEmbed = new EmbedBuilder()
+	.setColor(Math.floor(Math.random() * 16777215))
+	.addFields([
+		{ name: '**Add Cactie Bot to your server:**', value: '[Invite Cactie to your server using this link!](https://cactie.smhsmh.club/invite)' },
+		{ name: '**Bot Support:**', value: '[Join Cactie\'s guilded server!](https://guilded.gg/cactie)' },
+		{ name: '**Nether Depths:**', value: '[Also check out Nether Depths!](https://netherdepths.com)' },
+	]);
 module.exports = {
 	name: 'invite',
 	description: 'Get Cactie\'s invite links',
@@ -6,13 +13,6 @@ module.exports = {
 	cooldown: 10,
 	async execute(message, args, client) {
 		try {
-			const InvEmbed = new EmbedBuilder()
-				.setColor(Math.floor(Math.random() * 16777215))
-				.addFields([
-					{ name: '**Add Cactie Bot to your server:**', value: '[Invite Cactie to your server using this link!](https://cactie.smhsmh.club/invite)' },
-					{ name: '**Bot Support:**', value: '[Join Cactie\'s guilded server!](https://guilded.gg/cactie)' },
-					{ name: '**Nether Depths:**', value: '[Also check out Nether Depths!](https://netherdepths.com)' },
-				]);
 			await message.reply({ embeds: [InvEmbed] });
 		}
 		catch (err) { client.error(err, message); }
