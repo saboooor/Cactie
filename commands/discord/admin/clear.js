6120c418b192d4994682c5ab677a77cc84a3a533
Saboor <42164502+saboooor@users.noreply.github.com>
2022-05-09 13:10:41 -0600

    uhhhhhh

--- commands/discord/admin/clear.js
+++ commands/discord/admin/clear.js
@@ -19,8 +19,8 @@ module.exports = {
 			if (args[0] > 100) return client.error('You can only clear 100 messages at once!', message, true);
 
 			// Fetch the messages and bulk delete them
-			const messages = await message.channel.messages.fetch({ limit: args[0] });
-			if (args[1]) messages.filter(msg => msg.author.id == args[1]);
+			let messages = await message.channel.messages.fetch({ limit: args[0] });
+			if (args[1]) messages = messages.filter(msg => msg.author.id == args[1]);
 			message.channel.bulkDelete(messages).catch(err => client.error(err, message, true));
 
 			// Reply with response
