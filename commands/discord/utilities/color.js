94db6007f712108f4042e8bfd8e3a13e9f8b9762
saboooor <saboor547@gmail.com>
2022-05-09 22:30:44 -0600

    add color picker command

---
+++ commands/discord/utilities/color.js
@@ -0,0 +1,23 @@
+const { EmbedBuilder } = require('discord.js');
+const getColors = require('get-image-colors');
+module.exports = {
+	name: 'color',
+	description: 'Get a color or multiple colors from an image',
+	usage: '<URL of Image (gif, jpg, png, svg)> [Amount of colors]',
+	aliases: ['colors', 'c', 'pick'],
+	args: true,
+	options: require('../../options/color.js'),
+	async execute(message, args, client) {
+		try {
+			if (args[0] < 1 || args[0] > 5) return client.error('The amount of colors can only be between 1 and 5!', message, true);
+			const colors = await getColors(args[0], { count: args[1] ? parseInt(args[1]) : 1 });
+			const embeds = colors.map(color => {
+				return new EmbedBuilder()
+					.setColor(parseInt(color.hex().replace('#', '0x')))
+					.setTitle(`**${color.hex()}**`);
+			});
+			message.reply({ embeds });
+		}
+		catch (err) { client.error(err, message); }
+	},
+};
