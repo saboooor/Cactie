290378bd81ee337614ee8164d727f1315a85019c
Saboor <42164502+saboooor@users.noreply.github.com>
2022-05-10 23:27:27 -0600

    Update invite.js

--- commands/utilities/invite.js
+++ commands/utilities/invite.js
@@ -8,8 +8,7 @@ module.exports = {
 		try {
 			const Embed = new MessageEmbed()
 				.setColor(Math.round(Math.random() * 16777215))
-				.addField('**Add Cactie Bot to your server:**', '[Invite Cactie to your server using this link!](https://cactie.smhsmh.club/invite/discord)')
-				.addField('**Add the secondary Cactie Dev Bot:**', '[Invite Cactie Dev to your server using this link!](https://cactie.smhsmh.club/dev/discord)')
+				.addField('**Add Cactie Bot to your server:**', '[Invite Cactie to your server using this link!](https://cactie.smhsmh.club/invite)')
 				.addField('**Bot Support:**', '[Join Cactie\'s discord server!](https://cactie.smhsmh.club/discord)')
 				.addField('**Nether Depths:**', '[Also check out Nether Depths!](https://netherdepths.com/discord)');
 			await message.reply({ embeds: [Embed] });
