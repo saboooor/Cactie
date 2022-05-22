ddbb9c7177e7c9ea8f02ea377304038a6e070d8c
saboooor <saboor547@gmail.com>
2022-05-21 19:23:19 -0600

    converting mp4 to gif is more problematic than just putting it as an attachment

--- functions/redditFetch.js
+++ functions/redditFetch.js
@@ -1,9 +1,5 @@
-const { FormData } = require('formdata-node');
-const { fileFromPath } = require('formdata-node/file-from-path');
 const { EmbedBuilder } = require('discord.js');
-const ffmpegSync = require('./ffmpegSync.js');
-const { nsfw, refresh, warn } = require('../lang/int/emoji.json');
-const fs = require('fs');
+const { nsfw } = require('../lang/int/emoji.json');
 module.exports = async function redditFetch(subreddits, message, client, attempts) {
 	if (!attempts) attempts = 1;
 	const subreddit = subreddits[Math.floor(Math.random() * subreddits.length)];
@@ -41,43 +37,15 @@ module.exports = async function redditFetch(subreddits, message, client, attempt
 		const gif = await fetch(`https://api.redgifs.com/v2/gifs/${data.url.split('redgifs.com/watch/')[1]}`);
 		const gifData = await gif.json();
 		if (!gifData.gif || !gifData.gif.urls || !gifData.gif.urls.hd) return redditFetch(subreddits, message, client, attempts + 1);
-		if (gifData.gif.duration > 20) return redditFetch(subreddits, message, client, attempts + 1);
-		data.url = gifData.gif.urls.sd;
+		data.url = gifData.gif.urls.hd;
 		client.logger.info(`Redgifs URL: ${data.url}`);
 		PostEmbed.setAuthor({ name: `u/${data.author} (redgifs: @${gifData.gif.userName})`, url: gifData.user.profileUrl.startsWith('http') ? gifData.user.profileUrl : null })
 			.setColor(parseInt(gifData.gif.avgColor.replace('#', '0x')))
 			.setURL(data.url);
 	}
 	if (data.url.includes('v.redd.it')) data.url = `${data.url}/DASH_480.mp4?source=fallback`;
-	const file = `${timestamp}.gif`;
 	PostEmbed.setImage(data.url);
-	const msg = await message.reply({ embeds: [PostEmbed] });
-	if (data.url.endsWith('.gifv') || data.url.endsWith('.mp4') || data.url.endsWith('DASH_480.mp4?source=fallback')) {
-		if (client.type.name == 'guilded') return redditFetch(subreddits, message, client, attempts + 1);
-		msg.edit({ content: `<:refresh:${refresh}> **Processing GIF...**` });
-
-		// Convert file to gif
-		await ffmpegSync(data.url.replace('.gifv', '.mp4'), file, msg).catch(err => {
-			client.logger.error(err);
-			msg.edit({ content: `<:alert:${warn}> **Error while processing GIF**\n${err}` });
-			return redditFetch(subreddits, message, client, attempts + 1);
-		});
-
-		// Check file size
-		const stats = fs.statSync(file);
-		const fileSizeInBytes = stats.size;
-		if (fileSizeInBytes > 50000000) {
-			msg.edit({ content: `<:alert:${warn}> **GIF too big**` });
-			return redditFetch(subreddits, message, client, attempts + 1);
-		}
-
-		msg.edit({ content: `<:refresh:${refresh}> **Uploading GIF...**` });
-		// Create form and upload
-		const form = new FormData();
-		form.set('file', await fileFromPath(file));
-		await fetch('https://smhsmh.club/pupupload', { method: 'POST', body: form });
-		PostEmbed.setImage(`https://smhsmh.club/pup/${file}`);
-		msg.edit({ content: null, embeds: [PostEmbed] });
-		fs.unlinkSync(file);
-	}
+	let files;
+	if (data.url.endsWith('.mp4') || data.url.endsWith('.gifv') || data.url.endsWith('DASH_480.mp4?source=fallback')) files = [{ attachment: data.url, name: data.url.split('/').pop() }];
+	message.reply({ embeds: [PostEmbed], files });
 };
