013477d9b303cbe1f51f18af2c02e7ec2ec1a04b
saboooor <saboor547@gmail.com>
2022-05-24 23:55:05 -0600

    status

--- events/discord/ready/done.js
+++ events/discord/ready/done.js
@@ -1,6 +1,7 @@
 const { ActivityType } = require('discord.js');
 module.exports = async (client) => {
-	client.user.setPresence({ activities: [{ name: 'Just Restarted!', type: ActivityType.Game }], status: 'dnd' });
+	client.user.setPresence({ activities: [{ name: 'Just Restarted!', type: ActivityType.Game }] });
+	client.user.setStatus('dnd');
 	client.manager.init(client.user.id);
 	const timer = (Date.now() - client.startTimestamp) / 1000;
 	client.logger.info(`Done (${timer}s)! I am running!`);
