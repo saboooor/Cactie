311af0ea6dbf06ec4abf83ca6bc0eacf60c9e2c3
codefactor-io <support@codefactor.io>
2022-05-27 09:45:09 +0000

    [CodeFactor] Apply fixes to commit c225eb9

--- commands/universal/private/eval.js
+++ commands/universal/private/eval.js
@@ -7,8 +7,8 @@ module.exports = {
 	cooldown: 0.1,
 	async execute(message, args, client) {
 		// Check if user is sab lolololol
-		if (client.type.name == 'guilded' && message.createdById !== 'AYzRpEe4') return client.error('You can\'t do that!')
-		if (client.type.name != 'guilded' && message.author.id !== '249638347306303499') return client.error('You can\'t do that!')
+		if (client.type.name == 'guilded' && message.createdById !== 'AYzRpEe4') return client.error('You can\'t do that!');
+		if (client.type.name != 'guilded' && message.author.id !== '249638347306303499') return client.error('You can\'t do that!');
 		try {
 			const code = args.join(' ');
 			let evaled = eval(code);
