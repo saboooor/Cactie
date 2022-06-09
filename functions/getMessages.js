43f6f8002626cc1124073828748d60ccc6c93ded
Saboor <42164502+saboooor@users.noreply.github.com>
2022-06-09 13:00:26 -0600

    getMessages function

---
+++ functions/getMessages.js
@@ -0,0 +1,22 @@
+module.exports = async function getMessages(channel, limit = 100) {
+    let sum_messages = [];
+    let last_id;
+    if (limit <= 100) {
+        const messages = await channel.messages.fetch({ limit });
+        sum_messages = messages.array();
+    }
+    else {
+        while (true) {
+            const options = { limit: 100 };
+            if (last_id) options.before = last_id;
+    
+            const messages = await channel.messages.fetch(options);
+            sum_messages.push(...messages.array());
+            last_id = messages.last().id;
+    
+            if (messages.size != 100 || sum_messages >= limit) break;
+        }    
+    }
+
+    return sum_messages;
+}
