9b1862168f65e72ada1caa88acdfec22e2414abc
Saboor <42164502+saboooor@users.noreply.github.com>
2022-06-09 13:12:57 -0600

    idek what im doing at this point

--- functions/getMessages.js
+++ functions/getMessages.js
@@ -1,20 +1,24 @@
 module.exports = async function getMessages(channel, limit = 100) {
     let sum_messages = [];
+    let remaining = limit;
     let last_id;
     if (limit <= 100) {
         const messages = await channel.messages.fetch({ limit });
         sum_messages.push(messages);
     }
     else {
-        while (true) {
+        while (remaining > 0) {
             const options = { limit: 100 };
+            if (remaining < 100) {
+                options.limit = remaining;
+                remaining = 0
+            }
+            else { remaining = remaining - 100; }
             if (last_id) options.before = last_id;
     
             const messages = await channel.messages.fetch(options);
             sum_messages.push(messages);
             last_id = messages.last().id;
-    
-            if (messages.size != 100 || sum_messages.length >= Math.ceil(limit / 100)) break;
         }    
     }
 
