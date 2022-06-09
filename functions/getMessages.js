0488929094cf1f43d9be9e8a8f5558f449e2c6f6
codefactor-io <support@codefactor.io>
2022-06-09 19:00:35 +0000

    [CodeFactor] Apply fixes to commit 43f6f80

--- functions/getMessages.js
+++ functions/getMessages.js
@@ -9,14 +9,14 @@ module.exports = async function getMessages(channel, limit = 100) {
         while (true) {
             const options = { limit: 100 };
             if (last_id) options.before = last_id;
-    
+
             const messages = await channel.messages.fetch(options);
             sum_messages.push(...messages.array());
             last_id = messages.last().id;
-    
+
             if (messages.size != 100 || sum_messages >= limit) break;
-        }    
+        }
     }
 
     return sum_messages;
-}
+};
