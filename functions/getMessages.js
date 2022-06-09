5a1089afcf73b8897861ce9c353a11a7e18485a3
codefactor-io <support@codefactor.io>
2022-06-09 19:13:05 +0000

    [CodeFactor] Apply fixes to commit 9b18621

--- functions/getMessages.js
+++ functions/getMessages.js
@@ -1,5 +1,5 @@
 module.exports = async function getMessages(channel, limit = 100) {
-    let sum_messages = [];
+    const sum_messages = [];
     let remaining = limit;
     let last_id;
     if (limit <= 100) {
@@ -11,16 +11,16 @@ module.exports = async function getMessages(channel, limit = 100) {
             const options = { limit: 100 };
             if (remaining < 100) {
                 options.limit = remaining;
-                remaining = 0
+                remaining = 0;
             }
             else { remaining = remaining - 100; }
             if (last_id) options.before = last_id;
-    
+
             const messages = await channel.messages.fetch(options);
             sum_messages.push(messages);
             last_id = messages.last().id;
-        }    
+        }
     }
 
     return sum_messages;
-}
+};
