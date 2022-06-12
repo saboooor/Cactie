1f64dcbb237f097ae02d37abab561e9aa08041f6
saboooor <saboor547@gmail.com>
2022-06-12 16:26:26 -0600

    server created details

--- events/guilded/serverCreated.js
+++ events/guilded/serverCreated.js
@@ -1,8 +1,23 @@
-const { Embed } = require('guilded.js');
+const { EmbedBuilder } = require('discord.js');
 module.exports = async (client, member) => {
-	client.logger.info(`${client.user.name} has been added to ${member.serverId}`);
-	const AddEmbed = new Embed()
+	const guild = await client.servers.fetch(member.serverId);
+	let owner = await client.members.cache.get(`${guild.id}:${guild.ownerId}`);
+	if (!owner) owner = await client.members.fetch(guild.id, guild.ownerId);
+	const srvEmbed = new EmbedBuilder()
 		.setColor(Math.floor(Math.random() * 16777215))
-		.setTitle(`${client.user.name} has been added to ${member.serverId}`);
-	client.messages.send('e1eb6361-e31d-43b9-afce-2c05ba4fb95e', { embeds: [AddEmbed] });
+		.setTitle(`${client.user.name} has been added to ${guild.name}`)
+		.setThumbnail(guild.iconURL)
+		.setFooter({ text: `Owner: ${owner.user.name}`, iconURL: owner.user.avatar });
+	if (guild.raw.banner) srvEmbed.setImage(guild.raw.banner);
+	const timestamp = new Date(guild.createdAt);
+	const string = timestamp.toLocaleString('default', { month: 'long', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+	const members = await client.members.fetchMany(guild.id);
+	srvEmbed.addFields([
+		{ name: 'Members', value: `${members.size}` },
+		{ name: 'Time zone', value: `${guild.timezone}` },
+		{ name: 'Created At', value: `${string}` },
+	]);
+	if (guild.shortURL) srvEmbed.addFields([{ name: 'Vanity URL', value: `guilded.gg/${guild.shortURL}` }]);
+	client.messages.send('e1eb6361-e31d-43b9-afce-2c05ba4fb95e', { embeds: [srvEmbed] });
+	client.logger.info(`${client.user.name} has been added to ${member.serverId}`);
 };
