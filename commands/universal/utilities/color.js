e40a9fe4b3dd58a903514ca7a5412033471e99dc
saboooor <saboor547@gmail.com>
2022-06-12 17:41:04 -0600

    add color command to guilded

--- commands/universal/utilities/color.js
+++ commands/universal/utilities/color.js
@@ -1,5 +1,6 @@
 const { EmbedBuilder } = require('discord.js');
 const getColors = require('get-image-colors');
+const regex = /\[([^[]+)\](\((.*)\))/g;
 module.exports = {
 	name: 'color',
 	description: 'Get a color or multiple colors from an image',
@@ -10,13 +11,15 @@ module.exports = {
 	async execute(message, args, client) {
 		try {
 			if (args[0] < 1 || args[0] > 5) return client.error('The amount of colors can only be between 1 and 5!', message, true);
-			const colors = await getColors(args[0], { count: args[1] ? parseInt(args[1]) : 1 });
+			const matches = [...args[0].matchAll(regex)];
+			const url = matches ? matches[0][1] : args[0];
+			const colors = await getColors(url, { count: args[1] ? parseInt(args[1]) : 1 });
 			const embeds = colors.map(color => {
 				return new EmbedBuilder()
 					.setColor(color.num())
 					.setTitle(`**${color.hex()}**`);
 			});
-			message.reply({ embeds });
+			message.reply({ embeds: client.type.name == 'guilded' ? [embeds[0]] : embeds });
 		}
 		catch (err) { client.error(err, message); }
 	},
