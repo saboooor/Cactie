557bc79d375c03c976ff2c0d026e0be9d31b7efc
saboooor <saboor547@gmail.com>
2022-06-13 21:51:05 -0600

    clear more than 100 msgs

--- commands/discord/admin/clear.js
+++ commands/discord/admin/clear.js
@@ -1,5 +1,6 @@
-const { EmbedBuilder } = require('discord.js');
+const { EmbedBuilder, Collection } = require('discord.js');
 const getTranscript = require('../../../functions/getTranscript.js').discord;
+const getMessages = require('../../../functions/getMessages.js');
 const { yes } = require('../../../lang/int/emoji.json');
 module.exports = {
 	name: 'clear',
@@ -7,7 +8,7 @@ module.exports = {
 	ephemeral: true,
 	args: true,
 	aliases: ['purge'],
-	usage: '<Message Amount> [User to clear the messages of]',
+	usage: '<Message Amount> [Message Author] [Message Content Has]',
 	similarcmds: 'clearqueue or cleareffects',
 	permission: 'ManageMessages',
 	botperm: 'ManageMessages',
@@ -16,29 +17,34 @@ module.exports = {
 		try {
 			// Check if arg is a number and is more than 100
 			if (isNaN(args[0])) return client.error('That is not a number!', message, true);
-			if (args[0] > 100) return client.error('You can only clear 100 messages at once!', message, true);
+			if (args[0] > 1000) return client.error('You can only clear 1000 messages at once!', message, true);
 
 			// Fetch the messages and bulk delete them
-			let messages = await message.channel.messages.fetch({ limit: args[0] });
-			messages = messages.filter(msg => msg.createdTimestamp > Date.now() - 1209600);
-			if (args[1]) messages = messages.filter(msg => msg.author.id == args[1]);
-			if (args[2]) messages = messages.filter(msg => msg.content.includes(args[2]));
-			if (!messages.size) return client.error('There are no messages in that scope, try a higher number?', message, true);
-			message.channel.bulkDelete(messages).catch(err => client.error(err, message, true));
+			const messagechunks = await getMessages(message.channel, args[0]);
+			const allmessages = new Collection();
+			messagechunks.forEach(async messages => {
+				messages = messages.filter(msg => msg.createdTimestamp > Date.now() - 1209600);
+				if (args[1]) messages = messages.filter(msg => msg.author.id == args[1]);
+				if (args[2]) messages = messages.filter(msg => msg.content.includes(args[2]));
+				if (!messages.size) return;
+				message.channel.bulkDelete(messages).catch(err => client.error(err, message, true));
+				allmessages.concat(messages);
+			});
+			if (!allmessages.size) return client.error('There are no messages in that scope, try a higher number?', message, true);
 
 			// Reply with response
-			if (message.commandName) message.reply({ content: `<:yes:${yes}> **Cleared ${messages.size} messages!**` });
-			client.logger.info(`Cleared ${messages.size} messages from #${message.channel.name} in ${message.guild.name}`);
+			if (message.commandName) message.reply({ content: `<:yes:${yes}> **Cleared ${allmessages.size} messages!**` });
+			client.logger.info(`Cleared ${allmessages.size} messages from #${message.channel.name} in ${message.guild.name}`);
 
 			// Check if log channel exists and send message
 			const srvconfig = await client.getData('settings', 'guildId', message.guild.id);
 			const logchannel = message.guild.channels.cache.get(srvconfig.logchannel);
 			if (logchannel) {
 				const ClearEmbed = new EmbedBuilder()
-					.setTitle(`${message.member.user.tag} cleared ${messages.size} messages`)
+					.setTitle(`${message.member.user.tag} cleared ${allmessages.size} messages`)
 					.addFields([
 						{ name: 'Channel', value: `${message.channel}` },
-						{ name: 'Transcript', value: `${await getTranscript(messages)}` },
+						{ name: 'Transcript', value: `${await getTranscript(allmessages)}` },
 					]);
 				logchannel.send({ embeds: [ClearEmbed] });
 			}
--- functions/getMessages.js
+++ functions/getMessages.js
@@ -1,26 +1,26 @@
 module.exports = async function getMessages(channel, limit = 100) {
-    const sum_messages = [];
-    let remaining = limit;
-    let last_id;
-    if (limit <= 100) {
-        const messages = await channel.messages.fetch({ limit });
-        sum_messages.push(messages);
-    }
-    else {
-        while (remaining > 0) {
-            const options = { limit: 100 };
-            if (remaining < 100) {
-                options.limit = remaining;
-                remaining = 0;
-            }
-            else { remaining = remaining - 100; }
-            if (last_id) options.before = last_id;
+	const sum_messages = [];
+	let remaining = limit;
+	let last_id;
+	if (limit <= 100) {
+		const messages = await channel.messages.fetch({ limit });
+		sum_messages.push(messages);
+	}
+	else {
+		while (remaining > 0) {
+			const options = { limit: 100 };
+			if (remaining < 100) {
+				options.limit = remaining;
+				remaining = 0;
+			}
+			else { remaining = remaining - 100; }
+			if (last_id) options.before = last_id;
 
-            const messages = await channel.messages.fetch(options);
-            sum_messages.push(messages);
-            last_id = messages.last().id;
-        }
-    }
+			const messages = await channel.messages.fetch(options);
+			sum_messages.push(messages);
+			last_id = messages.last().id;
+		}
+	}
 
-    return sum_messages;
+	return sum_messages;
 };
