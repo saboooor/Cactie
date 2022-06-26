00a09d8f62d9fff935987ec260b61e8f3fd2799c
saboooor <saboor547@gmail.com>
2022-06-25 18:26:07 -0600

    rename greeting file

--- events/discord/guildCreate/greeting.js
+++ events/discord/guildCreate/greeting.js
@@ -23,9 +23,8 @@ module.exports = async (client, guild) => {
 		.setDescription(`
 My prefix is \`${srvconfig.prefix}\`, you may change this with \`/settings prefix\`
 Type \`${srvconfig.prefix}help\` for help, and \`${srvconfig.prefix}invite\` to invite me to other servers!
-You may also change the bot's language with \`/settings language\` or personal language with \`/language\`
-
-This bot has reactions to messages with some specific keywords which at times may be annoying. To turn them off, do \`/settings reactions\``)
+Please take some time going through the settings so that ${client.user.username} works well! \`/settings\`
+		`)
 		.setThumbnail('https://cactie.smhsmh.club/assets/images/Cactie.png');
 	const message = { embeds: [greetingEmbed], components: [row] };
 	const owner = await guild.fetchOwner();
