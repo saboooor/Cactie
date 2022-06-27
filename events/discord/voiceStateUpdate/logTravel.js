43370d2125d096cecd9b14b1a95a9aef056f352d
saboooor <saboor547@gmail.com>
2022-06-26 19:28:55 -0600

    fix voice travel audit log

--- events/discord/voiceStateUpdate/logTravel.js
+++ events/discord/voiceStateUpdate/logTravel.js
@@ -14,28 +14,28 @@ module.exports = async (client, oldState, newState) => {
 	const logEmbed = new EmbedBuilder()
 		.setColor(0x2f3136)
 		.setAuthor({ name: newState.member.user.tag, iconURL: newState.member.user.avatarURL() })
-		.setFields([
-			{ name: 'Member', value: `${newState.member}`, inline: true },
-			{ name: 'Channel', value: `${newState.channel}`, inline: true },
-		]);
+		.setFields([{ name: 'Member', value: `${newState.member}`, inline: true }]);
 
 	// Check if the user joined
 	if (!oldState.channelId && newState.channelId) {
 		// Check if log is enabled and set title accordingly
 		if (!['voicejoin', 'voice', 'other', 'all'].some(logtype => srvconfig.auditlogs.split(',').includes(logtype))) return;
-		logEmbed.setTitle(`<:in:${join}> Member joined voice channel`);
+		logEmbed.setTitle(`<:in:${join}> Member joined voice channel`)
+			.setFields([{ name: 'Channel', value: `${newState.channel}`, inline: true }]);
 	}
 	// Check if the user left
 	else if (oldState.channelId && !newState.channelId) {
 		// Check if log is enabled and set title accordingly
 		if (!['voiceleave', 'voice', 'other', 'all'].some(logtype => srvconfig.auditlogs.split(',').includes(logtype))) return;
-		logEmbed.setTitle(`<:out:${leave}> Member left voice channel`);
+		logEmbed.setTitle(`<:out:${leave}> Member left voice channel`)
+			.setFields([{ name: 'Channel', value: `${oldState.channel}`, inline: true }]);
 	}
 	// Check if user moved
 	else if (oldState.channelId != newState.channelId) {
 		// Check if log is enabled and set title accordingly
 		if (!['voicemove', 'voice', 'other', 'all'].some(logtype => srvconfig.auditlogs.split(',').includes(logtype))) return;
-		logEmbed.setTitle(`<:right:${right}> Member moved voice channels`);
+		logEmbed.setTitle(`<:right:${right}> Member moved voice channels`)
+			.setFields([{ name: 'Channels', value: `${oldState.channel} <:right:${right}> ${newState.channel}`, inline: true }]);
 	}
 
 	// Send log
