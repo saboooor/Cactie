a97038a5d7dec56028b9e7cd08a5b90b6e06e471
saboooor <saboor547@gmail.com>
2022-06-29 18:58:59 -0600

    Remove r/tit

--- commands/universal/nsfw/tits.js
+++ commands/universal/nsfw/tits.js
@@ -1,11 +1,11 @@
 const redditFetch = require('../../../functions/redditFetch.js');
 module.exports = {
 	name: 'tits',
-	aliases: ['tit', 'boob', 'boobs', 'boobies', 'tiddies', 'titty'],
+	aliases: ['boob', 'boobs', 'boobies', 'tiddies', 'titty'],
 	description: 'r/tits, r/TittyDrop, r/tit, r/boobs, r/titsonastick, r/BustyPetite, r/tittyfuck, r/BigBoobsGW, r/PetiteTits',
 	async execute(message, args, client) {
 		try {
-			redditFetch(['tits', 'TittyDrop', 'tit', 'boobs', 'titsonastick', 'BustyPetite', 'tittyfuck', 'BigBoobsGW', 'PetiteTits'], message, client);
+			redditFetch(['tits', 'TittyDrop', 'boobs', 'titsonastick', 'BustyPetite', 'tittyfuck', 'BigBoobsGW', 'PetiteTits'], message, client);
 		}
 		catch (err) { client.error(err, message); }
 	},
