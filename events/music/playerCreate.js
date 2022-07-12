2ebd6b2c83ee9b38d55e7f3fd09bf53913186871
saboooor <saboor547@gmail.com>
2022-07-11 21:43:46 -0600

    fix playerdestroy if bot was kicked

--- events/music/playerCreate.js
+++ events/music/playerCreate.js
@@ -1,3 +1,4 @@
 module.exports = async (client, player) => {
-	client.logger.info(`Player has been created in ${client.guilds.cache.get(player.guild).name}`);
+	const guild = client.guilds.cache.get(player.guild);
+	client.logger.info(`Player has been created in ${guild ? guild.name : player.guild}`);
 };
--- events/music/playerDestroy.js
+++ events/music/playerDestroy.js
@@ -1,3 +1,4 @@
 module.exports = async (client, player) => {
-	client.logger.info(`Player has been destroyed in ${client.guilds.cache.get(player.guild).name}`);
+	const guild = client.guilds.cache.get(player.guild);
+	client.logger.info(`Player has been destroyed in ${guild ? guild.name : player.guild}`);
 };
