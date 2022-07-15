4f98671c9c9c7d44860a560dd5e074acbb84cdb0
saboooor <saboor547@gmail.com>
2022-07-15 02:05:47 -0600

    fix animals

--- options/animals.js
+++ options/animals.js
@@ -1,12 +1,12 @@
 const { SlashCommandSubcommandBuilder } = require('discord.js');
 const { readdirSync } = require('fs');
 module.exports = async function options(cmd) {
-	const commands = readdirSync('./commands/universal/animals');
+	const commands = readdirSync('./commands/animals');
 	commands.forEach(commandName => {
 		cmd.addSubcommand(
 			new SlashCommandSubcommandBuilder()
 				.setName(commandName.replace('.js', ''))
-				.setDescription(require(`../universal/animals/${commandName}`).description),
+				.setDescription(require(`../commands/animals/${commandName}`).description),
 		);
 	});
 };
