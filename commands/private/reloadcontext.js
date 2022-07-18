16718336750f50af58aae3ff029bb2a22d8ae52d
saboooor <saboor547@gmail.com>
2022-07-17 18:35:42 -0600

    a

--- commands/private/reloadcontext.js
+++ commands/private/reloadcontext.js
@@ -8,6 +8,12 @@ module.exports = {
 			if (message.author.id !== '249638347306303499') return client.error('You can\'t do that!', message, true);
 			if (!client.application?.owner) await client.application?.fetch();
 			const commands = await client.application?.commands.fetch();
+			for (let command of commands) {
+				command = command[1];
+				if (client.slashcommands.find(c => c.name == command.name)) continue;
+				client.logger.info(`Detected /${command.name} has been deleted! Deleting command...`);
+				await command.delete();
+			}
 			for (let command of client.slashcommands) {
 				command = command[1];
 				if (!command.type) continue;
@@ -16,12 +22,6 @@ module.exports = {
 					.setType(ApplicationCommandType[command.type]);
 				await client.application?.commands.create(cmd.toJSON());
 				client.logger.info(`Overwritten /${command.name}`);
-			}
-			for (let command of commands) {
-				command = command[1];
-				if (client.slashcommands.find(c => c.name == command.name)) continue;
-				client.logger.info(`Detected /${command.name} has been deleted! Deleting command...`);
-				await command.delete();
 			}
 			message.reply('All context menu commands have been updated!');
 		}
