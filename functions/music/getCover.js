5b97927043a00e5ad19e41f7a224b9c3763335af
saboooor <saboor547@gmail.com>
2022-07-26 23:29:18 -0600

    yes

--- functions/music/getCover.js
+++ functions/music/getCover.js
@@ -4,32 +4,30 @@ const YAML = require('yaml');
 const { music } = YAML.parse(fs.readFileSync('./config.yml', 'utf8'));
 const lastfm = new LastFM(music.lastfm, { userAgent: 'Cactie/3.0.0 (https://cactie.smhsmh.club)' });
 module.exports = async function getCover(title, author, player) {
-	return new Promise((resolve, reject) => {
-		let img;
-		lastfm.trackSearch({ q: title }, async (err, data) => {
-			if (err) reject(err);
-			data.result.forEach(result => {
-				if (!author.toLowerCase().includes(result.artistName.toLowerCase()) || !result.images[0] || result.images[0].includes('2a96cbd8b46e442fc41c2b86b821562f')) return;
-				if (!img) img = result.images.pop();
-			});
-			if (!img) {
-				lastfm.albumSearch({ q: title }, async (alberr, albdata) => {
-					if (alberr) reject(alberr);
-					albdata.result.forEach(result => {
-						if (!author.toLowerCase().includes(result.artistName.toLowerCase()) || !result.images[0] || result.images[0].includes('2a96cbd8b46e442fc41c2b86b821562f')) return;
-						if (!img) img = result.images.pop();
-					});
-					if (!img) {
-						const Searched = await player.search(`${title} ${author}`);
-						const track = Searched.tracks[0];
-						if (track && track.displayThumbnail) img = track.displayThumbnail('hqdefault');
-					}
-					resolve(img);
-				});
-			}
-			else {
-				resolve(img);
-			}
+	let img;
+	lastfm.trackSearch({ q: title }, async (err, data) => {
+		if (err) throw new Error(err);
+		data.result.forEach(result => {
+			if (!author.toLowerCase().includes(result.artistName.toLowerCase()) || !result.images[0] || result.images[0].includes('2a96cbd8b46e442fc41c2b86b821562f')) return;
+			if (!img) img = result.images.pop();
 		});
+		if (!img) {
+			lastfm.albumSearch({ q: title }, async (alberr, albdata) => {
+				if (alberr) throw new Error(alberr);
+				albdata.result.forEach(result => {
+					if (!author.toLowerCase().includes(result.artistName.toLowerCase()) || !result.images[0] || result.images[0].includes('2a96cbd8b46e442fc41c2b86b821562f')) return;
+					if (!img) img = result.images.pop();
+				});
+				if (!img) {
+					const Searched = await player.search(`${title} ${author}`);
+					const track = Searched.tracks[0];
+					if (track && track.displayThumbnail) img = track.displayThumbnail('hqdefault');
+				}
+				return img;
+			});
+		}
+		else {
+			return img;
+		}
 	});
 };
--- handlers/error.js
+++ handlers/error.js
@@ -2,7 +2,7 @@ const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('
 module.exports = client => {
 	// Create a function for error messaging
 	client.error = function error(err, message, userError) {
-		err = err.stack ? err.stack : err;
+		err = err.stack ?? err;
 		logger.error(err);
 		const errEmbed = new EmbedBuilder()
 			.setColor(0xE74C3C)
