d70a26991d71494a6f124ae5c02d63805a09e464
saboooor <saboor547@gmail.com>
2022-07-28 01:13:17 -0600

    a

--- commands/nsfw/dick.js
+++ commands/nsfw/dick.js
@@ -3,10 +3,10 @@ const redditFetch = require('../../functions/redditFetch.js');
 module.exports = {
 	name: 'dick',
 	aliases: ['dicks', 'cock', 'cocks', 'penis'],
-	description: 'r/dicks, r/DickPics4Freedom, r/penis, r/ThickDick, r/DicksPointingUp, r/cock',
+	description: 'r/dicks, r/DickPics4Freedom, r/penis, r/ThickDick, r/cock',
 	async execute(message, args, client) {
 		try {
-			redditFetch(['dicks', 'DickPics4Freedom', 'penis', 'ThickDick', 'DicksPointingUp', 'cock'], message, client);
+			redditFetch(['dicks', 'DickPics4Freedom', 'penis', 'ThickDick', 'cock'], message, client);
 		}
 		catch (err) { client.error(err, message); }
 	},
