99d124b56113eb7641296a4ca7bdb8c1240d4091
saboooor <saboor547@gmail.com>
2022-07-27 23:59:40 -0600

    yes

--- commands/utilities/github.js
+++ commands/utilities/github.js
@@ -16,19 +16,12 @@ module.exports = {
 		catch (err) { client.error(err, message); }
 	},
 	async autoComplete(client, interaction) {
-		const options = interaction.options._hoistedOptions;
-		const option = options.find(o => o.focused);
-		if (option.name == 'repo') {
-			const query = await fetch(`https://api.github.com/search/repositories?q=${option.value}&per_page=25`, { headers: { 'Accept': 'application/json' } });
-			const searchResult = await query.json();
-			if (searchResult.items) {
-				if (!searchResult.items.length) return interaction.respond([{ name: 'No repos found', value: option.value }]);
-				const results = searchResult.items.map(item => { return { name: item.full_name, value: item.html_url }; });
-				interaction.respond(results);
-			}
-			else {
-				interaction.respond([{ name: `https://github.com/${option.value}`, value: option.value }]);
-			}
-		}
+		const option = interaction.options.getFocused();
+		const query = await fetch(`https://api.github.com/search/repositories?q=${option.value}&per_page=25`, { headers: { 'Accept': 'application/json' } });
+		const searchResult = await query.json();
+		if (!searchResult.items) return interaction.respond([{ name: `https://github.com/${option.value}`, value: option.value }]);
+		if (!searchResult.items.length) return interaction.respond([{ name: 'No repos found', value: option.value }]);
+		const results = searchResult.items.map(item => { return { name: item.full_name, value: item.html_url }; });
+		interaction.respond(results);
 	},
 };
