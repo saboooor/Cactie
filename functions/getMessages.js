98f7dfad7e8c5f12f355cf491648ab671cde15e9
saboooor <saboor547@gmail.com>
2022-08-24 12:54:23 -0600

    endless getmessages

--- functions/getMessages.js
+++ functions/getMessages.js
@@ -7,18 +7,21 @@ module.exports = async function getMessages(channel, limit = 100) {
 		messagechunks.push(messages);
 	}
 	else {
-		while (remaining > 0) {
+		while (remaining == 'infinite' || remaining > 0) {
 			const options = { limit: 100 };
 			if (remaining < 100) {
 				options.limit = remaining;
 				remaining = 0;
 			}
-			else { remaining = remaining - 100; }
+			else if (remaining != 'infinite') {
+				remaining = remaining - 100;
+			}
 			if (last_id) options.before = last_id;
 
 			const messages = await channel.messages.fetch(options).catch(err => { throw err; });
 			messagechunks.push(messages);
 			last_id = messages.last().id;
+			if (messages.size < 100) remaining = 0;
 		}
 	}
 
