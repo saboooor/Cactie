c7137c977e92f072c009d7dadc34e2f048e2a234
Saboor <42164502+saboooor@users.noreply.github.com>
2022-09-12 00:19:03 -0600

    messagedelete

---
+++ events/messageDelete/log.js
@@ -0,0 +1,22 @@
+const { EmbedBuilder } = require('discord.js');
+const { no } = require('../../lang/int/emoji.json');
+
+module.exports = async (client, message) => {
+	// Get current settings for the guild
+	const srvconfig = await client.getData('settings', 'guildId', channel.guild.id);
+
+	// Check if log is enabled and send log
+	if (['messagedelete', 'message', 'other', 'all'].some(logtype => srvconfig.auditlogs.split(',').includes(logtype))) {
+		const logchannel = channel.guild.channels.cache.get(srvconfig.logchannel);
+		if (!logchannel) return;
+		const logEmbed = new EmbedBuilder()
+			.setColor(0x2f3136)
+			.setAuthor({ name: message.author.tag })
+			.setTitle(`<:no:${no}> Message deleted`)
+			.setFields([
+				{ name: 'Channel', value: message.channel },
+				{ name: 'Content', value: message.content },
+			]);
+		logchannel.send({ embeds: [logEmbed] }).catch(err => logger.error(err));
+	}
+};
