15596e02149de75e2f4756e16813bb1e460873b8
saboooor <saboor547@gmail.com>
2022-09-26 00:01:52 -0600

    Fix join/leave message crashing the bot from empty message

--- events/guildMemberAdd/joinmsg.js
+++ events/guildMemberAdd/joinmsg.js
@@ -3,17 +3,17 @@ module.exports = async (client, member) => {
 	const srvconfig = await client.getData('settings', { guildId: member.guild.id });
 
 	// Check if join message is set
-	if (srvconfig.joinmessage == 'false') return;
+	if (srvconfig.joinmessage == 'false' || !srvconfig.joinmessage) return;
 
 	// Check if system channel is set
 	if (!member.guild.systemChannel) {
 		const owner = await member.guild.fetchOwner();
 		logger.warn(`${member.guild.name} (${owner.tag}) doesn't have a system channel set!`);
-		return owner.send({ content: `Join messages are enabled but a system message channel isn't set! Please either go into your server settings (${member.guild.name}) and set the system messages channel or turn off join messages with the \`/settings\` command` })
+		return owner.send({ content: `Join messages are enabled but a system message channel isn't set! Please either go into your server settings (${member.guild.name}) and set the system messages channel or turn off join messages at ${client.dashboardDomain}` })
 			.catch(err => logger.warn(err));
 	}
 
 	// Send the join message to the system channel
-	member.guild.systemChannel.send({ content: srvconfig.joinmessage.replace(/{USER MENTION}/g, `${member}`).replace(/{USER TAG}/g, member.user.tag) });
+	member.guild.systemChannel.send({ content: srvconfig.joinmessage.replace(/{USER MENTION}/g, `${member}`).replace(/{USER TAG}/g, member.user.tag) }).catch(err => logger.error(err));
 	logger.info(`Sent join message to ${member.guild.name} for ${member.user.tag}`);
 };
--- events/guildMemberRemove/leavemsg.js
+++ events/guildMemberRemove/leavemsg.js
@@ -3,17 +3,17 @@ module.exports = async (client, member) => {
 	const srvconfig = await client.getData('settings', { guildId: member.guild.id });
 
 	// Check if join message is set
-	if (srvconfig.leavemessage == 'false') return;
+	if (srvconfig.leavemessage == 'false' || !srvconfig.joinmessage) return;
 
 	// Check if system channel is set
 	if (!member.guild.systemChannel) {
 		const owner = await member.guild.fetchOwner();
 		logger.warn(`${member.guild.name} (${owner.tag}) doesn't have a system channel set!`);
-		return owner.send({ content: `Leave messages are enabled but a system message channel isn't set!\nPlease either go into your server settings (${member.guild.name}) and set the system messages channel or turn off leave messages with the \`/settings\` command` })
+		return owner.send({ content: `Leave messages are enabled but a system message channel isn't set!\nPlease either go into your server settings (${member.guild.name}) and set the system messages channel or turn off at ${client.dashboardDomain}` })
 			.catch(err => logger.warn(err));
 	}
 
 	// Send the join message to the system channel
-	member.guild.systemChannel.send({ content: srvconfig.leavemessage.replace(/{USER MENTION}/g, `${member}`).replace(/{USER TAG}/g, member.user.tag) });
+	member.guild.systemChannel.send({ content: srvconfig.leavemessage.replace(/{USER MENTION}/g, `${member}`).replace(/{USER TAG}/g, member.user.tag) }).catch(err => logger.error(err));
 	logger.info(`Sent leave message to ${member.guild.name} for ${member.user.tag}`);
 };
