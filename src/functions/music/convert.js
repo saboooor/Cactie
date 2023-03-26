001f2416d8437756cb05e7aca83b162ca24eb971
saboooor <saboor547@gmail.com>
2023-03-25 21:49:40 -0600

    music functions in ts

--- src/functions/music/convert.js
+++
@@ -1,9 +0,0 @@
-module.exports = function convertTime(duration) {
-	let seconds = parseInt((duration / 1000) % 60);
-	let minutes = parseInt((duration / (1000 * 60)) % 60);
-	const hours = parseInt((duration / (1000 * 60 * 60)) % 24);
-	seconds = (seconds < 10) ? '0' + seconds : seconds;
-	if (duration < 3600000) return minutes + ':' + seconds;
-	minutes = (minutes < 10) ? '0' + minutes : minutes;
-	return hours + ':' + minutes + ':' + seconds;
-};
---
+++ src/functions/music/convert.ts
@@ -0,0 +1,9 @@
+export default function convertTime(duration: number) {
+	let seconds: number | string = (duration / 1000) % 60;
+	let minutes: number | string = (duration / (1000 * 60)) % 60;
+	const hours: number | string = (duration / (1000 * 60 * 60)) % 24;
+	seconds = (seconds < 10) ? '0' + seconds : seconds;
+	if (duration < 3600000) return minutes + ':' + seconds;
+	minutes = (minutes < 10) ? '0' + minutes : minutes;
+	return hours + ':' + minutes + ':' + seconds;
+};
