9acc669c01da7feb459438b8948c5aa06ed0ef92
saboooor <saboor547@gmail.com>
2023-05-01 20:43:47 -0600

    a

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,6 +17,7 @@ module.exports = {
 	},
 	plugins: ['@typescript-eslint'],
 	rules: {
+		'@typescript-eslint/no-explicit-any': ['off'],
 		'@typescript-eslint/explicit-module-boundary-types': 'off',
 		'@typescript-eslint/no-inferrable-types': 'off',
 		'@typescript-eslint/no-non-null-assertion': 'off',
