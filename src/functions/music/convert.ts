5754d2571ec73209fb63041035c54be882fdde83
Saboor <42164502+saboooor@users.noreply.github.com>
2023-05-01 19:37:44 -0600

    Update convert.ts

--- src/functions/music/convert.ts
+++ src/functions/music/convert.ts
@@ -3,7 +3,7 @@ export default function convertTime(duration: number) {
   let minutes: number | string = (duration / (1000 * 60)) % 60;
   const hours: number | string = (duration / (1000 * 60 * 60)) % 24;
   seconds = (seconds < 10) ? '0' + seconds : seconds;
-  if (duration < 3600000) return Math.floor(minutes) + ':' + Math.floor(seconds);
+  if (duration < 3600000) return Math.floor(Number(minutes)) + ':' + Math.floor(Number(seconds));
   minutes = (minutes < 10) ? '0' + minutes : minutes;
-  return Math.floor(hours) + ':' + Math.floor(minutes) + ':' + Math.floor(seconds);
+  return Math.floor(Number(hours)) + ':' + Math.floor(Number(minutes)) + ':' + Math.floor(Number(seconds));
 }
