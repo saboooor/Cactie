e8cde0c7e7a8659bb411ae21676873f15041cb05
saboooor <saboor547@gmail.com>
2023-05-21 01:35:21 -0600

    fix mad

--- src/reactions/mad.ts
+++ src/reactions/mad.ts
@@ -2,7 +2,6 @@ import { Reaction } from '~/types/Objects';
 
 export const mad: Reaction = {
   triggers: ['mad', 'angry', 'kill ', 'punch', 'evil'],
-  additionaltriggers: ['cactie'],
   execute: (message) => {
     message.react('899340907432792105').catch(err => logger.error(err));
   },
