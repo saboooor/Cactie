679ef336bb31e584f2c3782295b2d2d51a12251b
saboooor <saboor547@gmail.com>
2023-07-04 19:25:42 -0600

    fix

--- src/events/interactionCreate/Command.ts
+++ src/events/interactionCreate/Command.ts
@@ -1,8 +1,7 @@
 import prisma, { getGuildConfig } from '~/functions/prisma';
 import { EmbedBuilder, Collection, ButtonBuilder, ButtonStyle, ActionRowBuilder, Client, CommandInteraction, GuildMember, GuildChannelResolvable, TextChannel, ApplicationCommandOptionType } from 'discord.js';
 import checkPerms from '~/functions/checkPerms';
-import { cooldowns } from '~/lists/private';
-import slashcommands from '~/lists/slash';
+import slashcommands, { cooldowns } from '~/lists/slash';
 import cooldownMessages from '~/misc/cooldown.json';
 
 export default async (client: Client, interaction: CommandInteraction) => {
