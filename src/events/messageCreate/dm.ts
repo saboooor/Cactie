c7c3cb62d68e38c35b8ae8a154b161fddf158bd7
saboooor <saboor547@gmail.com>
2023-12-06 21:25:18 -0700

    test

--- src/events/messageCreate/dm.ts
+++ src/events/messageCreate/dm.ts
@@ -14,13 +14,15 @@ export default async (client: Client, message: Message<false>) => {
     files.push(img);
   }
 
+  const branch = client.user?.username.split(' ')[1] ? `-${client.user.username.split(' ')[1].toLowerCase()}` : '';
+
   // If channel is DM,send the dm to the dms channel
   if (message.channel.isDMBased()) {
-    let thread = forum.threads.cache.find(t => t.name.endsWith(message.author.id));
+    let thread = forum.threads.cache.find(t => t.name.endsWith(message.author.id + branch));
 
     if (!thread) {
       thread = await forum.threads.create({
-        name: `${message.author.id}`,
+        name: message.author.id + branch,
         autoArchiveDuration: 1440,
         message: {
           content: `${message.author} **@${message.author.username}**`,
@@ -33,8 +35,14 @@ export default async (client: Client, message: Message<false>) => {
     thread.send({ content: `**${author}** > ${message.content}`, files, embeds: message.embeds, components: message.components });
   }
   else if (message.channel.parent?.id == forumId) {
-    const user = await client.users.fetch(message.channel.name);
-    if (user.bot || message.author.bot) return;
+    let id = message.channel.name;
+    if (branch != '') {
+      if (!id.endsWith(branch)) return;
+      id = id.replace(branch, '');
+    }
+
+    const user = await client.users.cache.get(id);
+    if (!user || user.bot || message.author.bot) return;
     const { content, embeds, components } = message;
     user.send({ content, files, embeds, components });
   }
