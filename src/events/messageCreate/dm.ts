8e886c5902c4d4039ded048707840dbe087e1a18
saboooor <saboor547@gmail.com>
2023-12-06 21:05:49 -0700

    a

--- src/events/messageCreate/dm.ts
+++ src/events/messageCreate/dm.ts
@@ -1,23 +1,11 @@
 import { AttachmentBuilder, Client, Message, TextChannel } from 'discord.js';
+const forumId = '1182123441491558511';
 
 export default async (client: Client, message: Message<false>) => {
-  // If channel is DM,send the dm to the dms channel
-  if (!message.channel.isDMBased()) return;
-
-  const forum = client.guilds.cache.get('811354612547190794')!.channels.cache.get('1182123441491558511') as TextChannel;
-  let thread = forum.threads.cache.find(t => t.name.endsWith(message.author.id));
-
-  if (!thread) {
-    thread = await forum.threads.create({
-      name: `${message.author.id}`,
-      autoArchiveDuration: 1440,
-      message: {
-        content: `${message.author}\n${message.author.username}`,
-      },
-    });
-  }
-
+  if (!message.channel.isDMBased() && message.channel.id != forumId) return;
+  const forum = client.guilds.cache.get('811354612547190794')!.channels.cache.get(forumId) as TextChannel;
   const files = [];
+
   for (const attachment of message.attachments) {
     const response = await fetch(attachment[1].url, { method: 'GET' });
     const arrayBuffer = await response.arrayBuffer();
@@ -25,7 +13,28 @@ export default async (client: Client, message: Message<false>) => {
       .setName(attachment[1].name);
     files.push(img);
   }
-  let author = `${message.author}`;
-  if (message.author.id == client.user?.id) author = `${client.user} > <@${message.channel.recipientId}>`;
-  thread.send({ content: `**${author}** > ${message.content}`, files, embeds: message.embeds, components: message.components });
+
+  // If channel is DM,send the dm to the dms channel
+  if (message.channel.isDMBased()) {
+    let thread = forum.threads.cache.find(t => t.name.endsWith(message.author.id));
+
+    if (!thread) {
+      thread = await forum.threads.create({
+        name: `${message.author.id}`,
+        autoArchiveDuration: 1440,
+        message: {
+          content: `${message.author} **@${message.author.username}**`,
+        },
+      });
+    }
+
+    let author = `${message.author}`;
+    if (message.author.id == client.user?.id) author = `${client.user} > <@${message.channel.recipientId}>`;
+    thread.send({ content: `**${author}** > ${message.content}`, files, embeds: message.embeds, components: message.components });
+  }
+  else if (message.channel.parent?.id == forumId) {
+    const user = await client.users.fetch(message.channel.name);
+    const { content, embeds, components } = message;
+    user.send({ content, files, embeds, components });
+  }
 };
